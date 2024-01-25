276f97cff3c60cc3cbf6391128bbed316bdce214
LF112 <lf@lf112.net>
2024-01-25 22:22:01 +0800

    feat: 置入 Million.js

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import react from '@vitejs/plugin-react-swc';
 
+import million from 'million/compiler';
 import path from 'path';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tsconfigPaths(), react()],
+  plugins: [million.vite({ auto: true }), tsconfigPaths(), react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src/'),
