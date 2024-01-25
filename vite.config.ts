4cd886543282548230aafff4c848d23308c8d9ae
LF112 <lf@lf112.net>
2024-01-25 22:02:54 +0800

    feat: 优化部分配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+
+import path from 'path';
+import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [tsconfigPaths(), react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src/'),
+    },
+  },
+  server: {
+    port: 2048,
+  },
 });
