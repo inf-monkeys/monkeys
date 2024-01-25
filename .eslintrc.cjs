92b4b982ad89f26ac9c9a5443f997ff0244cbe1d
LF112 <lf@lf112.net>
2024-01-25 21:55:40 +0800

    feat: 初始化 Vite + SWC + React + TS

---
+++ .eslintrc.cjs
@@ -0,0 +1,68 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+    'plugin:react/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'node_modules'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', '@typescript-eslint', 'react-hooks', 'simple-import-sort', 'prettier'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'func-names': 0,
+    'no-shadow': 0,
+    'linebreak-style': 'off',
+    '@typescript-eslint/no-shadow': 0,
+    '@typescript-eslint/no-unused-vars': [0, { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-use-before-define': 0,
+    '@typescript-eslint/ban-ts-ignore': 0,
+    '@typescript-eslint/no-empty-function': 0,
+    '@typescript-eslint/ban-ts-comment': 0,
+    '@typescript-eslint/no-var-requires': 0,
+    '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/no-this-alias': 0,
+    '@typescript-eslint/explicit-module-boundary-types': 0,
+    '@typescript-eslint/ban-types': 0,
+    'react-hooks/rules-of-hooks': 2,
+    'react-hooks/exhaustive-deps': 2,
+    'testing-library/no-unnecessary-act': 0,
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [['react'], ['^@?\\w'], ['@/(.*)'], ['^[./]'], ['(.*).module.scss']],
+      },
+    ],
+    'simple-import-sort/exports': 'error',
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx', '.js', '.jsx'],
+      parserOptions: {
+        project: ['./tsconfig.json'],
+        sourceType: 'module',
+      },
+    },
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+}
---
+++ src/main.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+ReactDOM.createRoot(document.getElementById('vines-ui')!).render(<React.StrictMode>Hi</React.StrictMode>);
