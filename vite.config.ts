d24978d54ab958f09e1d8bb3eeba2b8f2fd022aa
LF112 <lf@lf112.net>
2024-01-30 22:55:38 +0800

    feat: 优化配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import react from '@vitejs/plugin-react-swc';
-
-import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
-import million from 'million/compiler';
-import path from 'path';
 import { defineConfig } from 'vite';
+import path from 'path';
+
+import million from 'million/compiler';
+import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
