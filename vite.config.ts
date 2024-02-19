f7e1f8b7c95e0220be38d542b69202419e97e049
LF112 <lf@lf112.net>
2024-02-19 11:28:54 +0800

    chore: 新增 api 重写

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
+import dotenv from 'dotenv';
 
 import svgr from 'vite-plugin-svgr';
 import million from 'million/compiler';
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
+
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -25,5 +28,11 @@ export default defineConfig({
   },
   server: {
     port: 2048,
+    proxy: {
+      '/api': {
+        target: process.env.SERVER_ENDPOINT || 'https://ai.infmonkeys.com',
+        changeOrigin: true,
+      },
+    },
   },
 });
