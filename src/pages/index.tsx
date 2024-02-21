d3f88120e25784ddc00c050ef8b31c0110c55183
LF112 <lf@lf112.net>
2024-02-22 01:34:43 +0800

    revert: 代码回退

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,13 @@ import React from 'react';
 import { createFileRoute } from '@tanstack/react-router';
 
 const App: React.FC = () => {
-  return <h1 className="font-bold text-vines-500">Hello World!</h1>;
+  return (
+    <div className={'flex flex-col gap-8'}>
+      <h1 className="font-bold text-vines-500">Hello World!</h1>
+    </div>
+  );
 };
+
 export const Route = createFileRoute('/')({
   component: App,
 });
