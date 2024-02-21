9f704b7b24fb363096ac6133ed54abc0f5e3a45e
LF112 <lf@lf112.net>
2024-02-21 20:44:04 +0800

    prev: 优化按钮组件

--- src/components/ui/button/index.tsx
+++ src/components/ui/button/index.tsx
@@ -9,7 +9,7 @@ import { Spinner, SpinnerType } from '@/components/ui/spinner';
 import { cn } from '@/utils';
 
 const buttonVariants = cva(
-  'items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+  'vines-button items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
       variant: {
@@ -75,6 +75,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
         className={cn(buttonVariants({ variant, theme, size, className, block }))}
         ref={ref}
         disabled={disabled || loading}
+        data-variant={variant}
         {...props}
       >
         {(loading || loadResult || icon) && (
