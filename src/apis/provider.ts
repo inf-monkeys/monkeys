908eb00ebe4a2947aab31c84ae1e9456ef551664
LF112 <lf@lf112.net>
2024-02-22 12:51:36 +0800

    feat: OEM 配置缓存

---
+++ src/apis/provider.ts
@@ -0,0 +1,40 @@
+import { Cache } from 'swr/_internal';
+
+import stringify from 'fast-json-stable-stringify';
+
+export function localStorageProvider(): Cache<Map<unknown, unknown>> {
+  const whitelist = ['/api/configs'];
+  const expiryTime = 24 * 60 * 60 * 1000;
+  const cacheKey = 'vines-swr';
+
+  let initialMap: never[] = [];
+  let timestamp: number = 0;
+
+  try {
+    const localData = JSON.parse(localStorage.getItem(cacheKey) || 'null');
+    if (Array.isArray(localData) && localData.length === 2) {
+      [timestamp, initialMap] = localData as [number, never[]];
+    }
+  } catch (e) {
+    console.error('Failed to parse local storage data:', e);
+  }
+
+  const map = new Map(initialMap);
+
+  window.addEventListener('beforeunload', () => {
+    const currentTime = new Date().getTime();
+
+    for (const key of map.keys()) {
+      if (!whitelist.includes(key as string)) {
+        map.delete(key);
+      }
+    }
+
+    if (currentTime - timestamp > expiryTime) {
+      const appCache = stringify([currentTime, Array.from(map.entries())]);
+      localStorage.setItem(cacheKey, appCache);
+    }
+  });
+
+  return map as Cache<Map<unknown, unknown>>;
+}
