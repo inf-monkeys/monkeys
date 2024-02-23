55c94e0c2f70904b57a229f863cca2cf3ee8cda3
LF112 <lf@lf112.net>
2024-02-23 16:01:42 +0800

    feat: 完善登录模块

--- src/components/router/auth-guard.ts
+++ src/components/router/auth-guard.ts
@@ -1,7 +1,7 @@
 import { ParsedLocation, redirect } from '@tanstack/react-router';
 
 import { readLocalStorageValue } from '@mantine/hooks';
-import { get, set } from 'lodash';
+import { set } from 'lodash';
 import { decodeToken as jwtDecodeToken, isExpired as jwtIsExpired } from 'react-jwt';
 import { toast } from 'sonner';
 import { parse, stringify } from 'superjson';
@@ -9,13 +9,10 @@ import { parse, stringify } from 'superjson';
 const TOKEN_KEY = 'vines-tokens';
 
 export const isAuthed = () => {
-  const tokens = readLocalStorageValue({
-    key: TOKEN_KEY,
-    defaultValue: {},
-    deserialize: (str) => (str === undefined ? {} : parse(str)),
+  const token = readLocalStorageValue({
+    key: 'vines-token',
+    defaultValue: '',
   });
-
-  const token = get(tokens, `token`, '') as string;
 
   return token && jwtDecodeToken(token) && !jwtIsExpired(token);
 };
@@ -31,11 +28,11 @@ export const authGuard = ({ location }: { location: ParsedLocation }) => {
   }
 };
 
-export const saveAuthToken = (token: string) => {
+export const saveAuthToken = (token: string): number => {
   const decodeToken = jwtDecodeToken(token) as { id: string } | undefined;
   if (!decodeToken) {
     toast.error('登录信息解析失败！');
-    return;
+    return 0;
   }
 
   const localData = readLocalStorageValue({
@@ -45,12 +42,25 @@ export const saveAuthToken = (token: string) => {
   });
 
   const userId = decodeToken.id;
-  set(localData, 'current', userId);
-  set(localData, 'token', token);
   set(localData, `[${userId}].data`, decodeToken);
   set(localData, `[${userId}].token`, token);
+  localStorage.setItem('vines-token', token);
+  localStorage.setItem('vines-user', stringify(decodeToken));
 
   const stringifyData = stringify(localData);
   localStorage.setItem(TOKEN_KEY, stringifyData);
-  window.dispatchEvent(new CustomEvent('mantine-local-storage', { detail: { key: TOKEN_KEY, value: stringifyData } }));
+
+  const userCount = Object.keys(localData).length;
+  if (userCount > 1) {
+    window.dispatchEvent(
+      new CustomEvent('mantine-local-storage', {
+        detail: {
+          key: TOKEN_KEY,
+          value: stringifyData,
+        },
+      }),
+    );
+  }
+
+  return userCount;
 };
--- src/components/ui/smooth-transition-size/SmoothTransition.tsx
+++ src/components/ui/smooth-transition-size/SmoothTransition.tsx
@@ -1,47 +1,21 @@
-import React, { useRef, useState } from 'react';
+import React from 'react';
 
 import { useElementSize } from '@mantine/hooks';
 import { motion } from 'framer-motion';
-
 interface ISmoothTransitionProps extends React.ComponentPropsWithoutRef<'div'> {}
 
 export const SmoothTransition: React.FC<ISmoothTransitionProps> = ({ children }) => {
   const { ref, height } = useElementSize();
 
-  const prevHeight = useRef(0);
-  const [maskHeight, setMaskHeight] = useState(18);
-
   return (
     <motion.div
-      className="overflow-clip p-2"
       initial={{ height: 0, opacity: 0 }}
       animate={{
-        height: height + 16,
+        height: height,
         opacity: 1,
       }}
-      onAnimationStart={() => {
-        height - prevHeight.current > 0 && setMaskHeight(height);
-        setTimeout(() => setMaskHeight(0), 100);
-        prevHeight.current = height;
-      }}
     >
-      <motion.div
-        ref={ref}
-        variants={{
-          visible: {
-            maskImage: `linear-gradient(to bottom,transparent 0, 0, black calc(100% - ${maskHeight}px),transparent 100%)`,
-          },
-          hidden: {
-            maskImage: `linear-gradient(to bottom,transparent 0, 0, black calc(100%),transparent 100%)`,
-            transitionEnd: {
-              maskImage: 'none',
-            },
-          },
-        }}
-        animate={maskHeight > 0 ? 'visible' : 'hidden'}
-      >
-        {children}
-      </motion.div>
+      <div ref={ref}>{children}</div>
     </motion.div>
   );
 };
