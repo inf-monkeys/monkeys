bfe3f1a927690c2329aa5f546a8d60d789136cad
玖叁 <colour_93@furry.top>
2024-02-24 17:05:52 +0800

    feat: sidebar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 
 import { createFileRoute } from '@tanstack/react-router';
 
+import { Nav } from '@/components/layout/home/sidebar.tsx';
 import { authGuard } from '@/components/router/auth-guard';
 
 const App: React.FC = () => {
   return (
-    <div className="flex flex-col gap-8">
-      <h1 className="font-bold text-vines-500">Hello World!</h1>
+    <div className="flex bg-slateA-3">
+      <Nav />
+      <div className="flex w-[calc(100vw-220px)]">
+        <div className="m-4 ml-0 w-full rounded-xl bg-white p-4">
+          <h1 className="font-bold text-vines-500">Hello World!</h1>
+        </div>
+      </div>
     </div>
   );
 };
