5a4f5d3b8c169634bc397abdb98afd9c1fd36023
LF112 <lf@lf112.net>
2024-02-25 13:10:31 +0800

    styles: 优化背景颜色

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,10 +7,10 @@ import { authGuard } from '@/components/router/auth-guard';
 
 const App: React.FC = () => {
   return (
-    <div className="flex w-screen bg-slate-4">
+    <div className="flex w-screen bg-slate-3">
       <Sidebar />
       <div className="flex flex-1">
-        <div className="m-4 ml-0 w-full rounded-xl bg-slate-3 p-4">
+        <div className="m-4 ml-0 w-full rounded-xl bg-slate-1 p-4">
           <h1 className="font-bold text-vines-500">Hello World!</h1>
         </div>
       </div>
