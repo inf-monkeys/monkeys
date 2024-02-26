7f017a2a804cf9b8772c90db74c9438f162c77c0
玖叁 <colour_93@furry.top>
2024-02-26 18:05:50 +0800

    feat: command & dialog

--- src/pages/components/modal.tsx
+++ src/pages/components/modal.tsx
@@ -2,17 +2,18 @@ import React, { useState } from 'react';
 
 import { createFileRoute } from '@tanstack/react-router';
 
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from 'src/components/ui/dialog';
+
 import { TempComponentDemoWrapper } from '@/components/devtools/temp-component-demo-wrapper';
 import { Button } from '@/components/ui/button';
-import {
-  Modal,
-  ModalContent,
-  ModalDescription,
-  ModalFooter,
-  ModalHeader,
-  ModalTitle,
-  ModalTrigger,
-} from '@/components/ui/modal';
 
 export const ModalPage: React.FC = () => {
   const [modalOpen, setModalOpen] = useState(false);
@@ -21,26 +22,26 @@ export const ModalPage: React.FC = () => {
       <a href="https://www.radix-ui.com/primitives/docs/components/dialog#api-reference">API</a>
       <div className="flex gap-3 p-4">
         <TempComponentDemoWrapper title="props type & content type">
-          <Modal>
-            <ModalTrigger asChild>
+          <Dialog>
+            <DialogTrigger asChild>
               <Button>default</Button>
-            </ModalTrigger>
-            <ModalContent className="sm:max-w-[425px]">
-              <ModalHeader>
-                <ModalTitle>Edit profile</ModalTitle>
-                <ModalDescription>Make changes to your profile here. Click save when you're done.</ModalDescription>
-              </ModalHeader>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[425px]">
+              <DialogHeader>
+                <DialogTitle>Edit profile</DialogTitle>
+                <DialogDescription>Make changes to your profile here. Click save when you're done.</DialogDescription>
+              </DialogHeader>
               <div className="grid gap-4 py-4">111</div>
-              <ModalFooter>
+              <DialogFooter>
                 <Button type="submit">Save changes</Button>
-              </ModalFooter>
-            </ModalContent>
-          </Modal>
-          <Modal open={modalOpen} onClose={() => setModalOpen(false)}>
-            <ModalContent className="sm:max-w-[425px]">
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
+          <Dialog open={modalOpen} onClose={() => setModalOpen(false)}>
+            <DialogContent className="sm:max-w-[425px]">
               <div className="grid gap-4 py-4">111</div>
-            </ModalContent>
-          </Modal>
+            </DialogContent>
+          </Dialog>
           <Button onClick={() => setModalOpen(!modalOpen)}>state</Button>
         </TempComponentDemoWrapper>
       </div>
