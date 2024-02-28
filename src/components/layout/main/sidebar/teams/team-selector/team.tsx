95c16aa6b3caa87f0649845cfa42c5648a543dbe
LF112 <lf@lf112.net>
2024-02-28 14:20:45 +0800

    style: 修复文本溢出

--- src/components/layout/main/sidebar/teams/team-selector/team.tsx
+++ src/components/layout/main/sidebar/teams/team-selector/team.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar.tsx';
+import { cn } from '@/utils';
 
 interface ITeamProps extends React.ComponentPropsWithoutRef<'div'> {
   logo?: string;
@@ -8,9 +9,9 @@ interface ITeamProps extends React.ComponentPropsWithoutRef<'div'> {
   description?: string;
 }
 
-export const Team: React.FC<ITeamProps> = ({ name, logo, description }) => {
+export const Team: React.FC<ITeamProps> = ({ className, name, logo }) => {
   return (
-    <div className="flex items-center gap-2">
+    <div className={cn('flex items-center gap-2', className)}>
       <Avatar className="size-5">
         <AvatarImage className="aspect-auto" src={logo} alt={name} />
         <AvatarFallback className="rounded-none p-2 text-xs">团队</AvatarFallback>
