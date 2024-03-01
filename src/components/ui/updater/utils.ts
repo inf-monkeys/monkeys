66224d263a56729bf4012e1f4ecfb840ffe2aaaa
LF112 <lf@lf112.net>
2024-03-01 09:56:12 +0800

    feat: 支持上传

--- src/components/ui/updater/utils.ts
+++ src/components/ui/updater/utils.ts
@@ -1,7 +1,7 @@
 import { customAlphabet } from 'nanoid';
 import SparkMD5 from 'spark-md5';
 
-import { simpleFilePut, simpleGet } from '@/apis/non-fetcher.ts';
+import { simpleFilePut, simpleGet, simplePost } from '@/apis/non-fetcher.ts';
 
 export const coverFileSize = (size: number) => {
   if (size < 1024) {
@@ -64,4 +64,51 @@ export const uploadFile = async (file: File, filename: string, onProgress: (prog
     await simpleFilePut(url, file, onProgress);
     return baseUrl + url;
   }
+
+  const { UploadId } = await simplePost<{ UploadId: string }, Record<string, string>>(
+    '/api/medias/s3/multipart/create',
+    {
+      key: filename,
+      contentType: file.type,
+    },
+  );
+  const chunkSize = 1024 * 1024 * 16;
+  const chunks = Math.ceil(filesize / chunkSize);
+  const parts: { ETag: string; PartNumber: number }[] = [];
+
+  const load = async (current: number) => {
+    const start = current * chunkSize;
+    const end = start + chunkSize >= file.size ? file.size : start + chunkSize;
+    return {
+      data: file.slice(start, end),
+      size: end - start,
+    };
+  };
+
+  for (let i = 0; i < chunks; i++) {
+    const { data, size } = await load(i);
+    const currentProcess = ((i + 1) / chunks) * 100;
+
+    const signedUrl = await simplePost<string, Record<string, string | number>>('/api/medias/s3/multipart/part', {
+      key: filename,
+      uploadId: UploadId,
+      partNumber: i + 1,
+    });
+    const res = await simpleFilePut(signedUrl, <File>data, (process) => {
+      // TODO: 进度条
+    });
+    parts.push({
+      PartNumber: i + 1,
+      ETag: res.getResponseHeader('ETag'),
+    });
+    onProgress?.(currentProcess);
+  }
+
+  await simplePost<string, Record<string, unknown>>('/api/medias/s3/multipart/complete', {
+    key: filename,
+    uploadId: UploadId,
+    parts,
+  });
+
+  return baseUrl + filename;
 };
