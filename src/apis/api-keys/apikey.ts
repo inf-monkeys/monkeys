52ccb57ced0b3e07faabc67cb2966bcae635959e
LF112 <lf@lf112.net>
2024-03-02 23:41:38 +0800

    fix: 修复构建错误

--- src/apis/api-keys/apikey.ts
+++ src/apis/api-keys/apikey.ts
@@ -1,7 +1,7 @@
 import useSWR from 'swr';
 import useSWRMutation from 'swr/mutation';
 
-import { IApiKey } from '@/apis/apikeys/typings.ts';
+import { IApiKey } from '@/apis/api-keys/typings.ts';
 import { useAuthzGetFetcher, useAuthzPostFetcher } from '@/apis/fetcher.ts';
 import { authzPostFetcher } from '@/apis/non-fetcher.ts';
 
