31f3a22ed2dce96f12716e1b260c14675283477f
LF112 <lf@lf112.net>
2024-03-02 20:30:24 +0800

    fix: 修复部分问题

--- src/apis/authz/index.ts
+++ src/apis/authz/index.ts
@@ -2,10 +2,10 @@ import useSWRMutation from 'swr/mutation';
 
 import { IVinesUser } from '@/apis/authz/user/typings.ts';
 import { useAuthzPostFetcher, usePostFetcher } from '@/apis/fetcher.ts';
-import { authzPostFetcher } from '@/apis/non-fetcher.ts';
+import { simplePost } from '@/apis/non-fetcher.ts';
 
 export const sendSmsVerifyCode = (phone: string | number) =>
-  authzPostFetcher('/api/users/verify/phone', { phoneNumber: phone });
+  simplePost('/api/users/verify/phone', { phoneNumber: phone });
 
 export const useLogin = () => useSWRMutation<string>('/api/users/login', usePostFetcher);
 
