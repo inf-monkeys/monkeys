6eaa1837e33f73543f725b46245ab1438cc9565e
LF112 <lf@lf112.net>
2024-03-05 23:36:09 +0800

    marge: 合并更改

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,7 +4,6 @@ import {
 } from '@mantine/hooks';
 import clsx, { ClassValue } from 'clsx';
 import { isString } from 'lodash';
-import moment from 'moment';
 import { parse, stringify } from 'superjson';
 import { twMerge } from 'tailwind-merge';
 
@@ -39,58 +38,4 @@ export const setLocalStorage = <T>(key: string, value: T) => {
   const finalData = isString(value) ? value : stringify(value);
   localStorage.setItem(key, finalData);
   dispatchLocalStorageEvent(key, value);
-};
-
-export const useTimeDiff = () => {
-  const formatTimeDiff = (diffValue: number) => {
-    const duration = moment.duration(diffValue);
-
-    if (duration.asMonths() >= 1) {
-      return `${Math.floor(duration.asMonths())} 月`;
-    } else if (duration.asWeeks() >= 1) {
-      return `${Math.floor(duration.asWeeks())} 周`;
-    } else if (duration.asDays() >= 1) {
-      return `${Math.floor(duration.asDays())} 天`;
-    } else if (duration.asHours() >= 1) {
-      return `${Math.floor(duration.asHours())} 小时`;
-    } else if (duration.asMinutes() >= 1) {
-      return `${Math.floor(duration.asMinutes())} 分钟`;
-    } else {
-      return '数秒';
-    }
-  };
-
-  const formatTimeDiffPrevious = (timestamp: number) => {
-    // 对秒级时间戳特殊处理
-    if (String(timestamp).length === 10) {
-      timestamp *= 1000;
-    }
-    const diff = Date.now() - timestamp;
-    if (diff < 0) return '';
-    const text = formatTimeDiff(diff);
-    return text === '刚刚' ? text : text + '前';
-  };
-
-  const timestampToCST = (timestamp: number | undefined) => {
-    if (!timestamp) return '';
-    const date = new Date(timestamp);
-    return date.toLocaleString('zh-CN', { hour12: false });
-  };
-
-  const formatTimeGap = (timestamp: moment.MomentInput, prevTimestamp: moment.MomentInput) => {
-    const diff = moment.duration(moment(timestamp).valueOf() - moment(prevTimestamp).valueOf());
-    const diffAsSeconds = diff.asSeconds();
-    if (diffAsSeconds < 0) {
-      return '-';
-    }
-    if (diffAsSeconds < 60) {
-      return diff.seconds() + 's';
-    } else if (diff.asMinutes() < 60) {
-      return diff.minutes() + 'm ' + diff.seconds() + 's';
-    } else {
-      return diff.hours() + ':' + diff.minutes() + ':' + diff.seconds();
-    }
-  };
-
-  return { formatTimeDiff, formatTimeDiffPrevious, timestampToCST, formatTimeGap };
 };
---
+++ src/utils/time.ts
@@ -0,0 +1,55 @@
+import moment from 'moment/moment';
+
+export const useTimeDiff = () => {
+  const formatTimeDiff = (diffValue: number) => {
+    const duration = moment.duration(diffValue);
+
+    if (duration.asMonths() >= 1) {
+      return `${Math.floor(duration.asMonths())} 月`;
+    } else if (duration.asWeeks() >= 1) {
+      return `${Math.floor(duration.asWeeks())} 周`;
+    } else if (duration.asDays() >= 1) {
+      return `${Math.floor(duration.asDays())} 天`;
+    } else if (duration.asHours() >= 1) {
+      return `${Math.floor(duration.asHours())} 小时`;
+    } else if (duration.asMinutes() >= 1) {
+      return `${Math.floor(duration.asMinutes())} 分钟`;
+    } else {
+      return '数秒';
+    }
+  };
+
+  const formatTimeDiffPrevious = (timestamp: number) => {
+    // 对秒级时间戳特殊处理
+    if (String(timestamp).length === 10) {
+      timestamp *= 1000;
+    }
+    const diff = Date.now() - timestamp;
+    if (diff < 0) return '';
+    const text = formatTimeDiff(diff);
+    return text === '刚刚' ? text : text + '前';
+  };
+
+  const timestampToCST = (timestamp: number | undefined) => {
+    if (!timestamp) return '';
+    const date = new Date(timestamp);
+    return date.toLocaleString('zh-CN', { hour12: false });
+  };
+
+  const formatTimeGap = (timestamp: moment.MomentInput, prevTimestamp: moment.MomentInput) => {
+    const diff = moment.duration(moment(timestamp).valueOf() - moment(prevTimestamp).valueOf());
+    const diffAsSeconds = diff.asSeconds();
+    if (diffAsSeconds < 0) {
+      return '-';
+    }
+    if (diffAsSeconds < 60) {
+      return diff.seconds() + 's';
+    } else if (diff.asMinutes() < 60) {
+      return diff.minutes() + 'm ' + diff.seconds() + 's';
+    } else {
+      return diff.hours() + ':' + diff.minutes() + ':' + diff.seconds();
+    }
+  };
+
+  return { formatTimeDiff, formatTimeDiffPrevious, timestampToCST, formatTimeGap };
+};
