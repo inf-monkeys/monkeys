f10f69b9ca9f56c9a1640ceac49cefd485e5c3e9
LF112 <lf@lf112.net>
2024-03-05 23:30:59 +0800

    feat: 完善页面标签更改

--- src/components/layout-wrapper/workspace/utils.ts
+++ src/components/layout-wrapper/workspace/utils.ts
@@ -1,6 +1,12 @@
+import { useEffect, useRef } from 'react';
+
 import { useParams } from '@tanstack/react-router';
 
-import { useListWorkspacePages } from '@/apis/pages';
+import stringify from 'fast-json-stable-stringify';
+import { differenceWith, isEqual, omit } from 'lodash';
+
+import { updateWorkspacePages, useListWorkspacePages } from '@/apis/pages';
+import { IPageType } from '@/apis/pages/typings.ts';
 import { useGetWorkflow } from '@/apis/workflow';
 import { usePageStore } from '@/store/usePageStore';
 import { useLocalStorage } from '@/utils';
@@ -14,11 +20,68 @@ export const useVinesPage = () => {
 
   const { data: workflow } = useGetWorkflow(apikey, workflowId);
 
+  const originPagesRef = useRef<IPageType[] | null>(null);
+  const isUpdatePageLocker = useRef(false);
+  const setPages = (pages: IPageType[]) => {
+    void pagesMutate(pages, {
+      revalidate: false,
+    });
+
+    if (isUpdatePageLocker.current) return;
+    if (!originPagesRef.current) {
+      originPagesRef.current = JSON.parse(stringify(pages));
+      return;
+    }
+    isUpdatePageLocker.current = true;
+    const diffPages = differenceWith(pages, originPagesRef.current, isEqual);
+    const omitKeys: { [key: string]: string[] } = {};
+    diffPages.forEach((page) => {
+      const originPage = originPagesRef.current?.find((it) => it._id === page._id);
+      if (originPage) {
+        Object.entries(page).forEach(([key, value]) => {
+          if (isEqual(value, originPage[key as keyof IPageType])) {
+            if (!omitKeys[page._id]) {
+              omitKeys[page._id] = ['updatedTimestamp', key];
+            } else {
+              omitKeys[page._id].push(key);
+            }
+          }
+        });
+      }
+    });
+
+    const finalPages = diffPages
+      .map((it) => ({ pageId: it._id, ...omit(it, omitKeys[it._id]) }))
+      .filter((it) => Object.keys(it).length > 1);
+    originPagesRef.current = JSON.parse(stringify(pages));
+
+    if (!finalPages.length) {
+      setTimeout(() => (isUpdatePageLocker.current = false), 100);
+      return;
+    }
+
+    updateWorkspacePages(apikey, workflowId, finalPages).then((it) => {
+      setTimeout(() => (isUpdatePageLocker.current = false), 100);
+      void pagesMutate(it, {
+        revalidate: false,
+      });
+    });
+
+    setTimeout(() => (isUpdatePageLocker.current = false), 100);
+  };
+
+  useEffect(() => {
+    if (!pages) return;
+    if (originPagesRef.current === null) {
+      originPagesRef.current = JSON.parse(stringify(pages));
+    }
+  }, [pages]);
+
   return {
     workflow,
     workflowId,
     pages,
-    setPages: pagesMutate,
+    setPages,
     page,
     setPage,
     pageId,
