f1da109f17c852c351fea99b04ef5c636571d9ac
LF112 <lf@lf112.net>
2024-03-06 18:54:23 +0800

    feat: 新增图标选择器，完善工作流信息更改

--- src/apis/workflow/index.ts
+++ src/apis/workflow/index.ts
@@ -6,3 +6,17 @@ import { vinesFetcher } from '@/apis/fetcher.ts';
 
 export const useGetWorkflow = (apikey: string, workflowId: string) =>
   useSWR<MonkeyWorkflow>(workflowId ? `/api/workflow/${workflowId}` : null, vinesFetcher({ apikey }));
+
+export const updateWorkflow = (
+  apikey: string,
+  workflowId: string,
+  workflowVersion: number,
+  workflow: Partial<MonkeyWorkflow>,
+) =>
+  vinesFetcher<MonkeyWorkflow, Partial<MonkeyWorkflow>>({ method: 'PUT', simple: true, apikey })(
+    `/api/workflow/${workflowId}`,
+    {
+      ...workflow,
+      version: workflowVersion,
+    },
+  );
--- src/components/layout-wrapper/workspace/header/workflow-info-card.tsx
+++ src/components/layout-wrapper/workspace/header/workflow-info-card.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { Pencil } from 'lucide-react';
 
+import { WorkflowInfoEditor } from '@/components/layout/workspace/workflow/info-editor';
 import { useVinesPage } from '@/components/layout-wrapper/workspace/utils.ts';
 import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip';
 import { VinesIcon } from '@/components/ui/vines-icon';
@@ -13,21 +14,22 @@ export const WorkflowInfoCard: React.FC<IWorkflowInfoCardProps> = () => {
 
   return (
     <Tooltip>
-      <TooltipTrigger asChild>
-        <div className="group flex cursor-pointer items-center gap-2.5">
-          <VinesIcon size="sm">{workflow?.iconUrl}</VinesIcon>
-          <h1 className="font-bold">{workflow?.name}</h1>
+      <WorkflowInfoEditor>
+        <TooltipTrigger asChild>
+          <div className="group flex cursor-pointer items-center gap-2.5">
+            <VinesIcon size="sm">{workflow?.iconUrl}</VinesIcon>
+            <div className="flex flex-col gap-0.5">
+              <h1 className="font-bold leading-tight">{workflow?.name}</h1>
+              {workflow?.description && <span className="text-xxs">{workflow.description}</span>}
+            </div>
 
-          <div className="mt-0.5 opacity-0 transition-opacity group-hover:opacity-70">
-            <Pencil size={12} />
+            <div className="mt-0.5 opacity-0 transition-opacity group-hover:opacity-70">
+              <Pencil size={12} />
+            </div>
           </div>
-        </div>
-      </TooltipTrigger>
-      <TooltipContent>
-        <span className="text-sm font-bold">描述：</span>
-        <br />
-        {workflow?.description || '暂无描述'}
-      </TooltipContent>
+        </TooltipTrigger>
+      </WorkflowInfoEditor>
+      <TooltipContent>点击编辑</TooltipContent>
     </Tooltip>
   );
 };
