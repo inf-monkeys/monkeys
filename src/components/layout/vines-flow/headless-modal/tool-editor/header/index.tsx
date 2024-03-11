a358deb1eaef88e440040fa0c0f597734680e747
LF112 <lf@lf112.net>
2024-03-11 20:33:24 +0800

    feat: 新增编辑时快捷按键

--- src/components/layout/vines-flow/headless-modal/tool-editor/header/index.tsx
+++ src/components/layout/vines-flow/headless-modal/tool-editor/header/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { NodeCustomEditor } from '@/components/layout/vines-flow/headless-modal/tool-editor/header/node-custom-editor.tsx';
+import { QuickOptions } from '@/components/layout/vines-flow/headless-modal/tool-editor/header/quick-options.tsx';
 import { VinesNode } from '@/package/vines-flow/core/nodes';
 
 interface IHeaderProps {
@@ -9,8 +10,9 @@ interface IHeaderProps {
 
 export const Header: React.FC<IHeaderProps> = ({ node }) => {
   return (
-    <header className="flex w-full justify-between">
+    <header className="flex w-full items-center justify-between">
       <NodeCustomEditor node={node} />
+      <QuickOptions nodeId={node?.id} />
     </header>
   );
 };
