42fa9e7db2c940f5ea61272b1bb1f746fb6e9aa0
LF112 <lf@lf112.net>
2024-03-12 22:09:08 +0800

    fix: 修复背景覆盖

--- src/components/layout/vines-flow/wrapper/background.tsx
+++ src/components/layout/vines-flow/wrapper/background.tsx
@@ -1,6 +1,10 @@
 import React, { SVGProps } from 'react';
 
-import { cn } from '@/utils';
+import { useParams } from '@tanstack/react-router';
+
+import { IPinPage } from '@/apis/pages/typings.ts';
+import { useFlowStore } from '@/store/useFlowStore';
+import { cn, useLocalStorage } from '@/utils';
 
 type DotsProps = {
   gap: number;
@@ -17,17 +21,24 @@ export const DotsBackground: React.FC<SVGProps<SVGSVGElement> & DotsProps> = ({
   scale,
   onContextMenu,
 }) => {
+  const { workflowId } = useFlowStore();
+  const { workflowId: routeWorkflowId } = useParams({ from: '/$teamId/workspace/$workflowId/$pageId' });
+
+  const [page] = useLocalStorage<Partial<IPinPage>>('vines-ui-workbench-page', {});
+
   const gapScale = scale || 1;
   const [gapX, gapY] = Array.isArray(gap) ? gap : [gap, gap];
   const [dotWidth, dotHeight] = [gapX * gapScale || 1, gapY * gapScale || 1];
   const radius = (size || 1) / 2;
 
   return (
-    <svg className={cn('absolute left-0 top-0 h-full w-full', className)} onContextMenu={onContextMenu}>
-      <pattern id="vines-pattern" width={dotWidth} height={dotHeight} patternUnits="userSpaceOnUse">
-        <circle className="fill-gold-12" cx={radius} cy={radius} r={radius} />
-      </pattern>
-      <rect x="0" y="0" width="100%" height="100%" fill="url(#vines-pattern)" data-id="vines-drag" />
-    </svg>
+    workflowId === (routeWorkflowId ?? page?.workflowId) && (
+      <svg className={cn('absolute left-0 top-0 h-full w-full', className)} onContextMenu={onContextMenu}>
+        <pattern id="vines-pattern" width={dotWidth} height={dotHeight} patternUnits="userSpaceOnUse">
+          <circle className="fill-gold-12" cx={radius} cy={radius} r={radius} />
+        </pattern>
+        <rect x="0" y="0" width="100%" height="100%" fill="url(#vines-pattern)" data-id="vines-drag" />
+      </svg>
+    )
   );
 };
