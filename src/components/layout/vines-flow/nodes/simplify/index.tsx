9405277eb9ad2b6076b953e8eb984bc029b189df
LF112 <lf@lf112.net>
2024-03-12 10:03:04 +0800

    pref: 优化工具增删改过度

--- src/components/layout/vines-flow/nodes/simplify/index.tsx
+++ src/components/layout/vines-flow/nodes/simplify/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { motion } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 
 import { NodeDragOverlay } from '@/components/layout/vines-flow/nodes/simplify/drag-overlay.tsx';
 import { SimplifyNode } from '@/components/layout/vines-flow/nodes/simplify/node';
@@ -17,37 +17,27 @@ interface ISimplifyNodesProps {
 export const SimplifyNodes: React.FC<ISimplifyNodesProps> = ({ visible, nodes, nodeStagger }) => {
   return (
     <SimplifyWrapper>
-      {visible && (
-        <motion.div
-          variants={{
-            hidden: { opacity: 1 },
-            visible: {
-              opacity: 1,
-              transition: {
-                staggerChildren: nodeStagger,
-              },
-            },
-          }}
-          initial="hidden"
-          animate="visible"
-        >
-          {nodes.map((node, index) => (
-            <motion.div
-              key={index}
-              variants={{
-                hidden: { opacity: 0, y: -20 },
-                visible: {
-                  opacity: 1,
-                  y: 0,
-                },
-              }}
-            >
-              <SimplifyNode node={node} />
-              <NodeDragOverlay nodes={nodes}>{(_node) => <VinesSimpleNode node={_node} />}</NodeDragOverlay>
-            </motion.div>
-          ))}
-        </motion.div>
-      )}
+      <AnimatePresence>
+        {visible && (
+          <motion.div
+            key="vines-nodes-wrapper"
+            variants={{
+              hidden: { opacity: 1, transition: { duration: 0.1 } },
+              visible: { opacity: 1, transition: { duration: 0.1, staggerChildren: nodeStagger } },
+            }}
+            initial="hidden"
+            animate="visible"
+            exit="hidden"
+          >
+            {nodes.map((node, index) => (
+              <motion.div key={index} variants={{ hidden: { opacity: 0 }, visible: { opacity: 1 } }}>
+                <SimplifyNode node={node} />
+                <NodeDragOverlay nodes={nodes}>{(_node) => <VinesSimpleNode node={_node} />}</NodeDragOverlay>
+              </motion.div>
+            ))}
+          </motion.div>
+        )}
+      </AnimatePresence>
     </SimplifyWrapper>
   );
 };
