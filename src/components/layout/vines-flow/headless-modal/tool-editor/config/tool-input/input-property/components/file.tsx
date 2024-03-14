dd1f68d4cd71e337505fbc85eabf6ee275e6506e
LF112 <lf@lf112.net>
2024-03-14 22:14:09 +0800

    todo: 上传输入

---
+++ src/components/layout/vines-flow/headless-modal/tool-editor/config/tool-input/input-property/components/file.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { IVinesInputPropertyProps } from '@/components/layout/vines-flow/headless-modal/tool-editor/config/tool-input/input-property';
+
+export const FileInput: React.FC<IVinesInputPropertyProps> = ({ def, value, onChange, disabled }) => {
+  return <>暂不支持</>;
+};
--- src/components/layout/vines-flow/headless-modal/tool-editor/config/tool-input/input-property/index.tsx
+++ src/components/layout/vines-flow/headless-modal/tool-editor/config/tool-input/input-property/index.tsx
@@ -6,6 +6,7 @@ import { debounce, get, isEmpty, isNumber, isString } from 'lodash';
 import { BlankInput } from '@/components/layout/vines-flow/headless-modal/tool-editor/config/tool-input/input-property/components/blank.tsx';
 import { BooleanInput } from '@/components/layout/vines-flow/headless-modal/tool-editor/config/tool-input/input-property/components/boolean.tsx';
 import { EditorInput } from '@/components/layout/vines-flow/headless-modal/tool-editor/config/tool-input/input-property/components/editor.tsx';
+import { FileInput } from '@/components/layout/vines-flow/headless-modal/tool-editor/config/tool-input/input-property/components/file.tsx';
 import { NumberInput } from '@/components/layout/vines-flow/headless-modal/tool-editor/config/tool-input/input-property/components/number.tsx';
 import { OptionsInput } from '@/components/layout/vines-flow/headless-modal/tool-editor/config/tool-input/input-property/components/options.tsx';
 import { StringInput } from '@/components/layout/vines-flow/headless-modal/tool-editor/config/tool-input/input-property/components/string.tsx';
@@ -166,6 +167,7 @@ export const VinesInputProperty: React.FC<IVinesInputPropertyProps> = (props) =>
       {hasBooleanInput && <BooleanInput {...finalProps} />}
       {hasNumberInput && <NumberInput {...finalProps} />}
       {hasOptionsInput && <OptionsInput {...finalProps} />}
+      {hasFileInput && <FileInput {...finalProps} />}
 
       {isBlankInput && <BlankInput {...finalProps} />}
     </InputPropertyWrapper>
