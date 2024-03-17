9af9b93b10ec7d3e4307f480c5ececc9c7bc5655
LF112 <lf@lf112.net>
2024-03-17 09:20:13 +0800

    feat: 支持创建手动触发触发器

--- src/components/layout/vines-flow/headless-modal/endpoint/start-tool/workflow-input-config/triggers/trigger-selector/index.tsx
+++ src/components/layout/vines-flow/headless-modal/endpoint/start-tool/workflow-input-config/triggers/trigger-selector/index.tsx
@@ -1,10 +1,16 @@
 import React, { useEffect, useState } from 'react';
 
-import { useTriggerTypes } from '@/apis/workflow/trigger';
+import { useSWRConfig } from 'swr';
+
+import { toast } from 'sonner';
+
+import { useTriggerCreate, useTriggerTypes } from '@/apis/workflow/trigger';
+import { ITriggerType, WorkflowTriggerType } from '@/apis/workflow/trigger/typings.ts';
 import { Card } from '@/components/ui/card.tsx';
 import { Dialog, DialogContent, DialogDescription, DialogTitle } from '@/components/ui/dialog';
 import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip';
 import { VinesIcon } from '@/components/ui/vines-icon';
+import { useVinesFlow } from '@/package/vines-flow';
 import { useFlowStore } from '@/store/useFlowStore';
 import VinesEvent from '@/utils/events.ts';
 
@@ -13,7 +19,12 @@ interface ITriggerSelectorProps {}
 export const TriggerSelector: React.FC<ITriggerSelectorProps> = () => {
   const { workflowId } = useFlowStore();
 
+  const { mutate } = useSWRConfig();
+
+  const { vines } = useVinesFlow();
+
   const { data: triggerTypes } = useTriggerTypes();
+  const { trigger: createTrigger } = useTriggerCreate(workflowId);
 
   const [open, setOpen] = useState(false);
 
@@ -28,8 +39,22 @@ export const TriggerSelector: React.FC<ITriggerSelectorProps> = () => {
     };
   }, []);
 
-  const handleCreateTrigger = (type: string) => {
-    console.log('create trigger', type);
+  const workflowVersion = vines.version;
+
+  const handleCreateTrigger = (triggerType: ITriggerType) => {
+    const type = triggerType.type;
+
+    if (type === WorkflowTriggerType.MANUALLY) {
+      toast.promise(createTrigger({ triggerType: type, enabled: true, version: workflowVersion }), {
+        loading: '正在创建触发器',
+        success: () => {
+          void mutate(`/api/workflow/${workflowId}/triggers?version=${workflowVersion}`);
+          return '触发器创建成功';
+        },
+        error: '触发器创建失败',
+      });
+    }
+
     setOpen(false);
   };
 
@@ -39,35 +64,38 @@ export const TriggerSelector: React.FC<ITriggerSelectorProps> = () => {
         <DialogTitle>选择触发器</DialogTitle>
         <DialogDescription>选择一个触发器，通过触发事件或定时任务来触发工作流的运行</DialogDescription>
         <div className="grid w-full grid-cols-2 gap-4">
-          {triggerTypes?.map(({ type, icon, displayName, description }, i) => (
-            <Card
-              className="flex cursor-pointer items-center gap-4 p-4 hover:bg-gray-2 dark:hover:bg-gray-4"
-              onClick={() => handleCreateTrigger(type)}
-              key={i}
-            >
-              <div className="relative flex h-12 w-12 flex-shrink-0 items-end justify-end overflow-clip rounded-lg border-input shadow-md">
-                <div className="absolute">
-                  <VinesIcon className="size-full" size="lg" src={icon} />
+          {triggerTypes?.map((triggerType, i) => {
+            const { displayName, description, icon } = triggerType;
+            return (
+              <Card
+                className="flex cursor-pointer items-center gap-4 p-4 hover:bg-gray-2 dark:hover:bg-gray-4"
+                onClick={() => handleCreateTrigger(triggerType)}
+                key={i}
+              >
+                <div className="relative flex h-12 w-12 flex-shrink-0 items-end justify-end overflow-clip rounded-lg border-input shadow-md">
+                  <div className="absolute">
+                    <VinesIcon className="size-full" size="lg" src={icon} />
+                  </div>
                 </div>
-              </div>
-              <div className="leading-5">
-                <Tooltip>
-                  <TooltipTrigger asChild>
-                    <div className="line-clamp-1 select-none font-bold">{displayName}</div>
-                  </TooltipTrigger>
-                  <TooltipContent>{displayName}</TooltipContent>
-                </Tooltip>
-                <Tooltip>
-                  <TooltipTrigger asChild>
-                    <div className="mt-1 line-clamp-1 select-none text-xs opacity-50">{description}</div>
-                  </TooltipTrigger>
-                  <TooltipContent side="bottom" className="max-w-64">
-                    {description}
-                  </TooltipContent>
-                </Tooltip>
-              </div>
-            </Card>
-          ))}
+                <div className="leading-5">
+                  <Tooltip>
+                    <TooltipTrigger asChild>
+                      <div className="line-clamp-1 select-none font-bold">{displayName}</div>
+                    </TooltipTrigger>
+                    <TooltipContent>{displayName}</TooltipContent>
+                  </Tooltip>
+                  <Tooltip>
+                    <TooltipTrigger asChild>
+                      <div className="mt-1 line-clamp-1 select-none text-xs opacity-50">{description}</div>
+                    </TooltipTrigger>
+                    <TooltipContent side="bottom" className="max-w-64">
+                      {description}
+                    </TooltipContent>
+                  </Tooltip>
+                </div>
+              </Card>
+            );
+          })}
         </div>
       </DialogContent>
     </Dialog>
