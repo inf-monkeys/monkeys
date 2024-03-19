3fc565ce69ea4b632ae9ae5bebbe18a43a6ce3ca
LF112 <lf@lf112.net>
2024-03-19 09:27:29 +0800

    draft: 初步构建全参数模式触发器节点

--- src/components/layout/vines-flow/nodes/complicate/node/index.tsx
+++ src/components/layout/vines-flow/nodes/complicate/node/index.tsx
@@ -1,7 +1,10 @@
 import React, { useRef, useState } from 'react';
 
 import { useClickOutside } from '@mantine/hooks';
+import { CircularProgress } from '@nextui-org/progress';
+import { motion } from 'framer-motion';
 
+import { ComplicateTriggerNode } from '@/components/layout/vines-flow/nodes/complicate/node/endpoint/trigger.tsx';
 import { Card } from '@/components/ui/card.tsx';
 import { useVinesFlow } from '@/package/vines-flow';
 import { VinesNode } from '@/package/vines-flow/core/nodes';
@@ -20,6 +23,7 @@ export const ComplicateNode: React.FC<IComplicateNodeProps> = ({ node, index }) 
     id: nodeId,
     position: { x: nodeX, y: nodeY },
     size: { width, height },
+    customData,
   } = node;
   const { name: toolName } = node.getRaw();
 
@@ -51,6 +55,10 @@ export const ComplicateNode: React.FC<IComplicateNodeProps> = ({ node, index }) 
   };
 
   const tool = vines.getTool(toolName);
+  const isStartNode = nodeId === 'workflow_start';
+  const isEndNode = nodeId === 'workflow_end';
+  const isFakeNode = nodeId.startsWith('fake_node');
+  const isSimpleNode = !isStartNode && !isEndNode && !isFakeNode;
 
   return (
     <div
@@ -65,7 +73,40 @@ export const ComplicateNode: React.FC<IComplicateNodeProps> = ({ node, index }) 
         onClick={handleNodeClick}
         onMouseEnter={() => (mouseFocusRef.current = true)}
         onMouseLeave={() => (mouseFocusRef.current = false)}
-      ></Card>
+      >
+        {!tool && isSimpleNode ? (
+          <motion.div
+            className="flex h-full w-full items-center justify-center"
+            key={'vines-complicate-loading-' + nodeId}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <CircularProgress className="[&_circle:last-child]:stroke-vines-500" size="lg" aria-label="Loading..." />
+          </motion.div>
+        ) : (
+          <>
+            <motion.div
+              className="h-full w-full overflow-clip"
+              key={'vines-complicate-' + nodeId}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+            >
+              {isStartNode ? (
+                <ComplicateTriggerNode key="vines-complicate-trigger" />
+              ) : isEndNode ? (
+                <div key="vines-complicate-end-node" />
+              ) : isFakeNode ? (
+                <div />
+              ) : (
+                <div key={'vines-complicate-' + nodeId} />
+              )}
+            </motion.div>
+            {isSimpleNode && <div />}
+          </>
+        )}
+      </Card>
     </div>
   );
 };
