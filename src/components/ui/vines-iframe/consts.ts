b627b277ab6cf401ce8c4c3759cf79edd4f9a8d3
LF112 <lf@lf112.net>
2024-03-25 10:51:50 +0800

    refactor: 规范文件结构

--- src/components/ui/vines-iframe/consts.ts
+++ src/components/ui/vines-iframe/consts.ts
@@ -1,14 +1,15 @@
+import { VinesChatView } from 'src/view/vines-chat';
+import { VinesLogView } from 'src/view/vines-log';
+
 import { VinesFlow } from '@/components/layout/vines-flow';
 import { withProviders } from '@/components/ui/vines-iframe/utils.ts';
-import { VinesChatPage } from '@/pages/$teamId/workspace/$workflowId/vines-chat';
-import { VinesLogPage } from '@/pages/$teamId/workspace/$workflowId/vines-log';
-import { VinesPreviewPage } from '@/pages/$teamId/workspace/$workflowId/vines-preview';
 import { CanvasStoreProvider, createCanvasStore } from '@/store/useCanvasStore';
 import { createFlowStore, FlowStoreProvider } from '@/store/useFlowStore';
+import { VinesPreView } from '@/view/vines-preview';
 
 export const IFRAME_MAP = {
   process: withProviders(VinesFlow, [FlowStoreProvider, createFlowStore], [CanvasStoreProvider, createCanvasStore]),
-  log: VinesLogPage,
-  preview: VinesPreviewPage,
-  chat: VinesChatPage,
+  log: VinesLogView,
+  preview: VinesPreView,
+  chat: VinesChatView,
 };
