067b5e3d61ede6d12c2311104c0f6035470561ec
LF112 <lf@lf112.net>
2024-03-25 19:06:33 +0800

    feat: 优化 VinesFlow 加载逻辑

--- src/components/ui/vines-iframe/consts.ts
+++ src/components/ui/vines-iframe/consts.ts
@@ -1,14 +1,13 @@
 import { VinesChatView } from 'src/view/vines-chat';
 import { VinesLogView } from 'src/view/vines-log';
 
-import { VinesFlow } from '@/components/layout/vines-flow';
 import { withProviders } from '@/components/ui/vines-iframe/utils.ts';
 import { CanvasStoreProvider, createCanvasStore } from '@/store/useCanvasStore';
-import { createFlowStore, FlowStoreProvider } from '@/store/useFlowStore';
+import { VinesFlow } from '@/view/vines-flow';
 import { VinesPreView } from '@/view/vines-preview';
 
 export const IFRAME_MAP = {
-  process: withProviders(VinesFlow, [FlowStoreProvider, createFlowStore], [CanvasStoreProvider, createCanvasStore]),
+  process: withProviders(VinesFlow, [CanvasStoreProvider, createCanvasStore]),
   log: VinesLogView,
   preview: VinesPreView,
   chat: VinesChatView,
