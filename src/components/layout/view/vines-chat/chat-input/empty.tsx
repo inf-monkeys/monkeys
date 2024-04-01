feed379792aebd0d8ca2b91eed6e534d509bfbc5
LF112 <lf@lf112.net>
2024-04-01 20:35:43 +0800

    fix: 修复无法直接运行工作流

--- src/components/layout/view/vines-chat/chat-input/empty.tsx
+++ src/components/layout/view/vines-chat/chat-input/empty.tsx
@@ -11,13 +11,13 @@ interface IEmptyInputProps {
 
 export const EmptyInput: React.FC<IEmptyInputProps> = ({ disabled, onClick }) => {
   return (
-    <>
+    <div className="flex">
       <div className="vines-center flex-1">
         <p className="text-sm text-gray-10">暂未配置输入，可直接运行</p>
       </div>
-      <Button disabled={disabled} variant="outline" icon={<Play />} onClick={onClick}>
+      <Button disabled={disabled} variant="outline" icon={<Play />} onClick={() => onClick()}>
         运行
       </Button>
-    </>
+    </div>
   );
 };
