81e98df7ed95e857c1046c7073bf0e676e53399b
玖叁 <colour_93@furry.top>
2024-04-01 09:45:43 +0800

    perf: query-string 更换 qs

--- src/apis/model/index.ts
+++ src/apis/model/index.ts
@@ -1,11 +1,11 @@
 import useSWR from 'swr';
 
-import queryString from 'query-string';
+import qs from 'qs';
 
 import { vinesFetcher } from '@/apis/fetcher.ts';
 import { ISDModel, ISDModelsParams } from '@/apis/model/typings.ts';
 
 export const useSDModels = (query: ISDModelsParams) =>
-  useSWR<ISDModel[] | undefined>(`/api/sd/models?${queryString.stringify(query)}`, vinesFetcher());
+  useSWR<ISDModel[] | undefined>(`/api/sd/models?${qs.stringify(query)}`, vinesFetcher());
 
 export const useBuiltInModels = () => useSWR<ISDModel[] | undefined>('/api/sd/models/built-in', vinesFetcher());
