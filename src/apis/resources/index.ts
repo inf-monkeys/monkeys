4688074f4185f3b9983465d2a689bc10c7318bec
codevoyager1984 <codevoyager.jay@gmail.com>
2024-04-03 12:51:08 +0700

    feat: Media API

--- src/apis/resources/index.ts
+++ src/apis/resources/index.ts
@@ -5,8 +5,8 @@ export const getResourceByMd5 = (md5: string) =>
   vinesFetcher<{ data: IMd5Response }>({
     method: 'GET',
     responseResolver: (res) => res.json() as unknown as { data: IMd5Response },
-  })('/api/resources/md5/' + md5);
+  })('/api/medias/md5/' + md5);
 
 export const createResource = (
   parma: Pick<IVinesResource, 'type' | 'name' | 'url' | 'source' | 'tags' | 'categoryIds' | 'params' | 'size' | 'md5'>,
-) => vinesFetcher<IVinesResource>({ method: 'POST', simple: true })('/api/resources', parma);
+) => vinesFetcher<IVinesResource>({ method: 'POST', simple: true })('/api/medias', parma);
