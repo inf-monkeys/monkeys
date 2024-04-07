238a21a113a7ddf8938055d80fe48f90bee0ba23
LF112 <lf@lf112.net>
2024-04-07 22:05:32 +0800

    feat: 支持更换系统图标

--- src/components/layout/settings/theme/index.tsx
+++ src/components/layout/settings/theme/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { TeamLogo } from '@/components/layout/settings/theme/team-logo';
 import { TeamPrimaryColor } from '@/components/layout/settings/theme/team-primary-color';
 
 interface IVinesThemeProps extends React.ComponentPropsWithoutRef<'div'> {}
@@ -9,6 +10,7 @@ export const VinesTheme: React.FC<IVinesThemeProps> = () => {
     <div className="grid grid-cols-[1fr_2fr] items-start justify-center gap-4">
       <div className="grid items-start gap-4">
         <TeamPrimaryColor />
+        <TeamLogo />
       </div>
       <div className="grid items-start gap-4"></div>
     </div>
---
+++ src/components/layout/settings/theme/team-logo/index.tsx
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from 'react';
+
+import { useSWRConfig } from 'swr';
+
+import { get, set } from 'lodash';
+import { toast } from 'sonner';
+
+import { updateTeam } from '@/apis/authz/team';
+import { useVinesTeam } from '@/components/router/guard/team.tsx';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar.tsx';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card.tsx';
+import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs.tsx';
+
+interface ITeamLogoProps extends React.ComponentPropsWithoutRef<'div'> {}
+
+export const TeamLogo: React.FC<ITeamLogoProps> = () => {
+  const { mutate } = useSWRConfig();
+  const { team } = useVinesTeam();
+
+  const [selected, setSelected] = useState<string>('');
+
+  const teamName = team?.name || '团队';
+  const enableTeamLogo = get(team, 'customTheme.enableTeamLogo', void 0);
+
+  useEffect(() => {
+    if (!selected && enableTeamLogo !== void 0) {
+      setSelected(enableTeamLogo ? 'team' : 'system');
+    }
+  }, [enableTeamLogo]);
+
+  const handleUpdate = (val: string) => {
+    setSelected(val);
+    if (!team) {
+      toast.error('团队不存在');
+      return;
+    }
+    set(team, 'customTheme.enableTeamLogo', val === 'team');
+    toast.promise(updateTeam({ customTheme: team.customTheme }), {
+      loading: '正在更新',
+      success: () => {
+        void mutate('/api/teams');
+        return '更新成功';
+      },
+      error: '更新失败',
+    });
+  };
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>系统图标</CardTitle>
+        <CardDescription>你可以在这里设置团队的系统图标</CardDescription>
+      </CardHeader>
+      <CardContent className="flex items-center justify-between">
+        <Avatar className="size-10">
+          <AvatarImage className="aspect-auto" src={team?.logoUrl} alt={teamName} />
+          <AvatarFallback className="rounded-none p-2 text-xs">{teamName.substring(0, 2)}</AvatarFallback>
+        </Avatar>
+        <Tabs value={selected} onValueChange={handleUpdate}>
+          <TabsList>
+            <TabsTrigger value="team">团队图标</TabsTrigger>
+            <TabsTrigger value="system">系统预置</TabsTrigger>
+          </TabsList>
+        </Tabs>
+      </CardContent>
+    </Card>
+  );
+};
