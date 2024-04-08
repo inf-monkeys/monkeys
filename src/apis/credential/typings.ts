65f9aa3433bf52d33cdf4989a25befea988d31f5
LF112 <lf@lf112.net>
2024-04-08 23:18:45 +0800

    feat: 初步支持管理外部账号

---
+++ src/apis/credential/typings.ts
@@ -0,0 +1,25 @@
+import { BlockDefProperties } from '@inf-monkeys/vines/src/models/BlockDefDto.ts';
+
+export enum CredentialAuthType {
+  AKSK = 'AKSK',
+  OAUTH2 = 'OAUTH2',
+}
+
+export interface IVinesCredentialType {
+  _id?: string;
+  public: boolean;
+  name: string;
+  displayName: string;
+  logo?: string;
+  properties: BlockDefProperties[];
+  tokenScript: string;
+  testConnectionScript?: string;
+  type: CredentialAuthType;
+}
+
+export interface IVinesCredentialDetail {
+  _id?: string;
+  type: string;
+  displayName: string;
+  data: { [x: string]: any };
+}
