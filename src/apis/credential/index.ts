8a91a09b7416052498009133c10faa329332a90b
LF112 <lf@lf112.net>
2024-04-09 22:51:10 +0800

    feat: 支持创建账号

--- src/apis/credential/index.ts
+++ src/apis/credential/index.ts
@@ -1,4 +1,5 @@
 import useSWR from 'swr';
+import useSWRMutation from 'swr/mutation';
 
 import qs from 'qs';
 
@@ -13,3 +14,17 @@ export const useCredentials = (credentialType?: string) =>
     `/api/credentials${credentialType ? `?${qs.stringify({ credentialType })}` : ''}`,
     vinesFetcher(),
   );
+
+export const deleteCredential = (id: string) => vinesFetcher({ method: 'DELETE' })(`/api/credentials/${id}`);
+
+export const useCreateCredential = () =>
+  useSWRMutation<
+    IVinesCredentialDetail | undefined,
+    unknown,
+    string,
+    {
+      displayName: string;
+      type: string;
+      data: any;
+    }
+  >('/api/credentials', vinesFetcher({ method: 'POST' }));
