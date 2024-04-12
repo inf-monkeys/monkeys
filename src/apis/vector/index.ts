f45d5610f583d29cfd2da3e455c3bacb6f856763
LF112 <lf@lf112.net>
2024-04-12 09:51:59 +0800

    merge: main

--- src/apis/vector/index.ts
+++ src/apis/vector/index.ts
@@ -7,8 +7,8 @@ import {
   ICreateVectorDB,
   IFullTextSearchParams,
   IFullTextSearchResult,
-  IVectorCollection,
-  IVectorFrontEnd,
+  IKnowledgeBase,
+  IKnowledgeBaseFrontEnd,
   IVectorSupportedEmbeddingModel,
 } from '@/apis/vector/typings.ts';
 
