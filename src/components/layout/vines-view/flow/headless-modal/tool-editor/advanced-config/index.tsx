5914d2bc6c48aeee8ec9ea01905046769d9fef99
LF112 <lf@lf112.net>
2024-04-15 19:24:55 +0800

    feat: 支持工具配置高级配置

--- src/components/layout/vines-view/flow/headless-modal/tool-editor/advanced-config/index.tsx
+++ src/components/layout/vines-view/flow/headless-modal/tool-editor/advanced-config/index.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 
-import { zodResolver } from '@hookform/resolvers/zod';
-import { NumberInput } from '@mantine/core';
-import { useForm } from 'react-hook-form';
+import { debounce, get, set } from 'lodash';
+import { toast } from 'sonner';
 
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label.tsx';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select.tsx';
 import { VinesTask } from '@/package/vines-flow/core/nodes/typings.ts';
 import { useVinesFlow } from '@/package/vines-flow/use.ts';
-import { IToolsAdvancedConfig, toolsAdvancedConfigSchema } from '@/schema/tools/tools-advanced-config';
+import { cloneDeep } from '@/utils';
 
 interface INodeConfigProps {
   nodeId: string;
@@ -18,62 +18,75 @@ interface INodeConfigProps {
 export const ToolAdvancedConfig: React.FC<INodeConfigProps> = ({ nodeId, task }) => {
   const { vines } = useVinesFlow();
 
-  const toolName = task?.name ?? '';
+  const taskRef = useRef<VinesTask | null>(null);
 
-  const tool = vines.getTool(toolName);
-  const form = useForm<IToolsAdvancedConfig>({
-    resolver: zodResolver(toolsAdvancedConfigSchema),
-    defaultValues: {
-      outputAs: 'json',
-      timeout: 3600,
-    },
-  });
+  const handleUpdate = useCallback(
+    debounce((key: string, value: unknown) => {
+      if (!taskRef.current) {
+        toast.error('工具数据异常！');
+        return;
+      }
+      set(taskRef.current, `inputParameters.__advancedConfig.${key}`, value);
+      vines.updateRaw(nodeId, taskRef.current, false);
+    }, 300),
+    [nodeId, vines],
+  );
+
+  const [outputAsSelect, setOutputAsSelect] = useState<string | undefined>();
+  const [timeoutInput, setTimeoutInput] = useState<string | undefined>();
+
+  useEffect(() => {
+    const newTask = cloneDeep(task);
+    if (!newTask) {
+      toast.error('工具数据解析失败！');
+      return;
+    }
+    taskRef.current = newTask;
+
+    setOutputAsSelect(get(taskRef.current, 'inputParameters.__advancedConfig.outputAs'));
+    setTimeoutInput(get(taskRef.current, 'inputParameters.__advancedConfig.timeout'));
+  }, [nodeId]);
 
   return (
-    <main className="flex size-full overflow-clip">
-      <Form {...form}>
-        <form className="flex flex-col gap-2">
-          <FormField
-            name="outputAs"
-            control={form.control}
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>输出模式</FormLabel>
-                <FormControl>
-                  <Select onValueChange={field.onChange} defaultValue={field.value}>
-                    <FormControl>
-                      <SelectTrigger>
-                        <SelectValue placeholder="请选择一个输出模式" />
-                      </SelectTrigger>
-                    </FormControl>
-                    <SelectContent>
-                      {['json', 'stream'].map((it) => (
-                        <SelectItem value={it} key={it}>
-                          {it}
-                        </SelectItem>
-                      ))}
-                    </SelectContent>
-                  </Select>
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            name="timeout"
-            control={form.control}
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>超时时间（秒）</FormLabel>
-                <FormControl>
-                  <NumberInput placeholder="超时时间（秒）" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-        </form>
-      </Form>
+    <main className="flex size-full flex-col gap-4 overflow-clip px-4">
+      <Label>输出模式</Label>
+      <Select
+        value={outputAsSelect}
+        onValueChange={(val) => {
+          setOutputAsSelect(val);
+          handleUpdate('outputAs', val);
+        }}
+      >
+        <SelectTrigger>
+          <SelectValue placeholder="请选择一个输出模式" />
+        </SelectTrigger>
+        <SelectContent>
+          {['json', 'stream'].map((it) => (
+            <SelectItem value={it} key={it} className="cursor-pointer">
+              {it}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+      <Label>超时时间（秒）</Label>
+      <Input
+        placeholder="请输入超时时间（秒）"
+        type="number"
+        value={timeoutInput}
+        onChange={(val) => {
+          const number = Number(val);
+          if (isNaN(number)) {
+            toast.error('请输入数字');
+            return;
+          }
+          if (number < 0) {
+            toast.error('请输入大于等于 0 的数字');
+            return;
+          }
+          setTimeoutInput(val);
+          handleUpdate('timeout', number);
+        }}
+      />
     </main>
   );
 };
