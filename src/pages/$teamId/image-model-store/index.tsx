84e6213bfc4c89b499211a7fd370f3975d341778
玖叁 <colour_93@furry.top>
2024-04-17 02:38:12 +0800

    feat: 操作区域基础

--- src/pages/$teamId/image-model-store/index.tsx
+++ src/pages/$teamId/image-model-store/index.tsx
@@ -2,11 +2,24 @@ import React from 'react';
 
 import { createFileRoute, useNavigate } from '@tanstack/react-router';
 
+import { FileDown } from 'lucide-react';
+
 import { preloadUgcImageModelStore, useUgcImageModelStore } from '@/apis/ugc';
 import { UgcView } from '@/components/layout/ugc/view';
 import { RenderIcon } from '@/components/layout/ugc/view/utils/renderer.tsx';
 import { createImageModelStoreColumns } from '@/components/layout/ugc-pages/image-model-store/consts.tsx';
 import { teamIdGuard } from '@/components/router/guard/team-id.ts';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu.tsx';
+import { Tooltip, TooltipTrigger } from '@/components/ui/tooltip';
 import { formatTimeDiffPrevious } from '@/utils/time.ts';
 
 export const ImageModelStore: React.FC = () => {
@@ -34,6 +47,37 @@ export const ImageModelStore: React.FC = () => {
             return RenderIcon({ iconUrl: item.iconUrl, size: 'gallery' });
           },
         }}
+        operateArea={(item, trigger, tooltipTriggerContent) => (
+          <DropdownMenu>
+            {tooltipTriggerContent ? (
+              <Tooltip content={tooltipTriggerContent}>
+                <TooltipTrigger asChild>
+                  <DropdownMenuTrigger asChild>{trigger}</DropdownMenuTrigger>
+                </TooltipTrigger>
+              </Tooltip>
+            ) : (
+              <DropdownMenuTrigger asChild>{trigger}</DropdownMenuTrigger>
+            )}
+
+            <DropdownMenuContent
+              onClick={(e) => {
+                e.stopPropagation();
+                e.preventDefault();
+              }}
+            >
+              <DropdownMenuLabel>图像模型市场操作</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuGroup>
+                <DropdownMenuItem onSelect={() => {}}>
+                  <DropdownMenuShortcut className="ml-0 mr-2 mt-0.5">
+                    <FileDown size={15} />
+                  </DropdownMenuShortcut>
+                  导入该团队
+                </DropdownMenuItem>
+              </DropdownMenuGroup>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        )}
         onItemClick={(item) => {
           // void navigate({
           //   to: `/$teamId/action-tools/${item.name}`,
--- src/pages/$teamId/table-data/index.tsx
+++ src/pages/$teamId/table-data/index.tsx
@@ -2,11 +2,24 @@ import React from 'react';
 
 import { createFileRoute } from '@tanstack/react-router';
 
+import { FileUp, Trash } from 'lucide-react';
+
 import { preloadUgcTableData, useUgcTableData } from '@/apis/ugc';
 import { UgcView } from '@/components/layout/ugc/view';
 import { RenderIcon } from '@/components/layout/ugc/view/utils/renderer.tsx';
 import { createTableDataColumns } from '@/components/layout/ugc-pages/table-data/consts.tsx';
 import { teamIdGuard } from '@/components/router/guard/team-id.ts';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu.tsx';
+import { Tooltip, TooltipTrigger } from '@/components/ui/tooltip';
 import { formatTimeDiffPrevious } from '@/utils/time.ts';
 
 export const TableData: React.FC = () => {
@@ -29,6 +42,44 @@ export const TableData: React.FC = () => {
           ),
           cover: (item) => RenderIcon({ iconUrl: item.iconUrl, size: 'gallery' }),
         }}
+        operateArea={(item, trigger, tooltipTriggerContent) => (
+          <DropdownMenu>
+            {tooltipTriggerContent ? (
+              <Tooltip content={tooltipTriggerContent}>
+                <TooltipTrigger asChild>
+                  <DropdownMenuTrigger asChild>{trigger}</DropdownMenuTrigger>
+                </TooltipTrigger>
+              </Tooltip>
+            ) : (
+              <DropdownMenuTrigger asChild>{trigger}</DropdownMenuTrigger>
+            )}
+
+            <DropdownMenuContent
+              onClick={(e) => {
+                e.stopPropagation();
+                e.preventDefault();
+              }}
+            >
+              <DropdownMenuLabel>表格数据操作</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuGroup>
+                <DropdownMenuItem onSelect={() => {}}>
+                  <DropdownMenuShortcut className="ml-0 mr-2 mt-0.5">
+                    <FileUp size={15} />
+                  </DropdownMenuShortcut>
+                  发布到市场
+                </DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem className="text-red-10" onSelect={() => {}}>
+                  <DropdownMenuShortcut className="ml-0 mr-2 mt-0.5">
+                    <Trash size={15} />
+                  </DropdownMenuShortcut>
+                  删除
+                </DropdownMenuItem>
+              </DropdownMenuGroup>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        )}
       />
     </main>
   );
