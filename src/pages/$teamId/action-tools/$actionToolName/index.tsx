e029d14a27f1670b768e2c5c55ca8911cb1690f6
玖叁 <colour_93@furry.top>
2024-04-16 17:20:19 +0800

    feat: 优化返回逻辑

--- src/pages/$teamId/action-tools/$actionToolName/index.tsx
+++ src/pages/$teamId/action-tools/$actionToolName/index.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 
 import { createFileRoute, useNavigate } from '@tanstack/react-router';
 
+import { Undo2 } from 'lucide-react';
+
 import { useTool } from '@/apis/tools';
 import { useSearchReferenceWorkflows } from '@/apis/ugc';
 import { UgcDetailInfo } from '@/components/layout/ugc/detail/info';
 import { UgcDetailWorkflows } from '@/components/layout/ugc/detail/workflows';
 import { createActionToolsColumns } from '@/components/layout/ugc-pages/action-tools/consts.tsx';
 import { teamIdGuard } from '@/components/router/guard/team-id.ts';
+import { Button } from '@/components/ui/button';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs.tsx';
+import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip';
 
 interface IActionToolDetailProps {}
 
@@ -22,7 +26,23 @@ export const ActionToolDetail: React.FC<IActionToolDetailProps> = () => {
 
   return (
     <main className="flex size-full flex-col gap-4">
-      <h1 className="text-2xl font-bold">组件详情</h1>
+      <header className="flex items-center gap-4">
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button
+              icon={<Undo2 />}
+              variant="outline"
+              size="small"
+              className="-m-1 -ml-0.5 -mr-2 scale-85"
+              onClick={() => {
+                history.back();
+              }}
+            />
+          </TooltipTrigger>
+          <TooltipContent>返回</TooltipContent>
+        </Tooltip>
+        <h1 className="line-clamp-1 text-2xl font-bold">组件详情</h1>
+      </header>
       <Tabs
         defaultValue="info"
         className="[&_[role='tabpanel']]:mt-4 [&_[role='tabpanel']]:h-[calc(100vh-11.5rem)] [&_[role='tabpanel']]:overflow-y-auto [&_[role='tabpanel']]:overflow-x-hidden"
