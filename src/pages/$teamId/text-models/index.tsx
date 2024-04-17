de585887fd8750c465d67894b11b240b9f758432
玖叁 <colour_93@furry.top>
2024-04-17 22:57:52 +0800

    fix: 恢复错误页面

--- src/pages/$teamId/text-models/index.tsx
+++ src/pages/$teamId/text-models/index.tsx
@@ -1,5 +1,140 @@
-import { createFileRoute } from '@tanstack/react-router'
+import React, { useState } from 'react';
+
+import { mutate } from 'swr';
+import { createFileRoute, useNavigate } from '@tanstack/react-router';
+
+import { FileUp, Trash } from 'lucide-react';
+
+import { preloadUgcTextModels, useUgcTextModels } from '@/apis/ugc';
+import { IAssetItem } from '@/apis/ugc/typings.ts';
+import { UgcDeleteDialog } from '@/components/layout/ugc/delete-dialog';
+import { UgcPublishDialog } from '@/components/layout/ugc/publish-dialog';
+import { UgcView } from '@/components/layout/ugc/view';
+import { RenderIcon } from '@/components/layout/ugc/view/utils/renderer.tsx';
+import { createTextModelsColumns } from '@/components/layout/ugc-pages/text-models/consts.tsx';
+import { teamIdGuard } from '@/components/router/guard/team-id.ts';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu.tsx';
+import { Tooltip, TooltipTrigger } from '@/components/ui/tooltip';
+import { formatTimeDiffPrevious } from '@/utils/time.ts';
+
+export const TextModels: React.FC = () => {
+  const navigate = useNavigate();
+
+  const [publishVisible, setPublishVisible] = useState(false);
+  const [deleteVisible, setDeleteVisible] = useState(false);
+
+  const [current, setCurrent] = useState<IAssetItem | undefined>();
+
+  return (
+    <main className="size-full">
+      <UgcView
+        assetKey="text-models"
+        assetType="llm-model"
+        assetName="语言模型"
+        useUgcFetcher={useUgcTextModels}
+        preloadUgcFetcher={preloadUgcTextModels}
+        createColumns={() => createTextModelsColumns()}
+        renderOptions={{
+          subtitle: (item) => {
+            return (
+              <div className="flex gap-1">
+                <span>{item.user?.name ?? '未知'}</span>
+                <span>创建于</span>
+                <span>{formatTimeDiffPrevious(item.createdTimestamp)}</span>
+              </div>
+            );
+          },
+          cover: (item) => {
+            return RenderIcon({ iconUrl: item.iconUrl, size: 'gallery' });
+          },
+        }}
+        operateArea={(item, trigger, tooltipTriggerContent) => (
+          <DropdownMenu>
+            {tooltipTriggerContent ? (
+              <Tooltip content={tooltipTriggerContent}>
+                <TooltipTrigger asChild>
+                  <DropdownMenuTrigger asChild>{trigger}</DropdownMenuTrigger>
+                </TooltipTrigger>
+              </Tooltip>
+            ) : (
+              <DropdownMenuTrigger asChild>{trigger}</DropdownMenuTrigger>
+            )}
+
+            <DropdownMenuContent
+              onClick={(e) => {
+                e.stopPropagation();
+                e.preventDefault();
+              }}
+            >
+              <DropdownMenuLabel>语言模型操作</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuGroup>
+                <DropdownMenuItem
+                  onSelect={() => {
+                    setCurrent(item);
+                    setPublishVisible(true);
+                  }}
+                >
+                  <DropdownMenuShortcut className="ml-0 mr-2 mt-0.5">
+                    <FileUp size={15} />
+                  </DropdownMenuShortcut>
+                  发布到市场
+                </DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem
+                  className="text-red-10"
+                  onSelect={() => {
+                    setCurrent(item);
+                    setDeleteVisible(true);
+                  }}
+                >
+                  <DropdownMenuShortcut className="ml-0 mr-2 mt-0.5">
+                    <Trash size={15} />
+                  </DropdownMenuShortcut>
+                  删除
+                </DropdownMenuItem>
+              </DropdownMenuGroup>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        )}
+        onItemClick={(item) => {
+          void navigate({
+            to: `/$teamId/text-models/${item.name}`,
+          });
+        }}
+      />
+
+      <UgcPublishDialog
+        visible={publishVisible}
+        setVisible={setPublishVisible}
+        ugcId={current?._id}
+        item={current ?? {}}
+      />
+      <UgcDeleteDialog
+        visible={deleteVisible}
+        setVisible={setDeleteVisible}
+        assetType={current?.assetType}
+        ugcId={current?._id}
+        afterOperate={() => {
+          void mutate((key) => typeof key === 'string' && key.startsWith('/api/llm/models'), undefined, {
+            revalidate: true,
+          });
+        }}
+      />
+    </main>
+  );
+};
 
 export const Route = createFileRoute('/$teamId/text-models/')({
-  component: () => <div>Hello /$teamId/text-models/!</div>
-})
+  component: TextModels,
+  beforeLoad: teamIdGuard,
+});
