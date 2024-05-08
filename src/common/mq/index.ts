04d9d86045a63a592eeed4af2ed746792458a676
codevoyager1984 <codevoyager.jay@gmail.com>
2024-05-08 21:17:17 +0700

    fix: Multi tenant conductor task name conflict; Use inmemory event emitter as mq

--- src/common/mq/index.ts
+++ src/common/mq/index.ts
@@ -1,38 +1,28 @@
-import Redis, { Cluster } from 'ioredis';
-import { RedisConfig } from '../config';
-import { initRedisClient } from '../redis';
+import { EventEmitter } from 'events';
+import { logger } from '../logger';
 
 export interface Mq {
-  canuse: boolean;
   subscribe(channel: string, callback: (channel: string, message: string) => void): void;
-  publish(channel: string, message: string): Promise<number>;
+  publish(channel: string, message: string): Promise<void>;
 }
 
-export class InMemoryMq implements Mq {
-  canuse: boolean = false;
-  subscribe() {
-    throw new Error('Method not implemented.');
-  }
-  publish(): Promise<number> {
-    throw new Error('Method not implemented.');
-  }
-}
+class ChatMessageEventEmitter extends EventEmitter {}
 
-export class RedisMq implements Mq {
-  canuse: boolean = true;
-  sub: Redis | Cluster;
-  pub: Redis | Cluster;
-  constructor(redisConfig: RedisConfig) {
-    this.sub = initRedisClient(redisConfig);
-    this.pub = initRedisClient(redisConfig);
+export class EventEmitterMq implements Mq {
+  emitter: ChatMessageEventEmitter;
+
+  constructor() {
+    this.emitter = new ChatMessageEventEmitter();
   }
 
-  public publish(channel: string, message: string) {
-    return this.pub.publish(channel, message);
+  public async publish(channel: string, message: string) {
+    logger.debug(`Publishing message to channel ${channel}: ${message}`);
+    this.emitter.emit(channel, message);
   }
 
   public subscribe(channel: string, callback: (channel: string, message: string) => void) {
-    this.sub.subscribe(channel);
-    this.sub.on('message', callback);
+    this.emitter.on(channel, (data: string) => {
+      callback(channel, data);
+    });
   }
 }
