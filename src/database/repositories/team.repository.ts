19e7915c4fdb17a3314a57504ada38210c4284e6
codevoyager1984 <codevoyager.jay@gmail.com>
2024-05-09 13:57:32 +0700

    fix: refreshLogo

--- src/database/repositories/team.repository.ts
+++ src/database/repositories/team.repository.ts
@@ -27,7 +27,7 @@ export class TeamRepository {
 
   private async refreshLogo(teams: TeamEntity[]) {
     const s3Helpers = new S3Helpers();
-    const promises = teams.map(async (team) => {
+    const promises = teams.filter(Boolean).map(async (team) => {
       if (team.iconUrl) {
         try {
           const { refreshed, refreshedUrl } = await s3Helpers.refreshSignedUrl(team.iconUrl);
