65dbf1a6e6afb0b5d3510679caa6fb7c6db39504
LF112 <lf@lf112.net>
2024-05-23 20:50:25 +0800

    feat: hidden text-data-store page

--- ui/src/consts/sidebar.tsx
+++ ui/src/consts/sidebar.tsx
@@ -93,10 +93,10 @@ export const SIDEBAR_MAP: NavItemWithoutLabel[] = [
         path: '/$teamId/image-model-store',
         name: 'image-model-store',
       },
-      {
-        path: '/$teamId/text-data-store',
-        name: 'text-data-store',
-      },
+      // {
+      //   path: '/$teamId/text-data-store',
+      //   name: 'text-data-store',
+      // },
     ],
   },
 ];
