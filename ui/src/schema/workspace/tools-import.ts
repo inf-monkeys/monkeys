bdb6c4d1f0fad93e6a04d299161426c2fb724ebb
codevoyager1984 <codevoyager.jay@gmail.com>
2024-05-23 18:57:16 +0700

    feat: Import tool by openapi swagger spec

--- ui/src/schema/workspace/tools-import.ts
+++ ui/src/schema/workspace/tools-import.ts
@@ -1,7 +1,24 @@
 import z from 'zod';
 
+export enum ToolImportType {
+  manifest = 'manifest',
+  openapiSpec = 'openapiSpec',
+}
+
+export enum ToolOpenAPISpecType {
+  url = 'url',
+  upload = 'upload',
+  input = 'input',
+}
+
 export const importToolSchema = z.object({
-  manifestUrl: z.string().min(1, 'Manifest 地址不能未空').url('必须为合法的链接'),
+  importType: z.string(),
+  manifestUrl: z.string().url('必须为合法的链接').optional(),
+
+  // openapi spec
+  namespace: z.string().optional(),
+  openapiSpecUrl: z.string().url('必须为合法的链接').optional(),
+  openapiSpecType: z.string().optional(),
 });
 
 export type IImportTool = z.infer<typeof importToolSchema>;
