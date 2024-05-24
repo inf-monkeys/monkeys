de33f7145a44c3ea59b08e67635b3c00b1b0ec6e
codevoyager1984 <codevoyager.jay@gmail.com>
2024-05-24 16:17:09 +0700

    fix: Add NODE_TLS_REJECT_UNAUTHORIZED

--- src/main.ts
+++ src/main.ts
@@ -25,6 +25,8 @@ process.on('uncaughtException', (err: Error) => {
   logger.error('uncaughtException: ', err);
 });
 
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
