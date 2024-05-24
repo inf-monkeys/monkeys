27f630d783779f51a36f007a5bba0a79d7686d88
codevoyager1984 <codevoyager.jay@gmail.com>
2024-05-24 20:29:00 +0700

    feat: Add migrations

--- src/database/migrations/1716551483943-add-column-log-endpoint-for-tools-server.ts
+++ src/database/migrations/1716551483943-add-column-log-endpoint-for-tools-server.ts
@@ -1,12 +1,30 @@
 import { config } from '@/common/config';
-import { MigrationInterface, QueryRunner } from 'typeorm';
+import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
+import { NULLABLE, VARCHAR } from './columns';
+import { isColumnExist } from './utils';
 
 const appId = config.server.appId;
 
 export class MigartionAddColumnLogEndpointForToolsServer1716551483943 implements MigrationInterface {
-  tableName = `${appId}_tools_server`;
-  columnName = 'log_endpoint';
-  public async up(queryRunner: QueryRunner): Promise<void> {}
+  TABLE_NAME = `${appId}_tools_server`;
+  COLUMN_NAME = 'log_endpoint';
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    const exists = await isColumnExist(this.COLUMN_NAME, this.TABLE_NAME, queryRunner);
+    if (!exists) {
+      await queryRunner.addColumn(
+        this.TABLE_NAME,
+        new TableColumn({
+          name: this.COLUMN_NAME,
+          comment: 'Log Endpoint',
+          ...NULLABLE,
+          ...VARCHAR,
+          length: '255',
+        }),
+      );
+    }
+  }
 
-  public async down(queryRunner: QueryRunner): Promise<void> {}
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn(this.TABLE_NAME, this.COLUMN_NAME);
+  }
 }
