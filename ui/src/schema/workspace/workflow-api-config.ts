02d27874f523559c1203c40cff006a6a72877ccf
LF112 <lf@lf112.net>
2024-06-05 19:39:26 +0800

    prev: openai interface

--- ui/src/schema/workspace/workflow-api-config.ts
+++ ui/src/schema/workspace/workflow-api-config.ts
@@ -6,7 +6,7 @@ export const workflowApiConfigInfoSchema = z.object({
     windowMs: z.number().min(1000, '时间窗口最小为 1000 毫秒'),
     max: z.number().min(1, '单位时间窗口内运行最大并发最小为 1').max(1000, '单位时间窗口内运行最大并发最小为 1000'),
   }),
-  exposeOpenaiCompatibleInterface: z.any(),
+  exposeOpenaiCompatibleInterface: z.boolean(),
 });
 
 export type IWorkflowApiConfigInfo = z.infer<typeof workflowApiConfigInfoSchema>;
