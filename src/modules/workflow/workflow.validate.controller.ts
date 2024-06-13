e2244916d1be4c9e54af20339db5572611c9e777
codevoyager1984 <codevoyager.jay@gmail.com>
2024-06-13 16:52:41 +0700

    feat: Workflow validate API add workflowId and workflowVersion parameter

--- src/modules/workflow/workflow.validate.controller.ts
+++ src/modules/workflow/workflow.validate.controller.ts
@@ -5,13 +5,17 @@ import { ValidationIssueType } from '@/database/entities/workflow/workflow-metad
 import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { ValidateWorkflowDto } from './dto/req/validate-workflow.dto';
+import { WorkflowCrudService } from './workflow.curd.service';
 import { WorkflowValidateService } from './workflow.validate.service';
 
 @Controller('workflow')
 @ApiTags('Workflows/Validation')
 @UseGuards(CompatibleAuthGuard)
 export class WorkflowValidateController {
-  constructor(private readonly service: WorkflowValidateService) {}
+  constructor(
+    private readonly service: WorkflowValidateService,
+    private readonly workflowCrudService: WorkflowCrudService,
+  ) {}
 
   @Post('/validate')
   @ApiOperation({
@@ -20,10 +24,15 @@ export class WorkflowValidateController {
   })
   public async validateWorkflow(@Req() req: IRequest, @Body() body: ValidateWorkflowDto) {
     const { teamId } = req;
-    const { tasks, output = [] } = body;
+    const { tasks, output = [], workflowId, workflowVersion = 1 } = body;
+    await this.workflowCrudService.getWorkflowDef(workflowId, workflowVersion);
     const validationIssues = await this.service.validateWorkflow(teamId, tasks, output);
     const errors = validationIssues.filter((i) => i.issueType === ValidationIssueType.ERROR);
     const validated = errors.length === 0;
+    await this.workflowCrudService.updateWorkflowDef(teamId, workflowId, workflowVersion, {
+      validationIssues,
+    });
+
     return new SuccessResponse({
       data: {
         validated,
