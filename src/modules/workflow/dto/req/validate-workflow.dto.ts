d0853f8cec90972e656459fe3fb46b9503ab1a38
codevoyager1984 <codevoyager.jay@gmail.com>
2024-06-13 16:52:41 +0700

    feat: Workflow validate API add workflowId and workflowVersion parameter

--- src/modules/workflow/dto/req/validate-workflow.dto.ts
+++ src/modules/workflow/dto/req/validate-workflow.dto.ts
@@ -3,6 +3,18 @@ import { ApiProperty } from '@nestjs/swagger';
 
 export class ValidateWorkflowDto {
   @ApiProperty({
+    description: '工作流 id',
+    required: true,
+  })
+  workflowId: string;
+
+  @ApiProperty({
+    description: '工作流版本号',
+    required: true,
+  })
+  workflowVersion: number;
+
+  @ApiProperty({
     description: '工作流 task 定义',
     required: true,
   })
