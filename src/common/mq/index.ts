adc23b64fd66c7aa260601752ccfc36c2cb7a75d
codevoyager1984 <codevoyager.jay@gmail.com>
2024-06-17 08:30:48 +0700

    feat: Redis MQ

--- src/common/mq/index.ts
+++ src/common/mq/index.ts
@@ -1,4 +1,7 @@
 import { EventEmitter } from 'events';
+import Redis, { Cluster } from 'ioredis';
+import { RedisConfig } from '../config';
+import { initRedisClient } from '../redis';
 
 export interface Mq {
   subscribe(channel: string, callback: (channel: string, message: string) => void): void;
@@ -22,5 +25,25 @@ export class EventEmitterMq implements Mq {
     this.emitter.on(channel, (data: string) => {
       callback(channel, data);
     });
+  }
+}
+
+export class RedisMq implements Mq {
+  canuse: boolean = true;
+  sub: Redis | Cluster;
+  pub: Redis | Cluster;
+
+  constructor(redisConfig: RedisConfig) {
+    this.sub = initRedisClient(redisConfig);
+    this.pub = initRedisClient(redisConfig);
+  }
+
+  public async publish(channel: string, message: string) {
+    await this.pub.publish(channel, message);
+  }
+
+  public subscribe(channel: string, callback: (channel: string, message: string) => void) {
+    this.sub.subscribe(channel);
+    this.sub.on('message', callback);
   }
 }
