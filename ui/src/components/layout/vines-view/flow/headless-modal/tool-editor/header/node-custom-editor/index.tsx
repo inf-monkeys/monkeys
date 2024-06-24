cbc8cb8cd3ea038da154fe40ead213590bc3b511
LF112 <lf@lf112.net>
2024-06-24 09:06:14 +0800

    i18n: tool editor header

--- ui/src/components/layout/vines-view/flow/headless-modal/tool-editor/header/node-custom-editor/index.tsx
+++ ui/src/components/layout/vines-view/flow/headless-modal/tool-editor/header/node-custom-editor/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { useTranslation } from 'react-i18next';
+
 import { ToolCustomDataEditor } from '@/components/layout/vines-view/flow/headless-modal/tool-editor/header/node-custom-editor/editor.tsx';
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
 import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip';
@@ -13,6 +15,8 @@ interface INodeCustomEditorProps {
 }
 
 export const NodeCustomEditor: React.FC<INodeCustomEditorProps> = ({ node }) => {
+  const { t } = useTranslation();
+
   const { isLatestWorkflowVersion } = useFlowStore();
   const { vines } = useVinesFlow();
 
@@ -36,7 +40,9 @@ export const NodeCustomEditor: React.FC<INodeCustomEditorProps> = ({ node }) => 
               <div className="flex flex-col gap-1 leading-5">
                 <div className="flex items-center gap-2">
                   <div className="flex items-end gap-2">
-                    <p className="text-base font-bold leading-none">{toolDisplayName ?? '不受支持的工具'}</p>
+                    <p className="text-base font-bold leading-none">
+                      {toolDisplayName ?? t('workspace.flow-view.vines.tools.unknown')}
+                    </p>
                     {data?.title && <span className="text-text2 text-xs font-light">{tool?.displayName}</span>}
                   </div>
                 </div>
@@ -60,7 +66,7 @@ export const NodeCustomEditor: React.FC<INodeCustomEditorProps> = ({ node }) => 
           />
         </PopoverContent>
       </Popover>
-      <TooltipContent>点击编辑工具自定义信息</TooltipContent>
+      <TooltipContent>{t('workspace.flow-view.headless-modal.tool-editor.header.info.editor.button')}</TooltipContent>
     </Tooltip>
   );
 };
