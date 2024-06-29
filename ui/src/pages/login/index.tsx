216080807d28dd90a65ab38a6a7823e1ebc3f037
LF112 <lf@lf112.net>
2024-06-29 22:59:37 +0800

    i18n: login

--- ui/src/pages/login/index.tsx
+++ ui/src/pages/login/index.tsx
@@ -10,7 +10,9 @@ import { handleOidcLogin } from '@/apis/authz/oidc';
 import { useSystemConfig } from '@/apis/common';
 import { AuthMethod } from '@/apis/common/typings.ts';
 import { AuthContainer } from '@/components/layout/login';
+import { VinesDarkMode } from '@/components/layout/main/vines-darkmode.tsx';
 import { IUserTokens } from '@/components/router/guard/auth.ts';
+import { I18nSelector } from '@/components/ui/i18n-selector';
 import { AppLogo } from '@/components/ui/logo';
 import { SmoothTransition } from '@/components/ui/smooth-transition-size/SmoothTransition.tsx';
 import { pageSearchSchema } from '@/schema/common.ts';
@@ -48,37 +50,43 @@ const Login: React.FC = () => {
   const isServerError = error instanceof Error;
 
   return (
-    <div className="flex flex-col items-center gap-8">
-      {!isServerError && <AppLogo url={logoUrl} alt={appName} height={36} />}
-      <div className="relative flex w-full flex-col items-center">
-        <AnimatePresence>
-          {!loginMethodsLength ? (
-            <motion.div
-              className="flex select-none items-center justify-center"
-              key="vines-login-disabled"
-              initial={{ opacity: 0, height: 0 }}
-              animate={{ opacity: 1, height: 28, transition: { delay: 0.5 } }}
-              exit={{ opacity: 0, height: 0 }}
-            >
-              <h1 className={cn('text-lg font-bold text-vines-500', isServerError && 'animate-pulse')}>
-                {isServerError ? t('system.network-error') : t('system.empty-auth-methods')}
-              </h1>
-            </motion.div>
-          ) : (
-            <SmoothTransition initialHeight={264}>
-              <AuthContainer
-                loginMethodsLength={loginMethodsLength}
-                enableOidc={isOidcEnabled}
-                enablePassword={isPasswordEnable}
-                enablePhone={isPhoneEnable}
-                oidcButtonText={oidcButtonText}
-                hasTokens={hasTokens}
-              />
-            </SmoothTransition>
-          )}
-        </AnimatePresence>
+    <>
+      <div className="flex flex-col items-center gap-8">
+        {!isServerError && <AppLogo url={logoUrl} alt={appName} height={36} />}
+        <div className="relative flex w-full flex-col items-center">
+          <AnimatePresence>
+            {!loginMethodsLength ? (
+              <motion.div
+                className="flex select-none items-center justify-center"
+                key="vines-login-disabled"
+                initial={{ opacity: 0, height: 0 }}
+                animate={{ opacity: 1, height: 28, transition: { delay: 0.5 } }}
+                exit={{ opacity: 0, height: 0 }}
+              >
+                <h1 className={cn('text-lg font-bold text-vines-500', isServerError && 'animate-pulse')}>
+                  {isServerError ? t('system.network-error') : t('system.empty-auth-methods')}
+                </h1>
+              </motion.div>
+            ) : (
+              <SmoothTransition initialHeight={264}>
+                <AuthContainer
+                  loginMethodsLength={loginMethodsLength}
+                  enableOidc={isOidcEnabled}
+                  enablePassword={isPasswordEnable}
+                  enablePhone={isPhoneEnable}
+                  oidcButtonText={oidcButtonText}
+                  hasTokens={hasTokens}
+                />
+              </SmoothTransition>
+            )}
+          </AnimatePresence>
+        </div>
       </div>
-    </div>
+      <div className="absolute bottom-6 left-6 flex items-center gap-2">
+        <VinesDarkMode />
+        <I18nSelector />
+      </div>
+    </>
   );
 };
 
