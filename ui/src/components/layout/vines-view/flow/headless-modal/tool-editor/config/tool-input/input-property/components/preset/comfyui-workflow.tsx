9ea9ebbff03eab83447e548dc18ebb377c57d7f9
玖叁 <colour_93@furry.top>
2024-07-02 14:16:09 +0800

    fix: i18ncontent hook

--- ui/src/components/layout/vines-view/flow/headless-modal/tool-editor/config/tool-input/input-property/components/preset/comfyui-workflow.tsx
+++ ui/src/components/layout/vines-view/flow/headless-modal/tool-editor/config/tool-input/input-property/components/preset/comfyui-workflow.tsx
@@ -8,7 +8,7 @@ import { IVinesInputPropertyProps } from '@/components/layout/vines-view/flow/he
 import { IVinesInputPresetProps } from '@/components/layout/vines-view/flow/headless-modal/tool-editor/config/tool-input/input-property/components/preset/index.tsx';
 import { PresetWrapper } from '@/components/layout/vines-view/flow/headless-modal/tool-editor/config/tool-input/input-property/components/preset/wrapper.tsx';
 import { IVinesToolPropertiesOption, VinesToolDefProperties } from '@/package/vines-flow/core/tools/typings.ts';
-import { I18nContent } from '@/utils';
+import { getI18nContent } from '@/utils';
 
 export const ComfyuiWorkflowsSelector: React.FC<IVinesInputPropertyProps & IVinesInputPresetProps> = (props) => {
   const { t } = useTranslation();
@@ -22,7 +22,7 @@ export const ComfyuiWorkflowsSelector: React.FC<IVinesInputPropertyProps & IVine
     if (!comfyuiWorkflows) return;
 
     const opts = comfyuiWorkflows.map((m) => {
-      return { name: I18nContent(m.displayName) ?? '', value: m.id };
+      return { name: getI18nContent(m.displayName) ?? '', value: m.id };
     });
     setOptions(opts);
 
