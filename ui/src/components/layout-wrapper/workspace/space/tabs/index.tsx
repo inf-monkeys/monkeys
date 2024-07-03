8e799051975f01bb51af8e99ca50a11174038a3c
LF112 <lf@lf112.net>
2024-07-03 14:25:45 +0800

    prev: hidden execution button

--- ui/src/components/layout-wrapper/workspace/space/tabs/index.tsx
+++ ui/src/components/layout-wrapper/workspace/space/tabs/index.tsx
@@ -31,8 +31,6 @@ interface ITabsProps {}
 export const SpaceTabs: React.FC<ITabsProps> = () => {
   const { t } = useTranslation();
 
-  const { workflow } = useVinesPage();
-
   const { pages, setPages, page, pageId } = useVinesPage();
 
   const sensors = useSensors(
@@ -98,8 +96,6 @@ export const SpaceTabs: React.FC<ITabsProps> = () => {
   const pageNavLastIndex = pageLength - 1;
   const activeIndex = pages?.findIndex(({ id }) => id === pageId) ?? 0;
 
-  const openAIInterfaceEnabled = workflow?.exposeOpenaiCompatibleInterface ?? false;
-
   return (
     <DndContext
       sensors={sensors}
@@ -110,21 +106,19 @@ export const SpaceTabs: React.FC<ITabsProps> = () => {
       <header ref={headerNode} className="relative z-20 mx-3 mt-2 flex h-10 overflow-x-clip scroll-smooth">
         <div ref={tabsNode} className={cn('flex overflow-hidden', scrollToolVisible && 'pr-32')}>
           <SortableContext items={pageIds} strategy={horizontalListSortingStrategy} disabled={disableDND}>
-            {pages
-              ?.filter((it) => (openAIInterfaceEnabled ? it.type !== 'preview' : true))
-              ?.map(({ id, displayName, instance }, index) => (
-                <SpaceTab
-                  key={id}
-                  id={id}
-                  icon={instance?.icon ?? '⚠️'}
-                  displayName={displayName ?? t('workspace.wrapper.space.unknown-view')}
-                  activeIndex={activeIndex}
-                  isLastItem={pageNavLastIndex !== index}
-                  index={index}
-                  pages={pages}
-                  page={page}
-                />
-              ))}
+            {pages?.map(({ id, displayName, instance }, index) => (
+              <SpaceTab
+                key={id}
+                id={id}
+                icon={instance?.icon ?? '⚠️'}
+                displayName={displayName ?? t('workspace.wrapper.space.unknown-view')}
+                activeIndex={activeIndex}
+                isLastItem={pageNavLastIndex !== index}
+                index={index}
+                pages={pages}
+                page={page}
+              />
+            ))}
           </SortableContext>
         </div>
         <Expand visible={scrollToolVisible} tabsNode={tabsNode} />
--- ui/src/view/vines-preview/index.tsx
+++ ui/src/view/vines-preview/index.tsx
@@ -9,7 +9,6 @@ import { VinesActuator } from '@/components/layout/vines-view/execution/actuator
 import { Separator } from '@/components/ui/separator.tsx';
 import { useViewStore } from '@/store/useViewStore';
 import { cn } from '@/utils';
-import { ViewDisable } from '@/view/vines-preview/view-disable.tsx';
 
 // million-ignore
 export const VinesPreView: React.FC = () => {
@@ -21,31 +20,28 @@ export const VinesPreView: React.FC = () => {
   const finalHeight = height - (fullscreen ? 0 : 108);
 
   return (
-    <>
-      <div ref={ref} className={cn('relative h-full max-h-full p-6', !fullscreen && 'space-y-6')}>
-        <div className={cn('space-y-0.5', fullscreen && 'hidden')}>
-          <h2 className="text-2xl font-bold tracking-tight">{t('workspace.pre-view.title')}</h2>
-          <p className="text-muted-foreground">{t('workspace.pre-view.desc')}</p>
-        </div>
-        <Separator className={cn('my-6', fullscreen && 'hidden')} />
-        <motion.div
-          className="flex"
-          style={{ height: finalHeight }}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: finalHeight ? 1 : 0 }}
-          exit={{ opacity: 0 }}
-          transition={{ duration: 0.2 }}
-        >
-          <aside className={cn('relative w-3/5', fullscreen && 'w-full')}>
-            <VinesActuator height={finalHeight} />
-          </aside>
-          <Separator orientation="vertical" className={cn('mx-3', fullscreen && 'hidden')} />
-          <div className={cn('flex-1', fullscreen && 'hidden')}>
-            <VinesExecutionHistory />
-          </div>
-        </motion.div>
+    <div ref={ref} className={cn('relative h-full max-h-full p-6', !fullscreen && 'space-y-6')}>
+      <div className={cn('space-y-0.5', fullscreen && 'hidden')}>
+        <h2 className="text-2xl font-bold tracking-tight">{t('workspace.pre-view.title')}</h2>
+        <p className="text-muted-foreground">{t('workspace.pre-view.desc')}</p>
       </div>
-      <ViewDisable />
-    </>
+      <Separator className={cn('my-6', fullscreen && 'hidden')} />
+      <motion.div
+        className="flex"
+        style={{ height: finalHeight }}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: finalHeight ? 1 : 0 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.2 }}
+      >
+        <aside className={cn('relative w-3/5', fullscreen && 'w-full')}>
+          <VinesActuator height={finalHeight} />
+        </aside>
+        <Separator orientation="vertical" className={cn('mx-3', fullscreen && 'hidden')} />
+        <div className={cn('flex-1', fullscreen && 'hidden')}>
+          <VinesExecutionHistory />
+        </div>
+      </motion.div>
+    </div>
   );
 };
