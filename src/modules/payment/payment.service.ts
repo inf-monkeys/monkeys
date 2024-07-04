b9e67f1c475dc881fd13210638d92017f0da9b8b
LF112 <lf@lf112.net>
2024-07-04 11:31:46 +0800

    prev: wxpay api

--- src/modules/payment/payment.service.ts
+++ src/modules/payment/payment.service.ts
@@ -3,7 +3,7 @@ import { config } from '@/common/config';
 import axios from 'axios';
 import { logger } from '@/common/logger';
 import { toNumber } from 'lodash';
-import { PaymentBalanceDto } from '@/modules/payment/dto';
+import { PaymentBalanceDto, PaymentGetOrderDto } from '@/modules/payment/dto';
 
 @Injectable()
 export class PaymentService {
@@ -45,6 +45,44 @@ export class PaymentService {
     return balance;
   }
 
+  public async getOrders(teamId: string, query?: PaymentGetOrderDto): Promise<[any[], number]> {
+    const { enabled, baseUrl } = config.paymentServer;
+    if (!enabled || !baseUrl) {
+      throw new Error('Payment server is not enabled');
+    }
+
+    const api = `/payment/get-orders`;
+    let success: boolean;
+    let message: string;
+    let orders: any[];
+    let total = 0;
+    try {
+      const { data } = await axios<{ success: boolean; message: string; data: any[]; total: number }>({
+        method: 'POST',
+        url: api,
+        baseURL: baseUrl,
+        headers: {
+          'x-monkeys-appid': config.server.appId,
+          'x-monkeys-teamid': teamId,
+        },
+        data: query,
+      });
+      success = data.success;
+      message = data.message;
+      orders = data.data;
+      total = toNumber(data.total);
+    } catch (error) {
+      logger.warn(`Fetch orders failed: ${error.message}`);
+      success = false;
+    }
+    if (!success) {
+      logger.warn(`Fetch orders failed: ${message}`);
+      throw new Error(message ?? 'Fetch orders failed');
+    }
+
+    return [orders, total];
+  }
+
   public async createOrder(userId: string, teamId: string, amount: number) {
     const { enabled, baseUrl } = config.paymentServer;
     if (!enabled || !baseUrl) {
@@ -82,5 +120,69 @@ export class PaymentService {
     }
 
     return order;
+  }
+
+  public async getOrderById(orderId: string) {
+    const { enabled, baseUrl } = config.paymentServer;
+    if (!enabled || !baseUrl) {
+      throw new Error('Payment server is not enabled');
+    }
+
+    const api = `/payment/orders/${orderId}`;
+    let success: boolean;
+    let message: string;
+    let order: any;
+    try {
+      const { data } = await axios<{ success: boolean; message: string; data: any }>({
+        method: 'GET',
+        url: api,
+        baseURL: baseUrl,
+        headers: {
+          'x-monkeys-appid': config.server.appId,
+        },
+      });
+      success = data.success;
+      message = data.message;
+      order = data.data;
+    } catch (error) {
+      logger.warn(`Fetch order failed: ${error.message}`);
+      success = false;
+    }
+    if (!success) {
+      logger.warn(`Fetch order failed: ${message}`);
+      throw new Error(message ?? 'Fetch order failed');
+    }
+
+    return order;
+  }
+
+  public async closeOrder(orderId: string) {
+    const { enabled, baseUrl } = config.paymentServer;
+    if (!enabled || !baseUrl) {
+      throw new Error('Payment server is not enabled');
+    }
+
+    const api = `/payment/orders/${orderId}`;
+    let success: boolean;
+    let message: string;
+    try {
+      const { data } = await axios<{ success: boolean; message: string }>({
+        method: 'DELETE',
+        url: api,
+        baseURL: baseUrl,
+        headers: {
+          'x-monkeys-appid': config.server.appId,
+        },
+      });
+      success = data.success;
+      message = data.message;
+    } catch (error) {
+      logger.warn(`Close order failed: ${error.message}`);
+      success = false;
+    }
+    if (!success) {
+      logger.warn(`Close order failed: ${message}`);
+      throw new Error(message ?? 'Close order failed');
+    }
   }
 }
