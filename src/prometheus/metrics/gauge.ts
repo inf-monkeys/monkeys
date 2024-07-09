aa40dd4c488cc734d638a8f76a3ef91cf41d1d30
codevoyager1984 <codevoyager.jay@gmail.com>
2024-07-09 19:52:42 +0700

    feat: Add prometheus metrics endpoint

---
+++ src/prometheus/metrics/gauge.ts
@@ -0,0 +1,24 @@
+import { Provider } from '@nestjs/common';
+import * as client from 'prom-client';
+import { PrometheusContentType, RegistryContentType } from 'prom-client';
+import { PROMETHEUS_OPTIONS } from '../constants';
+import { PrometheusOptions } from '../interfaces';
+import { getOrCreateMetric, getToken } from './utils';
+
+/**
+ * @public
+ */
+export function makeGaugeProvider(options: client.GaugeConfiguration<string>): Provider {
+  return {
+    provide: getToken(options.name),
+    useFactory<T extends RegistryContentType = PrometheusContentType>(config?: PrometheusOptions<T>): client.Metric<string> {
+      return getOrCreateMetric('Gauge', options, config);
+    },
+    inject: [
+      {
+        token: PROMETHEUS_OPTIONS,
+        optional: true,
+      },
+    ],
+  };
+}
