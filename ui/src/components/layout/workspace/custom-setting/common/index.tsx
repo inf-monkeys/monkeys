6574f61fdb46e32c118257f456a72eb37c77aafa
LF112 <lf@lf112.net>
2024-07-12 16:44:08 +0800

    prev: iframe embed doc

--- ui/src/components/layout/workspace/custom-setting/common/index.tsx
+++ ui/src/components/layout/workspace/custom-setting/common/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { ApiKey } from '@/components/layout/settings/api-key';
 import { PagePermissions } from '@/components/layout/workspace/custom-setting/common/page-permissions.tsx';
+import { IFrameEmbed } from '@/components/layout/workspace/custom-setting/common/iframe-embed.tsx';
 
 interface ICommonSettingProps extends React.ComponentPropsWithoutRef<'div'> {}
 
@@ -10,6 +11,7 @@ export const CommonSetting: React.FC<ICommonSettingProps> = () => {
     <div className="grid grid-cols-2 items-start justify-center gap-4">
       <div className="grid items-start gap-4">
         <PagePermissions />
+        <IFrameEmbed />
       </div>
       <div className="grid items-start gap-4">
         <ApiKey />
