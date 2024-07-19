162c8ebd5fc68cbafac932c5f8a225f22325c94f
LF112 <lf@lf112.net>
2024-07-19 10:15:16 +0800

    prev: fix

--- ui/src/view/vines-form/index.tsx
+++ ui/src/view/vines-form/index.tsx
@@ -29,7 +29,7 @@ export const VinesForm: React.FC<IVinesFormProps> = () => {
       <div
         className={cn(
           'relative grid size-full grid-cols-2 p-6',
-          workbenchVisible && 'p-0',
+          workbenchVisible && 'py-0 px-4',
           isSmallFrame && 'h-[calc(100%-4rem)] grid-cols-1',
         )}
       >
