314b899133a11a92396001b8289f660f2a238f49
LF112 <lf@lf112.net>
2024-07-22 18:19:41 +0800

    prev: support edit & delete message

--- ui/src/components/layout/vines-view/chat/chat-bot/messages/chat-message/toolbar/index.tsx
+++ ui/src/components/layout/vines-view/chat/chat-bot/messages/chat-message/toolbar/index.tsx
@@ -3,22 +3,26 @@ import React from 'react';
 import { Copy, CopyCheck } from 'lucide-react';
 import { useTranslation } from 'react-i18next';
 
+import { DeleteMessage } from '@/components/layout/vines-view/chat/chat-bot/messages/chat-message/toolbar/delete-message.tsx';
+import { EditMessage } from '@/components/layout/vines-view/chat/chat-bot/messages/chat-message/toolbar/edit-message.tsx';
+import { IVinesMessage } from '@/components/layout/vines-view/chat/chat-bot/use-chat.ts';
 import { Button } from '@/components/ui/button';
 import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip';
 import { useCopy } from '@/hooks/use-copy.ts';
 
 interface IMessageToolbarProps extends React.ComponentPropsWithoutRef<'div'> {
-  chatId: string;
+  setMessageByIndex: (index: number, message?: Partial<IVinesMessage>) => void;
   content: string;
+  messageIndex: number;
 }
 
-export const MessageToolbar: React.FC<IMessageToolbarProps> = ({ content }) => {
+export const MessageToolbar: React.FC<IMessageToolbarProps> = ({ content, setMessageByIndex, messageIndex }) => {
   const { t } = useTranslation();
 
   const { copy, copied } = useCopy();
 
   return (
-    <div className="absolute -bottom-1 -right-9 flex scale-80 gap-2 opacity-0 transition-opacity group-hover:opacity-100">
+    <div className="-m-1 -ml-2 flex scale-75 gap-1 opacity-0 transition-opacity group-hover:opacity-100">
       <Tooltip>
         <TooltipTrigger asChild>
           <Button
@@ -31,6 +35,8 @@ export const MessageToolbar: React.FC<IMessageToolbarProps> = ({ content }) => {
         </TooltipTrigger>
         <TooltipContent>{t('common.utils.click-to-copy')}</TooltipContent>
       </Tooltip>
+      <EditMessage content={content} setMessageByIndex={setMessageByIndex} messageIndex={messageIndex} />
+      <DeleteMessage setMessageByIndex={setMessageByIndex} messageIndex={messageIndex} />
     </div>
   );
 };
