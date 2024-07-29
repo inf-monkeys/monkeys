dda4e4a629cf464cfe9a7aaf025af992ade770a2
玖叁 <colour_93@furry.top>
2024-07-29 10:17:56 +0800

    fix: stat filter

--- ui/src/components/layout/vines-view/execution-log/stat/utils.ts
+++ ui/src/components/layout/vines-view/execution-log/stat/utils.ts
@@ -4,6 +4,12 @@ export const getDayBegin = (rawDate: Date | number | string) => {
   return date;
 };
 
+export const getDayEnd = (rawDate: Date | number | string) => {
+  const date = new Date(rawDate);
+  date.setHours(23, 59, 59, 999);
+  return date;
+};
+
 export const getRelativeDate = (rawDate: Date | number | string, offset: number) => {
   const date = new Date(rawDate);
   date.setDate(date.getDate() + offset);
