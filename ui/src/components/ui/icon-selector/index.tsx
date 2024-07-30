552bcdcd974713058d93f917ec03dab51dcb7441
玖叁 <colour_93@furry.top>
2024-07-30 14:19:47 +0800

    fix: virtuoso

--- ui/src/components/ui/icon-selector/index.tsx
+++ ui/src/components/ui/icon-selector/index.tsx
@@ -1,18 +1,18 @@
-import React, { memo, useEffect, useMemo, useRef, useState } from 'react';
+import React, { forwardRef, memo, useEffect, useMemo, useRef, useState } from 'react';
 
 import { I18nValue } from '@inf-monkeys/monkeys';
 import { ChevronLeft, ChevronRight, Search } from 'lucide-react';
 import { useTranslation } from 'react-i18next';
+import { VirtuosoGrid } from 'react-virtuoso';
 
 import { Button } from '@/components/ui/button';
 import { ICONS_CATEGORIES } from '@/components/ui/icon-selector/consts.ts';
 import { Input } from '@/components/ui/input';
-import { ScrollArea } from '@/components/ui/scroll-area.tsx';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs.tsx';
 import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip';
 import { LucideIconRender } from '@/components/ui/vines-icon/lucide/render.tsx';
 import { useAppStore } from '@/store/useAppStore';
-import { getI18nContent } from '@/utils';
+import { cn, getI18nContent } from '@/utils';
 import VinesEvent from '@/utils/events.ts';
 
 interface IVinesIconSelectorProps extends Omit<React.ComponentPropsWithoutRef<'div'>, 'onChange'> {
@@ -115,27 +115,36 @@ export const VinesIconSelector: React.FC<IVinesIconSelectorProps> = memo(({ chil
         </div>
         {Object.keys(iconCateMap).map((cateName) => (
           <TabsContent value={cateName} key={cateName}>
-            <ScrollArea className="h-[calc(100vh-16rem)]">
-              <div className="flex h-full flex-wrap gap-2">
-                {iconCateMap[cateName].icons.map((iconName) => {
-                  return (
-                    <Tooltip key={iconName}>
-                      <TooltipTrigger>
-                        <div
-                          className="cursor-pointer rounded-md bg-gray-3 bg-opacity-60 p-3 transition-all hover:bg-opacity-15"
-                          onClick={() => {
-                            onIconSelect?.(iconName);
-                          }}
-                        >
-                          <LucideIconRender src={iconName} className="size-6" />
-                        </div>
-                      </TooltipTrigger>
-                      <TooltipContent side="bottom">{iconName}</TooltipContent>
-                    </Tooltip>
-                  );
-                })}
-              </div>
-            </ScrollArea>
+            <VirtuosoGrid
+              style={{ height: 'calc( 100vh - 16rem )' }}
+              data={iconCateMap[cateName].icons}
+              components={{
+                // eslint-disable-next-line react/display-name
+                List: forwardRef(({ children, className, ...props }, ref) => (
+                  <div ref={ref} className={cn('flex flex-wrap gap-2', className)} {...props}>
+                    {children}
+                  </div>
+                )),
+                Item: ({ children }) => children,
+              }}
+              itemContent={(_, iconName) => {
+                return (
+                  <Tooltip key={iconName}>
+                    <TooltipTrigger>
+                      <div
+                        className="cursor-pointer rounded-md bg-gray-3 bg-opacity-60 p-3 transition-all hover:bg-opacity-15"
+                        onClick={() => {
+                          onIconSelect?.(iconName);
+                        }}
+                      >
+                        <LucideIconRender src={iconName} className="size-6" />
+                      </div>
+                    </TooltipTrigger>
+                    <TooltipContent side="bottom">{iconName}</TooltipContent>
+                  </Tooltip>
+                );
+              }}
+            />
           </TabsContent>
         ))}
       </Tabs>
