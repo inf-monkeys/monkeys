b850eb2af6968a73010d002ff63dbe6be29ce75a
玖叁 <colour_93@furry.top>
2024-07-30 13:05:23 +0800

    feat: icon selector

--- ui/src/components/ui/icon-selector/index.tsx
+++ ui/src/components/ui/icon-selector/index.tsx
@@ -1,105 +1,146 @@
-import React, { memo, useEffect, useState } from 'react';
+import React, { memo, useEffect, useMemo, useRef, useState } from 'react';
 
-import data from '@emoji-mart/data';
-import i18n from '@emoji-mart/data/i18n/zh.json';
-import Picker from '@emoji-mart/react';
+import { I18nValue } from '@inf-monkeys/monkeys';
+import { ChevronLeft, ChevronRight, Search } from 'lucide-react';
 import { useTranslation } from 'react-i18next';
 
 import { Button } from '@/components/ui/button';
-import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
-import { cn } from '@/utils';
-import { useLocalStorage } from '@/hooks/use-local-storage';
+import { ICONS_CATEGORIES } from '@/components/ui/icon-selector/consts.ts';
+import { Input } from '@/components/ui/input';
+import { ScrollArea } from '@/components/ui/scroll-area.tsx';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs.tsx';
+import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip';
+import { LucideIconRender } from '@/components/ui/vines-icon/lucide/render.tsx';
+import { useAppStore } from '@/store/useAppStore';
+import { getI18nContent } from '@/utils';
+import VinesEvent from '@/utils/events.ts';
 
 interface IVinesIconSelectorProps extends Omit<React.ComponentPropsWithoutRef<'div'>, 'onChange'> {
-  emojiLink?: string;
-  onChange?: (emojiLink: string) => void;
-  onFinished?: (emojiLink: string) => void;
-  onlyEmoji?: boolean;
+  onIconSelect?: (iconName: string) => void | Promise<void>;
 }
 
-const colors = ['#434343', '#f2c1be', '#fadebb', '#fef8a3', '#ceefc5', '#d1dcfb', '#d9caf8'];
+export const VinesIconSelector: React.FC<IVinesIconSelectorProps> = memo(({ children, onIconSelect }) => {
+  const { t } = useTranslation();
 
-export const VinesIconSelector: React.FC<IVinesIconSelectorProps> = memo(
-  ({ children, onChange, emojiLink, onFinished, onlyEmoji = false }) => {
-    const { t } = useTranslation();
-    const [theme] = useLocalStorage<string>('vines-ui-dark-mode', 'auto', false);
+  const [searchValue, setSearchValue] = useState('');
 
-    const [selectedColor, setSelectedColor] = useState(colors[4]);
-    const [selectedEmoji, setSelectedEmoji] = useState('🍀');
+  const initialized = useAppStore((s) => s.iconInitialized);
+  useEffect(() => {
+    if (!initialized) {
+      VinesEvent.emit('vines-trigger-init-icons');
+    }
+  }, [initialized]);
 
-    const [visible, setVisible] = useState(false);
+  const iconMetadata = useAppStore((s) => s.iconMetadata);
 
-    useEffect(() => {
-      const nextEmoji = onlyEmoji ? selectedEmoji : `emoji:${selectedEmoji}:${selectedColor}`;
-      if (emojiLink !== nextEmoji && visible) {
-        onChange?.(nextEmoji);
-      }
-    }, [selectedColor, selectedEmoji, visible]);
-
-    useEffect(() => {
-      if (emojiLink && emojiLink.startsWith('emoji:')) {
-        const [, emoji, bgColor] = emojiLink.split(':');
-        if (!bgColor) {
-          setSelectedColor(colors[0]);
-        } else {
-          setSelectedColor(bgColor);
-        }
-        setSelectedEmoji(emoji);
-      }
-    }, [emojiLink]);
-
-    return (
-      <Popover open={visible} onOpenChange={setVisible}>
-        <PopoverTrigger asChild>{children}</PopoverTrigger>
-        <PopoverContent
-          className="h-[512px] w-[352px] scale-75 overflow-hidden rounded-xl bg-slate-2 p-0"
-          sideOffset={-55}
-        >
-          <Picker
-            i18n={i18n}
-            data={data}
-            onEmojiSelect={(e: any) => {
-              if (onlyEmoji) {
-                onChange?.(e.native);
+  const iconCateMap = useMemo(() => {
+    return ICONS_CATEGORIES.reduce(
+      (acc, { name: cateName, label }) => {
+        acc[cateName] =
+          cateName === 'all'
+            ? {
+                icons: Object.keys(iconMetadata).reduce((acc2, iconName) => {
+                  if (searchValue != '') {
+                    if (iconName.includes(searchValue) || iconMetadata[iconName].tags.includes(searchValue)) {
+                      acc2.push(iconName);
+                    }
+                  } else {
+                    acc2.push(iconName);
+                  }
+                  return acc2;
+                }, [] as string[]),
+                label,
               }
-              setSelectedEmoji(e.native);
-            }}
-            locale="zh"
-            previewPosition="none"
-            theme={theme}
-          />
-          <div className="flex h-20 w-full items-center justify-between gap-2 px-5 pb-1">
-            <div className={onlyEmoji ? 'hidden' : ''}>
-              <p className="mb-2 text-sm">{t('components.ui.icon-selector.background-color')}</p>
-              <div className="flex items-center gap-1">
-                {colors.map((color, index) => (
-                  <div
-                    key={index}
-                    className={cn(
-                      'mx-1 h-6 w-6 cursor-pointer rounded-full outline outline-[6px] outline-transparent transition-all',
-                      color === selectedColor && 'outline-gray-4',
-                    )}
-                    style={{ backgroundColor: color }}
-                    onClick={() => setSelectedColor(color)}
-                  />
-                ))}
-              </div>
-            </div>
-            <Button
-              className={onlyEmoji ? 'ml-auto' : ''}
-              variant="solid"
-              onClick={() => {
-                onFinished?.(onlyEmoji ? selectedEmoji : `emoji:${selectedEmoji}:${selectedColor}`);
-                setVisible(false);
-              }}
-            >
-              OK
-            </Button>
-          </div>
-        </PopoverContent>
-      </Popover>
+            : {
+                icons: Object.keys(iconMetadata).reduce((acc2, iconName) => {
+                  if (iconMetadata[iconName].categories.includes(cateName)) {
+                    if (searchValue != '') {
+                      if (iconName.includes(searchValue) || iconMetadata[iconName].tags.includes(searchValue)) {
+                        acc2.push(iconName);
+                      }
+                    } else {
+                      acc2.push(iconName);
+                    }
+                  }
+                  return acc2;
+                }, [] as string[]),
+                label,
+              };
+        return acc;
+      },
+      {} as Record<string, { icons: string[]; label: I18nValue }>,
     );
-  },
-);
+  }, [initialized, searchValue]);
+
+  const [activeTab, setActiveTab] = useState('all');
+
+  const tabsNode = useRef<HTMLDivElement>(null);
+
+  return (
+    <div className="flex h-[calc(100vh-10rem)] w-full flex-col gap-2">
+      <div className="relative flex w-full items-center">
+        <Search className="absolute ml-3 size-4 shrink-0 opacity-50" />
+        <Input
+          className="pl-9"
+          placeholder={t('components.ui.icon-selector.search.placeholder')}
+          onChange={setSearchValue}
+        />
+      </div>
+      <Tabs value={activeTab} onValueChange={setActiveTab}>
+        <div className="flex justify-between">
+          <div ref={tabsNode} className="max-w-[400px] overflow-x-hidden overflow-y-clip pr-2">
+            <TabsList>
+              {Object.keys(iconCateMap).map((cateName) => (
+                <TabsTrigger value={cateName} key={cateName}>
+                  {getI18nContent(iconCateMap[cateName].label)}
+                </TabsTrigger>
+              ))}
+            </TabsList>
+          </div>
+          <div className="relative flex items-center">
+            <div className="pointer-events-none absolute -left-4 h-full w-10 bg-gradient-to-l from-background from-60%" />
+            <Button
+              icon={<ChevronLeft size={16} />}
+              variant="outline"
+              className="!scale-75"
+              onClick={() => tabsNode.current?.scrollBy({ left: -200, behavior: 'smooth' })}
+            />
+            <Button
+              icon={<ChevronRight size={16} />}
+              variant="outline"
+              className="!scale-75"
+              onClick={() => tabsNode.current?.scrollBy({ left: 200, behavior: 'smooth' })}
+            />
+          </div>
+        </div>
+        {Object.keys(iconCateMap).map((cateName) => (
+          <TabsContent value={cateName} key={cateName}>
+            <ScrollArea className="h-[calc(100vh-16rem)]">
+              <div className="flex h-full flex-wrap gap-2">
+                {iconCateMap[cateName].icons.map((iconName) => {
+                  return (
+                    <Tooltip key={iconName}>
+                      <TooltipTrigger>
+                        <div
+                          className="cursor-pointer rounded-md bg-gray-3 bg-opacity-60 p-3 transition-all hover:bg-opacity-15"
+                          onClick={() => {
+                            onIconSelect?.(iconName);
+                          }}
+                        >
+                          <LucideIconRender src={iconName} className="size-6" />
+                        </div>
+                      </TooltipTrigger>
+                      <TooltipContent side="bottom">{iconName}</TooltipContent>
+                    </Tooltip>
+                  );
+                })}
+              </div>
+            </ScrollArea>
+          </TabsContent>
+        ))}
+      </Tabs>
+    </div>
+  );
+});
 
 VinesIconSelector.displayName = 'VinesIconSelector';
