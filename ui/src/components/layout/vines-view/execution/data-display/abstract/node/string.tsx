8e7acd3f4ff611e0bd41457feb9a4af0637c5f06
LF112 <lf@lf112.net>
2024-07-31 23:47:38 +0800

    prev: abstract string data use markdown render

--- ui/src/components/layout/vines-view/execution/data-display/abstract/node/string.tsx
+++ ui/src/components/layout/vines-view/execution/data-display/abstract/node/string.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 
 import { JSONValue } from '@/components/ui/code-editor';
+import { VinesMarkdown } from '@/components/ui/markdown';
 
 interface IVinesAbstractStringProps {
   children: JSONValue;
 }
 
 export const VinesAbstractString: React.FC<IVinesAbstractStringProps> = ({ children }) => {
-  return (
-    <div className="prose break-words text-sm dark:prose-invert prose-p:leading-relaxed prose-pre:p-0">
-      {children?.toString()}
-    </div>
-  );
+  return <VinesMarkdown allowHtml>{children?.toString()}</VinesMarkdown>;
 };
