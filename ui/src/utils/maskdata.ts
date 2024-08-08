e7fa98ecd6ae6b059f84c4635d1c39c6a1f92b1c
LF112 <lf@lf112.net>
2024-08-08 09:47:37 +0800

    fix: maskdata

--- ui/src/utils/maskdata.ts
+++ ui/src/utils/maskdata.ts
@@ -1,4 +1,5 @@
 import MaskData from 'maskdata';
+import * as validator from 'validator';
 
 export const maskPhone = (phone: string) =>
   MaskData.maskPhone(phone, {
@@ -7,7 +8,10 @@ export const maskPhone = (phone: string) =>
     unmaskedEndDigits: 4,
   });
 
-export const maskEmail = (email: string) => MaskData.maskEmail2(email);
+export const maskEmail = (email: string) => {
+  if (!validator.isEmail(email)) return email;
+  return MaskData.maskEmail2(email);
+};
 
 export const maskPassword = (password: string) =>
   MaskData.maskPassword(password, { unmaskedStartCharacters: 8, unmaskedEndCharacters: 6, maxMaskedCharacters: 30 });
