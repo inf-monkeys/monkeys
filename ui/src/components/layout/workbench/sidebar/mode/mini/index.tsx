5888a90a45a38ef717f0200fbcf5df8c40806ac9
LF112 <lf@lf112.net>
2024-08-24 17:31:01 +0800

    prev: optimizing virtual lists of workbench mini mode

--- ui/src/components/layout/workbench/sidebar/mode/mini/index.tsx
+++ ui/src/components/layout/workbench/sidebar/mode/mini/index.tsx
@@ -2,18 +2,15 @@ import React, { useRef, useState } from 'react';
 
 import { useCreation, useDebounceEffect, useThrottleEffect } from 'ahooks';
 import { useTranslation } from 'react-i18next';
-import { Virtuoso } from 'react-virtuoso';
 
 import { useWorkspacePages } from '@/apis/pages';
 import { IPinPage } from '@/apis/pages/typings.ts';
-import { ScrollArea } from '@/components/ui/scroll-area.tsx';
+import { VirtuaWorkbenchMiniViewList } from '@/components/layout/workbench/sidebar/mode/mini/virtua';
 import { Separator } from '@/components/ui/separator.tsx';
-import { VinesIcon } from '@/components/ui/vines-icon';
 import { VINES_IFRAME_PAGE_TYPE2ID_MAPPER } from '@/components/ui/vines-iframe/consts.ts';
 import { useLocalStorage } from '@/hooks/use-local-storage';
 import { useElementSize } from '@/hooks/use-resize-observer';
 import useUrlState from '@/hooks/use-url-state.ts';
-import { cn, getI18nContent } from '@/utils';
 
 interface IWorkbenchMiniModeSidebarProps extends React.ComponentPropsWithoutRef<'div'> {}
 
@@ -80,44 +77,15 @@ export const WorkbenchMiniModeSidebar: React.FC<IWorkbenchMiniModeSidebarProps> 
     { wait: 64 },
   );
 
-  const scrollAreaRef = useRef<HTMLDivElement>(null);
-
   return (
     <div className="flex h-full w-20 py-2" ref={ref}>
       <div className="-mt-2 flex w-full flex-col gap-4 px-2">
-        <ScrollArea ref={scrollAreaRef} style={{ height }} disabledOverflowMask>
-          <Virtuoso
-            style={{ height }}
-            data={originalPages}
-            customScrollParent={scrollAreaRef.current as HTMLElement}
-            itemContent={(_, page) => {
-              if (!page) {
-                return null;
-              }
-
-              const info = page?.workflow || page?.agent;
-              const pageId = page?.id ?? '';
-
-              return (
-                <div
-                  key={pageId}
-                  className={cn(
-                    'mt-2 flex cursor-pointer flex-col items-center justify-center gap-1 rounded-md border border-transparent py-2 transition-colors hover:bg-accent hover:text-accent-foreground',
-                    currentPage?.id === pageId && 'border-input bg-background text-accent-foreground',
-                  )}
-                  onClick={() => {
-                    setCurrentPage(page);
-                  }}
-                >
-                  <VinesIcon size="sm">{info?.iconUrl}</VinesIcon>
-                  <span className="text-xxs text-center">
-                    {getI18nContent(info?.displayName) ?? t('common.utils.untitled')}
-                  </span>
-                </div>
-              );
-            }}
-          />
-        </ScrollArea>
+        <VirtuaWorkbenchMiniViewList
+          data={originalPages}
+          height={height}
+          currentPageId={currentPage?.id}
+          onItemClicked={setCurrentPage}
+        />
       </div>
       <Separator orientation="vertical" />
     </div>
