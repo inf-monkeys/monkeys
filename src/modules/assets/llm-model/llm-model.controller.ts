a4109ee46c6bfab2b3e90f6f2ab6afa70b7b8759
LF112 <lf@lf112.net>
2024-09-05 19:44:55 +0800

    prev: update

--- src/modules/assets/llm-model/llm-model.controller.ts
+++ src/modules/assets/llm-model/llm-model.controller.ts
@@ -3,8 +3,7 @@ import { CompatibleAuthGuard } from '@/common/guards/auth.guard';
 import { WorkflowAuthGuard } from '@/common/guards/workflow-auth.guard';
 import { SuccessListResponse, SuccessResponse } from '@/common/response';
 import { IRequest } from '@/common/typings/request';
-import { LlmModelEntity, UpdateLlmModelParams } from '@/database/entities/assets/model/llm-model/llm-model';
-import { getModels } from '@/modules/tools/llm/llm.service';
+import { UpdateLlmModelParams } from '@/database/entities/assets/model/llm-model/llm-model';
 import { Body, Controller, Delete, Get, Param, Put, Query, Req, UseGuards } from '@nestjs/common';
 import { LlmModelService } from './llm-model.service';
 
@@ -16,31 +15,7 @@ export class LlmModelController {
   @Get('')
   public async listLlmModels(@Req() req: IRequest, @Query() dto: ListDto) {
     const { teamId } = req;
-    let { list } = await this.service.listLlmModels(teamId, dto);
-    const systemModels = getModels();
-
-    if (systemModels.length) {
-      const systemChannels = [] as LlmModelEntity[];
-
-      for (const { name, value, icon, desc } of systemModels) {
-        systemChannels.push({
-          assetType: 'llm-model',
-          channelId: 0,
-          channelType: 1,
-          displayName: name,
-          description: desc || {
-            'zh-CN': '系统内置大语言模型，由 OpenAI 标准接口提供',
-            'en-US': 'The system has a built-in large language model, provided by the OpenAI standard interface',
-          },
-          iconUrl: icon || 'https://monkeyminio01.daocloud.cn/monkeys/icons/openai.webp',
-          models: { [value]: value },
-          id: `0-${value}`,
-        } as LlmModelEntity);
-      }
-
-      list = [...list, ...systemChannels];
-    }
-
+    const list = await this.service.listLlmModels(teamId, dto);
     return new SuccessListResponse({
       data: list,
       total: list.length,
