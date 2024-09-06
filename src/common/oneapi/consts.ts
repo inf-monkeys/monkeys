f826771789898cb1e3f0b3ad14709233e5e450a6
LF112 <lf@lf112.net>
2024-09-06 15:45:04 +0800

    prev: support more model supplier

--- src/common/oneapi/consts.ts
+++ src/common/oneapi/consts.ts
@@ -71,8 +71,8 @@ export const DEFAULT_ONEAPI_CHANNEL_PROPERITIES: ToolProperty[] = [
     type: 'string',
     required: false,
     description: {
-      'zh-CN': '可选，不填默认使用官方的默认地址',
-      'en-US': 'Optional, if not filled in, the official default address will be used by default',
+      'zh-CN': '可选，不填默认使用官方的默认地址，如 https://domain.com',
+      'en-US': 'Optional, if not filled in, the official default address will be used by default, such as https://domain.com',
     },
     typeOptions: {
       foldUp: true,
@@ -92,16 +92,6 @@ export const ONEAPI_CHANNELS: Partial<LlmChannelEntity>[] = [
         'OpenAI 是一家成立于 2015 年 12 月的美国人工智能研究组织，研究人工智能的目标是开发“安全和有益”的人工智能通用智能，OpenAI 将其定义为“高度自主的系统，在大多数经济上有价值的工作中胜过人类。',
     },
     iconUrl: 'https://monkeyminio01.daocloud.cn/monkeys/icons/openai.webp',
-    properites: DEFAULT_ONEAPI_CHANNEL_PROPERITIES,
-  },
-  {
-    id: '14',
-    displayName: 'Anthropic Claude',
-    description: {
-      'en-US': 'Claude is a family of large language models developed by Anthropic. The first model was released in March 2023. Claude 3, released in March 2024, can also analyze images',
-      'zh-CN': 'Claude 是由 Anthropic 开发的大型语言模型系列。第一个模型于 2023 年 3 月发布。2024 年 3 月发布的 Claude 3 还可以分析图像。',
-    },
-    iconUrl: 'https://monkeyminio01.daocloud.cn/monkeys/icons/claude.webp',
     properites: DEFAULT_ONEAPI_CHANNEL_PROPERITIES,
   },
   {
@@ -178,6 +168,78 @@ export const ONEAPI_CHANNELS: Partial<LlmChannelEntity>[] = [
     ],
   },
   {
+    id: '14',
+    displayName: 'Anthropic Claude',
+    description: {
+      'en-US': 'Claude is a family of large language models developed by Anthropic. The first model was released in March 2023. Claude 3, released in March 2024, can also analyze images',
+      'zh-CN': 'Claude 是由 Anthropic 开发的大型语言模型系列。第一个模型于 2023 年 3 月发布。2024 年 3 月发布的 Claude 3 还可以分析图像。',
+    },
+    iconUrl: 'https://monkeyminio01.daocloud.cn/monkeys/icons/claude.webp',
+    properites: DEFAULT_ONEAPI_CHANNEL_PROPERITIES,
+  },
+  {
+    id: '33',
+    displayName: 'AWS',
+    description: {
+      'zh-CN': '在 AWS 上开启人工智能创新 — 借助人工智能工具、解决方案和基础设施领域久经考验的领导者，实现运营转型。',
+      'en-US': 'Unlock AI innovation on AWS - Transform your operations with the proven leader in artificial intelligence tools, solutions, and infrastructure.',
+    },
+    iconUrl: 'https://monkeyminio01.daocloud.cn/monkeys/icons/aws.webp',
+    properites: [
+      {
+        displayName: {
+          'zh-CN': '区域',
+          'en-US': 'Region',
+        },
+        name: 'region',
+        type: 'string',
+        required: true,
+        placeholder: 'region，e.g. us-west-2',
+        description: {
+          'zh-CN': '在此处填写 AWS 区域，例如：us-west-2',
+          'en-US': 'Enter the AWS region here, for example: us-west-2',
+        },
+      },
+      {
+        displayName: 'AK',
+        name: 'ak',
+        type: 'string',
+        required: true,
+        placeholder: 'AWS IAM Access Key',
+        description: {
+          'zh-CN': '在此处填写 AWS IAM Access Key',
+          'en-US': 'Enter the AWS IAM Access Key here',
+        },
+      },
+      {
+        displayName: 'SK',
+        name: 'sk',
+        type: 'string',
+        required: true,
+        placeholder: 'AWS IAM Secret Key',
+        description: {
+          'zh-CN': '在此处填写 AWS IAM Secret Key',
+          'en-US': 'Enter the AWS IAM Secret Key here',
+        },
+      },
+      {
+        displayName: {
+          'zh-CN': '模型列表',
+          'en-US': 'Model List',
+        },
+        name: 'models',
+        type: 'string',
+        required: true,
+        default: [],
+        typeOptions: {
+          assetType: 'oneapi-model',
+          multipleValues: true,
+        },
+      },
+      ...COMMON_DEFAULT_CHANNEL_PROPERITIES,
+    ],
+  },
+  {
     id: '11',
     displayName: 'Google PaLM2',
     description: {
@@ -208,6 +270,78 @@ export const ONEAPI_CHANNELS: Partial<LlmChannelEntity>[] = [
       'en-US':
         'Mistral AI is a French company specializing in artificial intelligence products. Founded in April 2023 by former employees of Meta Platforms and Google DeepMind, the company has quickly risen to prominence in the AI sector.',
       'zh-CN': 'Mistral AI 是一家专门从事人工智能产品的法国公司。该公司成立于 2023 年 4 月，由 Meta Platforms 和 Google DeepMind 的前员工创立，公司迅速在人工智能领域崭露头角。',
+    },
+  },
+  {
+    id: '41',
+    displayName: 'Novita',
+    description: {
+      'en-US':
+        'Novita AI is an all-in-one AI cloud solution that empowers businesses with open-source model APIs, serverless GPUs, and on-demand GPU instances. Drive innovation and gain a competitive edge with the power of Novita AI.',
+      'zh-CN': 'Novita AI 是一家一体化的 AI 云解决方案提供商，为企业和开发者提供开源模型 API、无服务器 GPU 和按需 GPU 实例。通过 Novita AI 的力量推动创新并获得竞争优势。',
+    },
+    iconUrl: 'https://monkeyminio01.daocloud.cn/monkeys/icons/novita.webp',
+    properites: DEFAULT_ONEAPI_CHANNEL_PROPERITIES,
+  },
+  {
+    id: '40',
+    displayName: {
+      'zh-CN': '字节跳动豆包',
+      'en-US': 'ByteDance Doubao',
+    },
+    iconUrl: 'https://monkeyminio01.daocloud.cn/monkeys/icons/doubao.webp',
+    properites: [
+      {
+        displayName: {
+          'zh-CN': '密钥',
+          'en-US': 'Key',
+        },
+        name: 'key',
+        type: 'string',
+        required: true,
+        description: {
+          'zh-CN': '在此处填写模型供应商提供的 APIKEY',
+          'en-US': 'Enter the APIKEY provided by the model supplier here',
+        },
+      },
+      {
+        displayName: {
+          'zh-CN': '模型列表',
+          'en-US': 'Model List',
+        },
+        description: {
+          'zh-CN': '对于豆包而言，需要手动去「模型推理页面」创建推理接入点，以接入点名称作为模型名称，例如：ep-20240608051426-tkxvl',
+          'en-US':
+            'For Doubao, you need to manually go to the "Model Inference Page" to create an inference access point, and use the access point name as the model name, for example: ep-20240608051426-tkxvl',
+        },
+        name: 'models',
+        type: 'string',
+        required: true,
+        default: [],
+        typeOptions: {
+          assetType: 'oneapi-model',
+          multipleValues: true,
+        },
+      },
+      {
+        displayName: 'Base URL',
+        name: 'base_url',
+        type: 'string',
+        required: false,
+        description: {
+          'zh-CN': '可选，不填默认使用官方的默认地址，如 https://domain.com',
+          'en-US': 'Optional, if not filled in, the official default address will be used by default, such as https://domain.com',
+        },
+        typeOptions: {
+          foldUp: true,
+        },
+      },
+      ...COMMON_DEFAULT_CHANNEL_PROPERITIES,
+    ],
+    description: {
+      'zh-CN': '字节跳动推出的自研大模型。通过字节跳动内部50+业务场景实践验证，每日千亿级 tokens 大使用量持续打磨，提供多模态能力，以优质模型效果为企业打造丰富的业务体验。',
+      'en-US':
+        'Doubao is a self-developed large model launched by ByteDance. It has been verified through 50+ business scenarios within ByteDance, with a daily usage of over 100 billion tokens, continuously polishing with high-quality model effects to provide rich business experiences for enterprises.',
     },
   },
   {
@@ -443,37 +577,259 @@ export const ONEAPI_CHANNELS: Partial<LlmChannelEntity>[] = [
     },
   },
   {
-    id: '40',
+    id: '32',
     displayName: {
-      'zh-CN': '字节跳动豆包',
-      'en-US': 'ByteDance Doubao',
+      'zh-CN': '阶跃星辰',
+      'en-US': 'StepFun',
     },
-    iconUrl: 'https://monkeyminio01.daocloud.cn/monkeys/icons/doubao.webp',
+    iconUrl: 'https://monkeyminio01.daocloud.cn/monkeys/icons/jieyuexingchen.webp',
+    properites: DEFAULT_ONEAPI_CHANNEL_PROPERITIES,
+    description: {
+      'zh-CN':
+        '阶跃星辰于2023年4月成立，以“智能阶跃，十倍每一个人的可能”为使命。阶跃星辰坚定自研超级模型，积极布局算力、数据等关键资源，发挥算法和人才优势，已完成 Step-1 千亿参数语言大模型和 Step-1V 千亿多模态大模型的研发，在图像理解、多轮指令跟随、数学能力、逻辑推理、文本创作等方面性能达到业界领先水平。',
+      'en-US':
+        'StepFun was founded in April 2023 with the mission of "Intelligent Step, Ten Times the Possibility of Every Person". StepFun resolutely develops its own super model, actively plans and optimizes key resources such as computing power and data, and leverages its algorithm and talent advantages to complete the development of the Step-1 100 billion parameter language large model and the Step-1V 100 billion multimodal large model, achieving industry-leading performance in image understanding, multi-round instruction following, mathematical ability, logical reasoning, and text creation.',
+    },
+  },
+  {
+    id: '34',
+    displayName: {
+      'zh-CN': '扣子 Coze',
+      'en-US': 'Coze',
+    },
+    description: {
+      'en-US':
+        'Coze is a next-generation AI application and chatbot developing platform for everyone. Regardless of your programming experience, Coze enables you to effortlessly create various chatbots and deploy them across different social platforms and messaging apps.',
+      'zh-CN': '扣子 Coze 是一个面向所有人的下一代 AI 应用和聊天机器人开发平台。无论您是否有编程经验，Coze 都让您可以轻松创建各种聊天机器人，并将其部署到不同的社交平台和消息应用中。',
+    },
+    iconUrl: 'https://monkeyminio01.daocloud.cn/monkeys/icons/coze.webp',
     properites: [
       {
         displayName: {
-          'zh-CN': '对于豆包而言，需要手动去「模型推理页面」创建推理接入点，以接入点名称作为模型名称，例如：ep-20240608051426-tkxvl',
-          'en-US':
-            'For Doubao, you need to manually go to the "Model Inference Page" to create an inference access point, and use the access point name as the model name, for example: ep-20240608051426-tkxvl',
+          'zh-CN': '模型列表',
+          'en-US': 'Model List',
         },
-        type: 'notice',
-        name: 'docs',
+        description: {
+          'zh-CN': '对于 Coze 而言，模型名称即 Bot ID，你可以添加一个前缀 `bot-`，例如：`bot-123456`。',
+          'en-US': 'For Coze, the model name is the Bot ID, you can add a prefix `bot-`, for example: `bot-123456`.',
+        },
+        name: 'models',
+        type: 'string',
+        required: true,
+        default: [],
+        typeOptions: {
+          assetType: 'oneapi-model',
+          multipleValues: true,
+        },
+      },
+      {
+        displayName: 'User ID',
+        name: 'user_id',
+        type: 'string',
+        required: true,
+        placeholder: '生成该密钥的用户 ID',
+        description: {
+          'zh-CN': '生成该密钥的用户 ID',
+          'en-US': 'The user ID that generated this key',
+        },
+      },
+      {
+        displayName: {
+          'zh-CN': '密钥',
+          'en-US': 'Key',
+        },
+        name: 'key',
+        type: 'string',
+        required: true,
+        description: {
+          'zh-CN': '在此处填写模型供应商提供的 APIKEY',
+          'en-US': 'Enter the APIKEY provided by the model supplier here',
+        },
+      },
+      {
+        displayName: 'Base URL',
+        name: 'base_url',
+        type: 'string',
+        required: false,
+        description: {
+          'zh-CN': '可选，不填默认使用官方的默认地址，如 https://domain.com',
+          'en-US': 'Optional, if not filled in, the official default address will be used by default, such as https://domain.com',
+        },
+        typeOptions: {
+          foldUp: true,
+        },
+      },
+      ...COMMON_DEFAULT_CHANNEL_PROPERITIES,
+    ],
+  },
+  {
+    id: '35',
+    displayName: {
+      'zh-CN': 'Cohere',
+      'en-US': 'Cohere',
+    },
+    iconUrl: 'https://monkeyminio01.daocloud.cn/monkeys/icons/cohere.webp',
+    properites: DEFAULT_ONEAPI_CHANNEL_PROPERITIES,
+    description: {
+      'en-US': 'Cohere provides industry-leading large language models (LLMs) and RAG capabilities tailored to meet the needs of enterprise use cases that solve real-world problems.',
+      'zh-CN': 'Cohere 提供行业领先的大型语言模型 (LLMs) 和 RAG 能力，以满足解决现实世界问题的企业用例的需求。',
+    },
+  },
+  {
+    id: '36',
+    displayName: {
+      'zh-CN': '深度求索 DeepSeek',
+      'en-US': 'DeepSeek',
+    },
+    iconUrl: 'https://monkeyminio01.daocloud.cn/monkeys/icons/deepseek.webp',
+    properites: DEFAULT_ONEAPI_CHANNEL_PROPERITIES,
+    description: {
+      'en-US': 'DeepSeek, unravel the mystery of AGI with curiosity. Answer the essential question with long-termism.',
+      'zh-CN': '深度求索，以好奇心解开 AGI 的神秘面纱，以长期主义回答根本问题。',
+    },
+  },
+  {
+    id: '37',
+    displayName: {
+      'zh-CN': 'Cloudflare',
+      'en-US': 'Cloudflare',
+    },
+    iconUrl: 'https://monkeyminio01.daocloud.cn/monkeys/icons/cloudflare.webp',
+    properites: [
+      {
+        displayName: 'Account ID',
+        name: 'user_id',
+        type: 'string',
+        required: true,
+        placeholder: '请输入 Account ID，例如：d8d7c61dbc244c32d3ced280e4bf42b4',
+        description: {
+          'zh-CN': 'Account ID，例如：d8d7c61dbc244c32d3ced280e4bf42b4',
+          'en-US': 'Account ID, for example: d8d7c61dbc244c32d3ced280e4bf42b4',
+        },
       },
       ...DEFAULT_ONEAPI_CHANNEL_PROPERITIES,
     ],
     description: {
-      'zh-CN': '字节跳动推出的自研大模型。通过字节跳动内部50+业务场景实践验证，每日千亿级 tokens 大使用量持续打磨，提供多模态能力，以优质模型效果为企业打造丰富的业务体验。',
+      'en-US': 'Run fast, low-latency inference tasks on pre-trained machine learning models natively on Cloudflare Workers.',
+      'zh-CN': '在 Cloudflare Workers 上原生运行预训练机器学习模型的快速、低延迟推理任务。',
+    },
+  },
+  {
+    id: '38',
+    displayName: {
+      'zh-CN': 'DeepL',
+      'en-US': 'DeepL',
+    },
+    iconUrl: 'https://monkeyminio01.daocloud.cn/monkeys/icons/deepl.webp',
+    properites: DEFAULT_ONEAPI_CHANNEL_PROPERITIES,
+    description: {
+      'en-US': 'DeepL Write is a tool that helps you perfect your writing. Write clearly, precisely, with ease, and without errors.',
+      'zh-CN': 'DeepL Write 是一个帮助您完善写作的工具。清晰、准确、轻松地写作，没有错误。',
+    },
+  },
+  {
+    id: '39',
+    displayName: 'together.ai',
+    description: {
+      'en-US': 'Build gen AI models with Together AI. Benefit from the fastest and most cost-efficient tools and infra. Collaborate with our expert AI team that’s dedicated to your success.',
+      'zh-CN': '与我们的专家 AI 团队合作，共同构建生成式 AI 模型。利用最快的工具和基础设施，从我们的 AI 专家那里获得帮助，以实现您的成功。',
+    },
+    iconUrl: 'https://monkeyminio01.daocloud.cn/monkeys/icons/together_ai.webp',
+    properites: DEFAULT_ONEAPI_CHANNEL_PROPERITIES,
+  },
+  {
+    id: '42',
+    displayName: 'VertexAI',
+    iconUrl: 'https://monkeyminio01.daocloud.cn/monkeys/icons/vertexai.webp',
+    properites: [
+      {
+        name: 'region',
+        displayName: 'Region',
+        type: 'string',
+        required: true,
+        placeholder: 'Vertex AI Region.g. us-east5',
+        description: {
+          'zh-CN': 'Vertex AI 区域，例如：us-east5',
+          'en-US': 'Vertex AI Region.g. us-east5',
+        },
+      },
+      {
+        name: 'vertex_ai_project_id',
+        displayName: 'Project ID',
+        type: 'string',
+        required: true,
+        placeholder: 'Vertex AI Project ID',
+        description: 'Vertex AI Project ID',
+      },
+      {
+        name: 'vertex_ai_adc',
+        displayName: 'Google Cloud Application Default Credentials JSON',
+        type: 'string',
+        required: true,
+        placeholder: 'Google Cloud Application Default Credentials JSON',
+        description: 'Google Cloud Application Default Credentials JSON',
+      },
+      ...DEFAULT_ONEAPI_CHANNEL_PROPERITIES.slice(1),
+    ],
+    description: {
       'en-US':
-        'Doubao is a self-developed large model launched by ByteDance. It has been verified through 50+ business scenarios within ByteDance, with a daily usage of over 100 billion tokens, continuously polishing with high-quality model effects to provide rich business experiences for enterprises.',
+        'Vertex AI is a machine learning (ML) platform that lets you train and deploy ML models and AI applications, and customize large language models (LLMs) for use in your AI-powered applications. Vertex AI combines data engineering, data science, and ML engineering workflows, enabling your teams to collaborate using a common toolset and scale your applications using the benefits of Google Cloud.',
+      'zh-CN':
+        'Vertex AI 是一个机器学习 (ML) 平台，可让您训练和部署机器学习模型和 AI 应用，以及自定义大型语言模型 (LLM)，以在 AI 驱动的应用中使用。Vertex AI 结合了数据工程、数据科学和机器学习工程工作流，使您的团队能够使用通用工具集进行协作，并利用 Google Cloud 的优势扩缩您的应用。',
+    },
+  },
+  {
+    id: '43',
+    displayName: 'Proxy',
+    iconUrl: 'https://monkeyminio01.daocloud.cn/monkeys/icons/openai.webp',
+    properites: [
+      {
+        displayName: {
+          'zh-CN': '密钥',
+          'en-US': 'Key',
+        },
+        name: 'key',
+        type: 'string',
+        required: true,
+        description: {
+          'zh-CN': '在此处填写模型供应商提供的 APIKEY',
+          'en-US': 'Enter the APIKEY provided by the model supplier here',
+        },
+      },
+      {
+        displayName: 'Base URL',
+        name: 'base_url',
+        type: 'string',
+        required: false,
+        description: {
+          'zh-CN': '可选，不填默认使用官方的默认地址，如 https://domain.com',
+          'en-US': 'Optional, if not filled in, the official default address will be used by default, such as https://domain.com',
+        },
+      },
+      ...COMMON_DEFAULT_CHANNEL_PROPERITIES,
+    ],
+    description: {
+      'zh-CN': 'OpenAI 标准接口代理',
+      'en-US': 'OpenAI Standard Interface Proxy',
+    },
+  },
+  {
+    id: '44',
+    displayName: 'SiliconFlow',
+    iconUrl: 'https://monkeyminio01.daocloud.cn/monkeys/icons/siliconflow.webp',
+    properites: DEFAULT_ONEAPI_CHANNEL_PROPERITIES,
+    description: {
+      'zh-CN': '硅基流动致力于打造规模化、标准化、高效能 AI Infra 平台，提供高效能、低成本的多品类AI 模型服务，助力开发者和企业聚焦产品创新。',
+      'en-US': 'SiliconFlow is a platform that provides a comprehensive set of tools and resources for building and deploying AI applications.',
     },
   },
 ];
 
 export const CHANNEL_OPTIONS = [
   { key: 1, text: 'OpenAI', displayName: { 'zh-CN': 'OpenAI', 'en-US': 'OpenAI' }, value: 1, color: 'green' },
+  { key: 3, text: 'Azure OpenAI', displayName: { 'zh-CN': 'Azure OpenAI', 'en-US': 'Azure OpenAI' }, value: 3, color: 'olive' },
   { key: 14, text: 'Anthropic Claude', displayName: { 'zh-CN': 'Anthropic Claude', 'en-US': 'Anthropic Claude' }, value: 14, color: 'black' },
   { key: 33, text: 'AWS', displayName: { 'zh-CN': 'AWS', 'en-US': 'AWS' }, value: 33, color: 'black' },
-  { key: 3, text: 'Azure OpenAI', displayName: { 'zh-CN': 'Azure OpenAI', 'en-US': 'Azure OpenAI' }, value: 3, color: 'olive' },
   { key: 11, text: 'Google PaLM2', displayName: { 'zh-CN': 'Google PaLM2', 'en-US': 'Google PaLM2' }, value: 11, color: 'orange' },
   { key: 24, text: 'Google Gemini', displayName: { 'zh-CN': 'Google Gemini', 'en-US': 'Google Gemini' }, value: 24, color: 'orange' },
   { key: 28, text: 'Mistral AI', displayName: { 'zh-CN': 'Mistral AI', 'en-US': 'Mistral AI' }, value: 28, color: 'orange' },
