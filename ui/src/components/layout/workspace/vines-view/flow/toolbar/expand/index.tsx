dafd22d28938fa2fffc12968dc5e9a03ccf24e9a
LF112 <lf@lf112.net>
2024-09-10 22:06:36 +0800

    feat: hidden workflow version

--- ui/src/components/layout/workspace/vines-view/flow/toolbar/expand/index.tsx
+++ ui/src/components/layout/workspace/vines-view/flow/toolbar/expand/index.tsx
@@ -2,13 +2,9 @@ import React from 'react';
 
 import { VinesExpandToolErrors } from '@/components/layout/workspace/vines-view/flow/toolbar/expand/errors';
 import { VinesRunInsideToolbar } from '@/components/layout/workspace/vines-view/flow/toolbar/expand/execution';
-import { VinesVersionToolbar } from '@/components/layout/workspace/vines-view/flow/toolbar/expand/version';
-import { WorkflowRelease } from '@/components/layout/workspace/vines-view/flow/toolbar/expand/version/release.tsx';
-import { Card } from '@/components/ui/card.tsx';
 import { useVinesFlow } from '@/package/vines-flow';
 import { IVinesFlowRenderType } from '@/package/vines-flow/core/typings.ts';
 import { useCanvasStore } from '@/store/useCanvasStore';
-import { cn } from '@/utils';
 
 interface IVinesVersionToolbarProps extends React.ComponentPropsWithoutRef<'div'> {}
 
@@ -16,21 +12,21 @@ export const VinesExpandToolbar: React.FC<IVinesVersionToolbarProps> = () => {
   const isWorkflowRUNNING = useCanvasStore((s) => s.isWorkflowRUNNING);
 
   const { vines } = useVinesFlow();
-  const vinesVersion = vines.version;
-
-  const handleVersionChange = (version: number) => {
-    vines.update({ version });
-  };
+  // const vinesVersion = vines.version;
+  //
+  // const handleVersionChange = (version: number) => {
+  //   vines.update({ version });
+  // };
 
   const enableExecutionInside = vines.renderOptions.type === IVinesFlowRenderType.COMPLICATE;
 
   return (
     <div className="absolute right-0 top-0 z-40 m-4 flex items-center gap-2">
       {enableExecutionInside && <VinesRunInsideToolbar />}
-      <Card className={cn('flex flex-nowrap gap-2 p-2', isWorkflowRUNNING && 'hidden')}>
-        <VinesVersionToolbar version={vinesVersion} onVersionChange={handleVersionChange} />
-        <WorkflowRelease version={vinesVersion} onVersionChange={handleVersionChange} />
-      </Card>
+      {/*<Card className={cn('flex flex-nowrap gap-2 p-2', isWorkflowRUNNING && 'hidden')}>*/}
+      {/*  <VinesVersionToolbar version={vinesVersion} onVersionChange={handleVersionChange} />*/}
+      {/*  <WorkflowRelease version={vinesVersion} onVersionChange={handleVersionChange} />*/}
+      {/*</Card>*/}
       <VinesExpandToolErrors disabled={isWorkflowRUNNING} />
     </div>
   );
