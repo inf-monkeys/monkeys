776957e3765e90ca236b6eb526902020683e968e
LF112 <lf@lf112.net>
2024-11-06 21:39:37 +0800

    prev: update comp

--- ui/src/components/ui/vines-image/index.tsx
+++ ui/src/components/ui/vines-image/index.tsx
@@ -1,28 +1,18 @@
-import React, { memo } from 'react';
+import React, { lazy, Suspense } from 'react';
 
-import { useInViewport } from 'ahooks';
-import { AnimatePresence } from 'framer-motion';
+import { Skeleton } from '@/components/ui/skeleton.tsx';
 
-import { LazyImage } from '@/components/ui/vines-image/image.tsx';
-import { useElementSize } from '@/hooks/use-resize-observer.ts';
-import { cn } from '@/utils';
-
-interface IVinesImageProps {
+export interface IVinesImageProps {
   src: string;
   alt?: string;
 
   className?: string;
 }
 
-export const VinesImage: React.FC<IVinesImageProps> = memo(({ src, alt = 'image', className }) => {
-  const { ref, height, width } = useElementSize();
-  const [inViewport] = useInViewport(ref);
+const VinesImageCore = lazy(() => import('./vines-image-lazy.tsx'));
 
-  return (
-    <div ref={ref} className={cn('relative flex items-center justify-center', className)}>
-      <AnimatePresence>{inViewport && <LazyImage src={src} alt={alt} height={height} width={width} />}</AnimatePresence>
-    </div>
-  );
-});
-
-VinesImage.displayName = 'VinesImage';
+export const VinesImage: React.FC<IVinesImageProps> = (props) => (
+  <Suspense fallback={<Skeleton className="size-full min-h-52 min-w-52" />}>
+    <VinesImageCore {...props} />
+  </Suspense>
+);
