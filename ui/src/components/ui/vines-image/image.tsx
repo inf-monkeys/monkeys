06a0e17bab14b5b20757a506e53c98b05778987b
LF112 <lf@lf112.net>
2024-11-18 17:12:09 +0800

    prev: auto style

--- ui/src/components/ui/vines-image/image.tsx
+++ ui/src/components/ui/vines-image/image.tsx
@@ -18,53 +18,58 @@ interface ILazyImageProps {
 
   width?: number;
   height?: number;
+
+  style?: React.CSSProperties;
 }
 
-export const LazyImage: React.FC<ILazyImageProps> = memo(({ src, alt, width, height, disabledPreview = false }) => {
-  const [image, setImage] = useState<string>('');
+export const LazyImage: React.FC<ILazyImageProps> = memo(
+  ({ src, alt, width, height, style, disabledPreview = false }) => {
+    const [image, setImage] = useState<string>('');
 
-  const [themeMode] = useLocalStorage<string>('vines-ui-dark-mode', 'auto', false);
-  const isDark = themeMode === 'dark';
+    const [themeMode] = useLocalStorage<string>('vines-ui-dark-mode', 'auto', false);
+    const isDark = themeMode === 'dark';
 
-  const { data: cacheImageUrls } = useSWR<Record<string, string>>('vines-image-urls', null, {
-    fallbackData: {},
-  });
-
-  useEffect(() => {
-    VinesEvent.emit('vines-optimize-image', {
-      src,
-      width,
-      height,
-      callback: setImage,
+    const { data: cacheImageUrls } = useSWR<Record<string, string>>('vines-image-urls', null, {
+      fallbackData: {},
     });
-  }, []);
 
-  return image ? (
-    <Image
-      src={image}
-      alt={alt}
-      fallback={isDark ? '/fallback_image_dark.webp' : '/fallback_image.webp'}
-      preview={
-        disabledPreview
-          ? false
-          : {
-              src: cacheImageUrls?.[src] || src,
-              mask: <Eye className="stroke-white" />,
-            }
-      }
-    />
-  ) : (
-    <>
-      <motion.div
-        key="vines-image-loading-skeleton"
-        className="size-full animate-pulse rounded-md bg-muted"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
+    useEffect(() => {
+      VinesEvent.emit('vines-optimize-image', {
+        src,
+        width,
+        height,
+        callback: setImage,
+      });
+    }, []);
+
+    return image ? (
+      <Image
+        src={image}
+        alt={alt}
+        fallback={isDark ? '/fallback_image_dark.webp' : '/fallback_image.webp'}
+        style={style}
+        preview={
+          disabledPreview
+            ? false
+            : {
+                src: cacheImageUrls?.[src] || src,
+                mask: <Eye className="stroke-white" />,
+              }
+        }
       />
-      <VinesLoading key="vines-image-loading" className="absolute" />
-    </>
-  );
-});
+    ) : (
+      <>
+        <motion.div
+          key="vines-image-loading-skeleton"
+          className="size-full animate-pulse rounded-md bg-muted"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+        />
+        <VinesLoading key="vines-image-loading" className="absolute" />
+      </>
+    );
+  },
+);
 
 LazyImage.displayName = 'VinesLazyImage';
