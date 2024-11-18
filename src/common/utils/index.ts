d5a3274bbda9acb32c8d6f0c58f4fc3cabeb3256
codevoyager1984 <codevoyager.jay@gmail.com>
2024-11-18 17:09:10 +0700

    fix: check is valid url

--- src/common/utils/index.ts
+++ src/common/utils/index.ts
@@ -27,17 +27,12 @@ export function isValidToolName(str: string) {
 }
 
 export function isValidUrl(url: string) {
-  const urlPattern = new RegExp(
-    '^(https?:\\/\\/)?' + // protocol
-      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|' + // domain name and extension
-      '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
-      '(\\:\\d+)?' + // port
-      '(\\/[-a-z\\d%@_.~+&:]*)*' + // path
-      '(\\?[;&a-z\\d%@_.,~+&:=-]*)?' + // query string
-      '(\\#[-a-z\\d_]*)?$',
-    'i',
-  ); // fragment locator
-  return !!urlPattern.test(url);
+  try {
+    new URL(url);
+    return true;
+  } catch (error) {
+    return false;
+  }
 }
 
 export function getHostFromUrl(url: string): string {
