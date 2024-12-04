506472e59e691220a4fb89c3798896ae75977fff
LF112 <lf@lf112.net>
2024-12-04 13:04:46 +0800

    prev: new group sidebar

---
+++ ui/src/components/layout/workbench/sidebar/mode/normal/group-virua/index.tsx
@@ -0,0 +1,41 @@
+import React, { useRef } from 'react';
+
+import { Virtualizer } from 'virtua';
+
+import { IPageGroup, IPinPage } from '@/apis/pages/typings.ts';
+import { Button } from '@/components/ui/button';
+import { ScrollArea } from '@/components/ui/scroll-area.tsx';
+import { cn } from '@/utils';
+
+interface IVirtuaWorkbenchViewGroupListProps extends React.ComponentPropsWithoutRef<'div'> {
+  groupId: string;
+  setGroupId: React.Dispatch<React.SetStateAction<string>>;
+  data: (Omit<IPageGroup, 'pageIds'> & { pages: IPinPage[] })[];
+}
+
+export const VirtuaWorkbenchViewGroupList: React.FC<IVirtuaWorkbenchViewGroupListProps> = ({
+  groupId,
+  setGroupId,
+  data,
+}) => {
+  const scrollRef = useRef<HTMLDivElement>(null);
+
+  return (
+    <div className="h-full min-w-32 p-4">
+      <ScrollArea className="-mr-3 pr-3" ref={scrollRef} disabledOverflowMask>
+        <Virtualizer scrollRef={scrollRef}>
+          {data.map(({ displayName, id }, i) => (
+            <Button
+              key={i}
+              className={cn('mb-2 w-full hover:bg-accent', groupId === id && 'border border-input bg-background')}
+              variant="ghost"
+              onClick={() => setGroupId(id)}
+            >
+              {displayName}
+            </Button>
+          ))}
+        </Virtualizer>
+      </ScrollArea>
+    </div>
+  );
+};
--- ui/src/components/layout/workbench/sidebar/mode/normal/virtua/index.tsx
+++ ui/src/components/layout/workbench/sidebar/mode/normal/virtua/index.tsx
@@ -1,8 +1,5 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 
-import { useCreation } from 'ahooks';
-import { set } from 'lodash';
-import { useTranslation } from 'react-i18next';
 import { Virtualizer, VListHandle } from 'virtua';
 
 import { IPinPage } from '@/apis/pages/typings.ts';
@@ -11,16 +8,12 @@ import {
   IWorkbenchViewItemProps,
   ViewItem,
   WorkbenchViewItemCurrentData,
-  WorkbenchViewListStickyIndexContext,
-  WorkbenchViewListStickyIndexesContext,
-  WorkbenchViewListStickyItem,
 } from '@/components/layout/workbench/sidebar/mode/normal/virtua/item.tsx';
-import { Label } from '@/components/ui/label.tsx';
 import { ScrollArea } from '@/components/ui/scroll-area.tsx';
 
 interface IVirtuaWorkbenchViewListProps {
   height: number;
-  data: { pages: IPinPage[]; id: string; displayName: string; isBuiltIn: boolean }[];
+  data: IPinPage[];
 
   currentPageId?: string;
   currentGroupId?: string;
@@ -35,65 +28,28 @@ export const VirtuaWorkbenchViewList: React.FC<IVirtuaWorkbenchViewListProps> = 
   currentGroupId,
   onChildClick,
 }) => {
-  const { t } = useTranslation();
-
-  const [activeIndex, setActiveIndex] = useState(0);
-
-  const [stickyIndexes, list] = useCreation(() => {
-    const flatList = data
-      .map((it) => [{ type: 'v-label', displayName: it.displayName }, ...it.pages.map((p) => set(p, 'groupId', it.id))])
-      .flat() as IWorkbenchViewItemPage[];
-    return [new Set(flatList.map(({ type }, i) => (type === 'v-label' ? i : -1)).filter((i) => i !== -1)), flatList];
-  }, [data]);
-
   const scrollRef = useRef<HTMLDivElement>(null);
 
   const ref = useRef<VListHandle>(null);
   useEffect(() => {
     if (!currentPageId || !currentGroupId || !ref.current) return;
 
-    const index = list.findIndex((it) => it?.id === currentPageId && it.groupId === currentGroupId);
+    const index = data.findIndex((it) => it?.id === currentPageId);
     if (index === -1) return;
 
     requestIdleCallback(() => ref.current?.scrollToIndex(index, { smooth: true, offset: -40 }));
-  }, [currentGroupId, currentPageId, list]);
+  }, [currentGroupId, currentPageId, data]);
 
   return (
-    <WorkbenchViewListStickyIndexContext.Provider value={activeIndex}>
-      <WorkbenchViewListStickyIndexesContext.Provider value={stickyIndexes}>
-        <WorkbenchViewItemCurrentData.Provider value={{ pageId: currentPageId, groupId: currentGroupId }}>
-          <ScrollArea
-            className="-mb-2 -mr-3 pr-3 [&>[data-radix-scroll-area-viewport]]:-mt-2"
-            ref={scrollRef}
-            style={{ height: height + 8 }}
-            disabledOverflowMask
-          >
-            <Virtualizer
-              ref={ref}
-              item={WorkbenchViewListStickyItem}
-              keepMounted={[activeIndex]}
-              onRangeChange={(start) => {
-                const activeStickyIndex = [...stickyIndexes].reverse().find((index) => start >= index)!;
-                setActiveIndex(activeStickyIndex);
-              }}
-              scrollRef={scrollRef}
-            >
-              {list.map((it, i) =>
-                it.type === 'v-label' ? (
-                  <div key={i} className="select-none bg-slate-1 py-2 pl-2">
-                    <Label className="text-sm leading-none text-muted-foreground">
-                      {t([`workspace.wrapper.space.menu.group.name-${it.displayName}`, it.displayName])}
-                    </Label>
-                  </div>
-                ) : (
-                  <ViewItem key={i} page={it as IWorkbenchViewItemPage} onClick={onChildClick} />
-                ),
-              )}
-              <div />
-            </Virtualizer>
-          </ScrollArea>
-        </WorkbenchViewItemCurrentData.Provider>
-      </WorkbenchViewListStickyIndexesContext.Provider>
-    </WorkbenchViewListStickyIndexContext.Provider>
+    <WorkbenchViewItemCurrentData.Provider value={{ pageId: currentPageId, groupId: currentGroupId }}>
+      <ScrollArea className="-mr-3 pr-3" ref={scrollRef} style={{ height }} disabledOverflowMask>
+        <Virtualizer ref={ref} scrollRef={scrollRef}>
+          {data.map((it, i) => (
+            <ViewItem key={i} page={it as IWorkbenchViewItemPage} onClick={onChildClick} />
+          ))}
+          <div />
+        </Virtualizer>
+      </ScrollArea>
+    </WorkbenchViewItemCurrentData.Provider>
   );
 };
