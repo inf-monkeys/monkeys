0bb97710f676d79dc583dc13638dc37f5e606fae
玖叁 <colour_93@furry.top>
2025-02-13 14:57:57 +0800

    feat: update comfyui model info when create team

--- src/modules/auth/teams/teams.module.ts
+++ src/modules/auth/teams/teams.module.ts
@@ -1,3 +1,6 @@
+import { ComfyuiModelModule } from '@/modules/assets/comfyui-model/comfyui-model.module';
+import { ComfyuiModelService } from '@/modules/assets/comfyui-model/comfyui-model.service';
+import { ComfyUIModule } from '@/modules/tools/comfyui/comfyui.module';
 import { ConductorModule } from '@/modules/workflow/conductor/conductor.module';
 import { Module } from '@nestjs/common';
 import { TeamsController } from './teams.controller';
@@ -5,8 +8,8 @@ import { TeamsService } from './teams.service';
 
 @Module({
   controllers: [TeamsController],
-  providers: [TeamsService],
-  imports: [ConductorModule],
+  providers: [TeamsService, ComfyuiModelService],
+  imports: [ConductorModule, ComfyuiModelModule, ComfyUIModule],
   exports: [TeamsService],
 })
-export class TeamsModule {}
+export class TeamsModule { }
