a144e46767afc8c0fc556f5cb673b58d6865c6fe
Zreal <xiaota@foxmail.com>
2025-04-30 12:11:24 +0800

    feat

--- ui/src/view/vines-form/index.tsx
+++ ui/src/view/vines-form/index.tsx
@@ -2,7 +2,7 @@ import React, { lazy, Suspense } from 'react';
 
 import { SkeletonWithFullscreenUseLoading } from '@/components/ui/skeleton.tsx';
 
-const VinesFormLazy = lazy(() => import('./vines-form-lazy.tsx'));
+const VinesFormLazy = lazy(() => import('./vines-form-lazy'));
 
 export const VinesForm: React.FC = () => (
   <Suspense fallback={<SkeletonWithFullscreenUseLoading />}>
