ba3e43a03dd687418ba97e4baef829abacb22579
Zreal <xiaota@foxmail.com>
2025-05-08 11:18:35 +0800

    style(ui): 调整 Slider 组件样式

--- ui/src/components/ui/slider.tsx
+++ ui/src/components/ui/slider.tsx
@@ -13,11 +13,11 @@ export interface SliderProps extends Omit<React.ComponentPropsWithoutRef<typeof 
 
 const Slider = React.forwardRef<React.ElementRef<typeof SliderPrimitive.Root>, SliderProps>(
   ({ className, label, value, disabledLabel = false, ...props }, ref) => (
-    <div className="flex flex-col items-end gap-2">
+    <div className="flex flex-col items-end gap-2 rounded-lg bg-gray-100 p-3 dark:bg-gray-800">
       {!disabledLabel && (
         <div className={cn('flex w-full justify-between', { 'justify-end': !label })}>
-          {label && <span className="text-sm">{label}</span>}
-          <span className="text-sm">{isUndefined(value?.[0]) ? 'NaN' : value[0]}</span>
+          {label && <span className="text-sm font-medium">{label}</span>}
+          <span className="text-sm font-medium">{isUndefined(value?.[0]) ? 'NaN' : value[0]}</span>
         </div>
       )}
       <SliderPrimitive.Root
@@ -26,10 +26,10 @@ const Slider = React.forwardRef<React.ElementRef<typeof SliderPrimitive.Root>, S
         value={value}
         {...props}
       >
-        <SliderPrimitive.Track className="relative h-2 w-full grow overflow-hidden rounded-full bg-secondary">
-          <SliderPrimitive.Range className="absolute h-full bg-vines-300" />
+        <SliderPrimitive.Track className="relative h-2 w-full grow overflow-hidden rounded-md bg-gray-200 dark:bg-gray-700">
+          <SliderPrimitive.Range className="absolute h-full bg-gray-500 dark:bg-gray-400" />
         </SliderPrimitive.Track>
-        <SliderPrimitive.Thumb className="block size-4 cursor-grab rounded-full border-2 border-vines-400 bg-background ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-vines-500 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50" />
+        <SliderPrimitive.Thumb className="block h-4 w-1.5 cursor-grab rounded-sm border-0 bg-gray-700 ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-800 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-300" />
       </SliderPrimitive.Root>
     </div>
   ),
