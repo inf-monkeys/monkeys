f5e2a867fa56458651c6a692b3f4519510c39599
plintash <plintash@proton.me>
2025-05-08 16:20:08 +0800

    feat: improve settings page

--- ui/src/components/layout/settings/team-credit/index.tsx
+++ ui/src/components/layout/settings/team-credit/index.tsx
@@ -1,7 +1,15 @@
+import { ConsumerDetails } from '../account/consumer-details';
+import { RechargeDetails } from '../account/recharge-details';
+import { TeamProperty } from '../account/team-property';
+
 export const TeamCredit = () => {
   return (
-    <div className="grid grid-cols-1 items-start justify-center gap-4 md:grid-cols-2 lg:grid-cols-3">
-      <h2>施工中🚧</h2>
+    <div className="= grid grid-cols-1 items-start justify-center gap-4 md:grid-cols-2">
+      <div className="flex flex-col gap-4">
+        <TeamProperty />
+        <RechargeDetails />
+        <ConsumerDetails />
+      </div>
     </div>
   );
 };
