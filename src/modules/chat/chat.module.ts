375dbb8fc68e2162df78a38eef40b358d1cb8625
plintash <plintash@proton.me>
2025-05-12 16:27:59 +0800

    WIP

--- src/modules/chat/chat.module.ts
+++ src/modules/chat/chat.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
+import { MediaModule } from '../assets/media/media.module';
 import { LLMToolsModule } from '../tools/llm/llm.module';
 import { WorkflowModule } from '../workflow/workflow.module';
 import { WorkflowOpenAICompatibleController } from './chat.controller';
-
 @Module({
   controllers: [WorkflowOpenAICompatibleController],
-  imports: [WorkflowModule, LLMToolsModule],
+  imports: [WorkflowModule, LLMToolsModule, MediaModule],
 })
 export class ChatModule {}
