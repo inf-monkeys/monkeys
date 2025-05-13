6fa8d25f2bd068578b1fff9896f57d60fd408c78
plintash <plintash@proton.me>
2025-05-13 16:34:55 +0800

    style: lint

--- src/database/migrations/1745922805471-add-table-workflow-observability-migartion.ts
+++ src/database/migrations/1745922805471-add-table-workflow-observability-migartion.ts
@@ -7,45 +7,45 @@ import { config } from '@/common/config';
 const appId = config.server.appId;
 
 export class MigartionAddWorkflowObservability1745922805471 implements MigrationInterface {
-    tableName = `${appId}_workflow_observability`;
+  tableName = `${appId}_workflow_observability`;
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.createTable(
-            new Table({
-                name: this.tableName,
-                columns: [
-                    ...COMMON_COLUMNS,
-                    {
-                        name: 'workflow_id',
-                        type: 'varchar',
-                        length: '255',
-                    },
-                    {
-                        name: 'team_id',
-                        type: 'varchar',
-                        length: '255',
-                    },
-                    {
-                        name: 'name',
-                        type: 'varchar',
-                        length: '255',
-                        isNullable: true,
-                    },
-                    {
-                        name: 'platform',
-                        type: 'varchar',
-                        length: '255',
-                    },
-                    {
-                        name: 'platform_config',
-                        type: 'text',
-                    },
-                ],
-            }),
-        );
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: this.tableName,
+        columns: [
+          ...COMMON_COLUMNS,
+          {
+            name: 'workflow_id',
+            type: 'varchar',
+            length: '255',
+          },
+          {
+            name: 'team_id',
+            type: 'varchar',
+            length: '255',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            length: '255',
+            isNullable: true,
+          },
+          {
+            name: 'platform',
+            type: 'varchar',
+            length: '255',
+          },
+          {
+            name: 'platform_config',
+            type: 'text',
+          },
+        ],
+      }),
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropTable(this.tableName);
-    }
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable(this.tableName);
+  }
 }
