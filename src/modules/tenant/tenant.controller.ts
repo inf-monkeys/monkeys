f299d9967a92bc89744649e99d6fc96ecb8472cf
plintash <plintash@proton.me>
2025-05-14 14:17:57 +0800

    WIP

--- src/modules/tenant/tenant.controller.ts
+++ src/modules/tenant/tenant.controller.ts
@@ -1,11 +1,15 @@
 import { TenantStaticsAuthGuard } from '@/common/guards/tenant-statics.guard';
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, HttpCode, Res, UseGuards } from '@nestjs/common';
+import axios from 'axios';
+import { Response } from 'express';
 import { TenantService } from './tenant.service';
+
+const BATCH_SIZE = 5000;
 
 @Controller('tenant')
 @UseGuards(TenantStaticsAuthGuard)
 export class TenantController {
-  constructor(private readonly tenantService: TenantService) { }
+  constructor(private readonly tenantService: TenantService) {}
 
   // @Post()
   // async create(@Body() createTenantDto: CreateTenantDto) {
@@ -18,22 +22,46 @@ export class TenantController {
   }
 
   @Get('es')
-  async findAllEs() {
-    return await this.tenantService.findAllEs();
+  @HttpCode(200)
+  async findAllEs(@Res() res: Response) {
+    res.setHeader('content-type', 'text/event-stream');
+    res.status(200);
+    const searchQuery = {
+      size: BATCH_SIZE, // Limit to 10 results
+      from: 0, // Start from the beginning (skip 0 results)
+      query: {
+        match_all: {}, // Example query: fetch all documents
+      },
+      sort: [
+        { startTime: 'asc' }, // Sort by start time descending for consistent pagination
+      ],
+    };
+    while (true) {
+      try {
+        const result = await axios.post('http://elasticsearch-master:9200/conductor_workflow/_search?pretty', searchQuery, {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+        // Check for successful response
+        if (result.status === 200) {
+          res.write(result.data);
+          if (result.data.length < BATCH_SIZE) {
+            searchQuery.from += BATCH_SIZE;
+            break;
+          }
+        } else {
+          // Handle non-200 status codes
+          res.write({ error: 'Unexpected response status', status: result.status });
+          res.end();
+          return;
+        }
+      } catch (error) {
+        // Handle any network or other errors
+        res.write(error);
+        res.end();
+      }
+    }
+    res.end();
   }
-
-  // @Get(':id')
-  // async findOne(@Param('id') id: string) {
-  //   return await this.tenantService.findOne(+id);
-  // }
-
-  // @Patch(':id')
-  // async update(@Param('id') id: string, @Body() updateTenantDto: UpdateTenantDto) {
-  //   return await this.tenantService.update(+id, updateTenantDto);
-  // }
-
-  // @Delete(':id')
-  // async remove(@Param('id') id: string) {
-  //   return await this.tenantService.remove(+id);
-  // }
 }
