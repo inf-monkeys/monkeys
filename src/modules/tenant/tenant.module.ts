20a2a16f0b02861b8cf16fa3b6be5b4d30ff901c
玖叁 <x@fur93.icu>
2025-05-14 19:08:20 +0800

    WIP

--- src/modules/tenant/tenant.module.ts
+++ src/modules/tenant/tenant.module.ts
@@ -1,9 +1,12 @@
+import { WorkflowExecutionEntity } from '@/database/entities/workflow/workflow-execution';
 import { Module } from '@nestjs/common';
-import { TenantService } from './tenant.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TenantController } from './tenant.controller';
+import { TenantService } from './tenant.service';
 
 @Module({
   controllers: [TenantController],
   providers: [TenantService],
+  imports: [TypeOrmModule.forFeature([WorkflowExecutionEntity])],
 })
-export class TenantModule {}
+export class TenantModule { }
