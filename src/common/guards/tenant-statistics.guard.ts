e9988b39ae11f3fc86e2fc42bcb7590432691e7c
plintash <plintash@proton.me>
2025-05-15 13:53:43 +0800

    WIP

--- src/common/guards/tenant-statistics.guard.ts
+++ src/common/guards/tenant-statistics.guard.ts
@@ -2,18 +2,18 @@ import { config } from '@/common/config';
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 
 @Injectable()
-export class TenantStaticsAuthGuard implements CanActivate {
-  constructor() { }
+export class TenantStatisticsAuthGuard implements CanActivate {
+  constructor() {}
   async canActivate(context: ExecutionContext) {
-    // Skip if config.tenantStatics.bearerToken is not configured in config.yaml
-    const configToken = config.tenantStatics.bearerToken;
+    // Skip if config.tenantStatistics.bearerToken is not configured in config.yaml
+    const configToken = config.tenantStatistics.bearerToken;
     if (!configToken) {
       return false;
     }
 
     // Get Bearer Token from request
     const request = context.switchToHttp().getRequest();
-    const authorizationToken = (request.headers['x-monkeys-tenantstatics'] || request.headers['authorization']) as string | undefined;
+    const authorizationToken = request.headers['authorization'] as string | undefined;
     const userToken = authorizationToken?.replace('Bearer ', '');
 
     if (configToken !== userToken) {
--- src/modules/tenant/tenant.controller.ts
+++ src/modules/tenant/tenant.controller.ts
@@ -1,21 +1,12 @@
-import { TenantStaticsAuthGuard } from '@/common/guards/tenant-statics.guard';
+import { TenantStatisticsAuthGuard } from '@/common/guards/tenant-statistics.guard';
 import { SuccessResponse } from '@/common/response';
-import { Controller, Get, HttpCode, Query, UseGuards } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 import { TenantService } from './tenant.service';
 
-const BATCH_SIZE = 1000;
-
 @Controller('tenant')
-@UseGuards(TenantStaticsAuthGuard)
+@UseGuards(TenantStatisticsAuthGuard)
 export class TenantController {
-  constructor(
-    private readonly tenantService: TenantService,
-  ) { }
-
-  // @Post()
-  // async create(@Body() createTenantDto: CreateTenantDto) {
-  //   return await this.tenantService.create(createTenantDto);
-  // }
+  constructor(private readonly tenantService: TenantService) {}
 
   @Get()
   async findAll() {
@@ -25,48 +16,9 @@ export class TenantController {
     });
   }
 
-  @Get('es')
-  @HttpCode(200)
-  async findAllEs(@Query() from: number, @Query() size?: number) {
-    // const searchQuery = {
-    //   size: size || BATCH_SIZE, // Limit to 10 results
-    //   from: from || 0, // Start from the beginning (skip 0 results)
-    //   query: {
-    //     match_all: {}, // Example query: fetch all documents
-    //   },
-    //   sort: [
-    //     { startTime: 'asc' }, // Sort by start time descending for consistent pagination
-    //   ],
-    // };
-    // try {
-    //   const result = await axios.post('http://10.93.0.110:9200/conductor_workflow/_search?pretty', searchQuery, {
-    //     headers: {
-    //       'Content-Type': 'application/json',
-    //     },
-    //   });
-    //   if (result.status === 200) {
-    //     const data = result.data;
-    //     const hits: Record<string, any> = data.hits.hits!;
-    //     const finalResult = [];
-    //     hits.forEach((e) => {
-    //       try {
-    //         const output = JSON.parse(e.output);
-    //         if (output.success) {
-    //           finalResult.push(output);
-    //         }
-    //       } catch (e) { }
-    //     });
-    //     return finalResult;
-    //   } else {
-    //     // Handle non-200 status codes
-    //     return { error: 'Unexpected response status', status: result.status };
-    //   }
-    // } catch (error) {
-    //   // Handle any network or other errors
-    //   return error;
-    // }
-
-    const result = await this.tenantService.findAllEs();
+  @Get()
+  async findBetween(@Query() startTime: number, @Query() endTime: number) {
+    const result = await this.tenantService.findBetween(startTime, endTime);
     return new SuccessResponse({
       data: result,
     });
