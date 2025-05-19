644e496834ddb8918143297c833581ce952c86ba
玖叁 <x@fur93.icu>
2025-05-20 01:13:16 +0800

    feat: swr masonry

--- ui/src/components/layout/workspace/vines-view/form/execution-result/grid/item.tsx
+++ ui/src/components/layout/workspace/vines-view/form/execution-result/grid/item.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import { SWRInfiniteResponse } from 'swr/infinite';
+
 import type { EventEmitter } from 'ahooks/lib/useEventEmitter';
-import { CirclePause } from 'lucide-react';
+import { CirclePause, FileMinus } from 'lucide-react';
 import { useTranslation } from 'react-i18next';
 
 import { VinesAbstractDataPreview } from '@/components/layout/workspace/vines-view/_common/data-display/abstract';
+import { IAddDeletedInstanceId } from '@/components/layout/workspace/vines-view/form/execution-result/grid/index.tsx';
 import { VinesLoading } from '@/components/ui/loading';
 import { getAlt } from '@/utils';
 import { IVinesExecutionResultItem } from '@/utils/execution.ts';
@@ -15,14 +18,31 @@ import { VirtuaExecutionResultGridWrapper } from '../virtua/item/wrapper';
 interface IExecutionResultItemProps {
   result: IVinesExecutionResultItem;
   event$: EventEmitter<void>;
+  isDeleted?: boolean;
+  addDeletedInstanceId?: IAddDeletedInstanceId;
+  mutate?: SWRInfiniteResponse['mutate'];
 }
 
-export const ExecutionResultItem: React.FC<IExecutionResultItemProps> = ({ result, event$ }) => {
+export const ExecutionResultItem: React.FC<IExecutionResultItemProps> = ({
+  result,
+  event$,
+  isDeleted = false,
+  addDeletedInstanceId,
+  mutate,
+}) => {
   const { render } = result;
   const { type, data, status } = render;
   const { t } = useTranslation();
 
   const alt = getAlt(result);
+
+  if (isDeleted)
+    return (
+      <div className="flex h-10 items-center justify-center gap-1 rounded-lg border border-input shadow-sm">
+        <FileMinus className="stroke-gray-11" size={16} />
+        <span className="text-sm font-bold text-gray-11">{t('common.utils.deleted')}</span>
+      </div>
+    );
 
   switch (status) {
     case 'SCHEDULED':
@@ -52,7 +72,13 @@ export const ExecutionResultItem: React.FC<IExecutionResultItemProps> = ({ resul
       // 使用包装组件来支持下载和删除功能
       return (
         <div className="relative overflow-hidden rounded-lg border border-input shadow-sm">
-          <VirtuaExecutionResultGridWrapper data={result} src={data as string} event$={event$}>
+          <VirtuaExecutionResultGridWrapper
+            data={result}
+            src={data as string}
+            event$={event$}
+            addDeletedInstanceId={addDeletedInstanceId}
+            mutate={mutate}
+          >
             <div className="h-full w-full" onClick={(e) => e.stopPropagation()}>
               <VirtuaExecutionResultGridImageItem src={data as string} alt={alt} instanceId={result.instanceId} />
             </div>
@@ -62,7 +88,12 @@ export const ExecutionResultItem: React.FC<IExecutionResultItemProps> = ({ resul
     default:
       return (
         <div className="relative overflow-hidden rounded-lg border border-input shadow-sm">
-          <VirtuaExecutionResultGridWrapper data={result} event$={event$}>
+          <VirtuaExecutionResultGridWrapper
+            data={result}
+            event$={event$}
+            addDeletedInstanceId={addDeletedInstanceId}
+            mutate={mutate}
+          >
             <div className="max-h-96 min-h-40 overflow-auto p-2">
               <VinesAbstractDataPreview data={data} className="h-full" />
             </div>
