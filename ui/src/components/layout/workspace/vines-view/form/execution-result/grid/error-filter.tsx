1014183c9e38d3a4248c75eee0ae362f78ced812
plintash <plintash@proton.me>
2025-05-21 16:17:43 +0800

    feat: filter errors in execution results

--- ui/src/components/layout/workspace/vines-view/form/execution-result/grid/error-filter.tsx
+++ ui/src/components/layout/workspace/vines-view/form/execution-result/grid/error-filter.tsx
@@ -1,4 +1,5 @@
 import { FilterIcon } from 'lucide-react';
+import { useTranslation } from 'react-i18next';
 
 import { Button } from '@/components/ui/button';
 import {
@@ -10,19 +11,20 @@ import {
 import { useShouldFilterErrorStore } from '@/store/useShouldErrorFilterStore';
 export const ErrorFilter = () => {
   const { filter, setFilterOn, setFilterOff } = useShouldFilterErrorStore();
+  const { t } = useTranslation();
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="borderless" className="mb-2 hover:bg-slate-1 active:bg-slate-1" icon={<FilterIcon />}>
-          过滤错误
+          {t('workspace.logs-view.log.list.error-filter.title')}
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="start" side="bottom">
         <DropdownMenuCheckboxItem checked={filter} onCheckedChange={setFilterOn}>
-          是
+          {t('workspace.logs-view.log.list.error-filter.options.yes')}
         </DropdownMenuCheckboxItem>
         <DropdownMenuCheckboxItem checked={!filter} onCheckedChange={setFilterOff}>
-          否
+          {t('workspace.logs-view.log.list.error-filter.options.no')}
         </DropdownMenuCheckboxItem>
       </DropdownMenuContent>
     </DropdownMenu>
