c1ad38e8e2fa91d8b1a625b3b4f2f464cc4bd3f7
plintash <plintash@proton.me>
2025-06-09 11:24:37 +0800

    checkpoint

--- ui/src/components/layout/workspace/vines-view/flow/headless-modal/endpoint/start-tool/workflow-input-config/input-config/input-editor/field/display-name.tsx
+++ ui/src/components/layout/workspace/vines-view/flow/headless-modal/endpoint/start-tool/workflow-input-config/input-config/input-editor/field/display-name.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import { EditIcon } from 'lucide-react';
 import { UseFormReturn } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 
+import { Button } from '@/components/ui/button';
+import { Dialog, DialogContent, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
 import { FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form.tsx';
+import { LANGUAGES_LIST } from '@/components/ui/i18n-selector/consts';
 import { Input } from '@/components/ui/input';
 import { IWorkflowInput } from '@/schema/workspace/workflow-input.ts';
 
@@ -22,12 +26,33 @@ export const FieldDisplayName: React.FC<IFieldDisplayNameProps> = ({ form }) => 
         <FormItem>
           <FormLabel>{t('workspace.flow-view.endpoint.start-tool.input.config-form.display-name.label')}</FormLabel>
           <FormControl>
-            <Input
-              placeholder={t('workspace.flow-view.endpoint.start-tool.input.config-form.display-name.placeholder')}
-              {...field}
-              className="grow"
-              autoFocus
-            />
+            <>
+              <Input
+                placeholder={t('workspace.flow-view.endpoint.start-tool.input.config-form.display-name.placeholder')}
+                {...field}
+                className="grow"
+                autoFocus
+              />
+              <Dialog>
+                <DialogTrigger asChild>
+                  <Button icon={<EditIcon />} variant="outline" size="icon" className="absolute right-2 top-2" />
+                </DialogTrigger>
+                <DialogContent>
+                  <DialogTitle>Edit i18n Name</DialogTitle>
+                  {LANGUAGES_LIST.map(([key, label]) => (
+                    <div key={key}>
+                      <Input
+                        placeholder={t(
+                          'workspace.flow-view.endpoint.start-tool.input.config-form.display-name.placeholder',
+                        )}
+                        // {...field}
+                        className="grow"
+                      />
+                    </div>
+                  ))}
+                </DialogContent>
+              </Dialog>
+            </>
           </FormControl>
           <FormMessage />
         </FormItem>
