514faefb04d595a41d4c8a34da1bcaf90cff8230
ahao <ahao20010222@gmail.com>
2025-06-12 20:22:21 +0800

    modify width

--- ui/src/components/layout/workbench/history/index.tsx
+++ ui/src/components/layout/workbench/history/index.tsx
@@ -3,14 +3,16 @@ import { cn } from '@/utils';
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '@/components/ui/carousel';
 import { Card } from '@/components/ui/card';
 import { IVinesExecutionResultItem } from '@/utils/execution';
+import { usePageStore } from '@/store/usePageStore';
 
 interface HistoryResultProps {
   loading: boolean;
   imageItems: IVinesExecutionResultItem[];
   isMiniFrame?: boolean;
+  className?: string;
 }
 
-export const HistoryResult: React.FC<HistoryResultProps> = ({ loading, imageItems, isMiniFrame }) => {
+export const HistoryResult: React.FC<HistoryResultProps> = ({ loading, imageItems, isMiniFrame, className }) => {
   const [draggedItem, setDraggedItem] = useState<IVinesExecutionResultItem | null>(null);
   const [dragOverIndex, setDragOverIndex] = useState<number | null>(null);
   const [isDragging, setIsDragging] = useState(false);
@@ -49,13 +51,16 @@ export const HistoryResult: React.FC<HistoryResultProps> = ({ loading, imageItem
     }
     handleDragEnd();
   };
-
   return (
     <div
       className={cn(
-        'w-full h-35 relative col-span-5 mt-4 overflow-hidden rounded-xl border border-slate-200 bg-slate-1 px-12 py-6 shadow-sm',
+        ' relative col-span-5 mt-4 overflow-hidden rounded-xl border border-slate-200 bg-slate-1 px-12 py-6 shadow-sm',
         isMiniFrame && 'mt-2',
+        className,
       )}
+      style={{
+        width: window.innerWidth * 0.765,
+      }}
     >
       <Carousel
         opts={{
@@ -70,14 +75,14 @@ export const HistoryResult: React.FC<HistoryResultProps> = ({ loading, imageItem
         className="w-full px-4"
         ref={carouselRef}
       >
-        <CarouselContent className=" overflow-hidden">
+        <CarouselContent className=" overflow-hidden space-x-0">
           {loading ? (
             <div className="flex w-full items-center justify-center">
               <span>Loading...</span>
             </div>
           ) : imageItems.length > 0 ? (
             imageItems.map((item, index) => (
-              <CarouselItem key={item.render.key} className={cn('basis-[98px]', dragOverIndex === index && '')}>
+              <CarouselItem key={item.render.key} className={cn('basis-auto', dragOverIndex === index && '')}>
                 <Card
                   className={cn(
                     'h-[90px] w-[90px] cursor-move overflow-hidden',
