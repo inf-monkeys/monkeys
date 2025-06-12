3dfa7b5dd896abf0da827e122eeb4db14b78d1ae
FlorianLiao <huahuadeliaoliao@icloud.com>
2025-06-13 04:27:11 +0800

    feat: market controllers init

--- src/modules/marketplace/controllers/marketplace.admin.controller.ts
+++ src/modules/marketplace/controllers/marketplace.admin.controller.ts
@@ -1,8 +1,7 @@
 import { ListDto } from '@/common/dto/list.dto';
 import { SuccessListResponse, SuccessResponse } from '@/common/response';
-import { Body, Controller, Get, Param, Put, Query } from '@nestjs/common';
+import { Controller, Get, Param, Put, Query } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
-import { UpdateMarketplaceAppDto } from '../dto/update-app.dto';
 import { MarketplaceService } from '../services/marketplace.service';
 
 @ApiTags('Marketplace Admin')
@@ -22,17 +21,10 @@ export class MarketplaceAdminController {
     });
   }
 
-  @Put('/submissions/:appId')
-  @ApiOperation({ summary: 'Update the application information pending review' })
-  async updateSubmission(@Param('appId') appId: string, @Body() body: UpdateMarketplaceAppDto) {
-    const app = await this.marketplaceService.updateSubmission(appId, body);
-    return new SuccessResponse({ data: app });
-  }
-
   @Put('/submissions/:appId/approve')
   @ApiOperation({ summary: 'Approve a submission' })
-  async approveSubmission(@Param('appId') appId: string, @Body() body: { isPreset?: boolean }) {
-    const app = await this.marketplaceService.approveSubmission(appId, body.isPreset);
+  async approveSubmission(@Param('appId') appId: string) {
+    const app = await this.marketplaceService.approveSubmission(appId);
     return new SuccessResponse({ data: app });
   }
 
