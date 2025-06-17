ae19f72369981bf9d822151dbb27e4c8cf47d937
plintash <plintash@proton.me>
2025-06-17 14:18:37 +0800

    style

--- ui/src/components/layout/settings/theme/team-custom-config/index.tsx
+++ ui/src/components/layout/settings/theme/team-custom-config/index.tsx
@@ -1,9 +1,10 @@
-import { useTeams } from "@/apis/authz/team";
-import { useCustomConfigs } from "@/apis/authz/team/custom-configs";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import { Switch } from "@/components/ui/switch";
-import { useTranslation } from "react-i18next";
-import { toast } from "sonner";
+import { useTranslation } from 'react-i18next';
+import { toast } from 'sonner';
+
+import { useTeams } from '@/apis/authz/team';
+import { useCustomConfigs } from '@/apis/authz/team/custom-configs';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { Switch } from '@/components/ui/switch';
 
 export const TeamCustomConfig = () => {
   const { t } = useTranslation();
@@ -29,7 +30,7 @@ export const TeamCustomConfig = () => {
         error: t('common.update.error'),
       },
     );
-  }
+  };
 
   return (
     <Card>
@@ -38,12 +39,16 @@ export const TeamCustomConfig = () => {
         <CardDescription>{t('settings.theme.team-custom-config.description')}</CardDescription>
       </CardHeader>
       <CardContent>
-        <Card className="border-0 shadow-none p-0">
+        <Card className="border-0 p-0 shadow-none">
           <div className="flex items-center justify-between">
             <div className="flex flex-col">
               <div className="flex flex-col">
-                <h3 className="line-clamp-1 font-semibold leading-tight">{t('settings.theme.team-custom-config.configs.show-form-in-image-detail.title')}</h3>
-                <p className="text-xs text-muted-foreground">{t('settings.theme.team-custom-config.configs.show-form-in-image-detail.description')}</p>
+                <h3 className="line-clamp-1 font-semibold leading-tight">
+                  {t('settings.theme.team-custom-config.configs.show-form-in-image-detail.title')}
+                </h3>
+                <p className="text-xs text-muted-foreground">
+                  {t('settings.theme.team-custom-config.configs.show-form-in-image-detail.description')}
+                </p>
               </div>
             </div>
             <div className="flex items-center space-x-2">
