95f146faaacf182529295abe95cecd6e4553ef53
玖叁 <x@fur93.icu>
2025-06-18 12:48:08 +0800

    fix: oidc & style

--- src/modules/auth/teams/teams.service.ts
+++ src/modules/auth/teams/teams.service.ts
@@ -6,7 +6,6 @@ import { TeamRepository } from '@/database/repositories/team.repository';
 import { ComfyuiModelService } from '@/modules/assets/comfyui-model/comfyui-model.service';
 import { DesignMetadataService } from '@/modules/design/design.metadata.service';
 import { DesignProjectService } from '@/modules/design/design.project.service';
-import { CreateDesignProjectDto } from '@/modules/design/dto/create-design-project.dto';
 import { ConductorService } from '@/modules/workflow/conductor/conductor.service';
 import { WorkflowPageService } from '@/modules/workflow/workflow.page.service';
 import { Injectable } from '@nestjs/common';
@@ -52,20 +51,20 @@ export class TeamsService {
   public async initTeam(teamId: string, userId: string) {
     // Init assets from built-in marketplace
     await this.forkAssetsFromMarketPlace(teamId, userId);
-    // TEMP TODO: 默认新建一个画板
-    const project = await this.designProjectService.create({
-      teamId,
-      creatorUserId: userId,
-      displayName: 'Design Board',
-      iconUrl: 'emoji:🎨:#eeeef1',
-      description: '',
-    } as CreateDesignProjectDto);
-    const board = (await this.designMetadataService.findAllByProjectId(project.id))[0];
-    const pageGroup = await this.pageService.getPageGroups(teamId)[0];
-    await this.pageService.updatePageGroup(teamId, pageGroup.id, {
-      pageId: 'design-board-' + board.id,
-      mode: 'add',
-    });
+    // // TEMP TODO: 默认新建一个画板
+    // const project = await this.designProjectService.create({
+    //   teamId,
+    //   creatorUserId: userId,
+    //   displayName: 'Design Board',
+    //   iconUrl: 'emoji:🎨:#eeeef1',
+    //   description: '',
+    // } as CreateDesignProjectDto);
+    // const board = (await this.designMetadataService.findAllByProjectId(project.id))[0];
+    // const pageGroup = await this.pageService.getPageGroups(teamId)[0];
+    // await this.pageService.updatePageGroup(teamId, pageGroup.id, {
+    //   pageId: 'design-board-' + board.id,
+    //   mode: 'add',
+    // });
     // 初始化内置图像模型类型
     await this.comfyuiModelService.updateTypesFromInternals(teamId);
     // 自动更新内置图像模型列表
