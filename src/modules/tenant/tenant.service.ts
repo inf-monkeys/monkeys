067b4ceb5006517261ac6b3c4777053331b14075
FlorianLiao <huahuadeliaoliao@icloud.com>
2025-06-23 12:11:24 +0800

    fix: use conductor service to get team workflow history in tenant API

--- src/modules/tenant/tenant.service.ts
+++ src/modules/tenant/tenant.service.ts
@@ -1,14 +1,19 @@
 import { conductorClient } from '@/common/conductor';
 import { config } from '@/common/config';
+import { OrderBy } from '@/common/dto/order.enum';
+import { PaginationDto } from '@/common/dto/pagination.dto';
 import { WorkflowStatusEnum } from '@/common/dto/status.enum';
 import { flattenObject } from '@/common/utils';
 import { WorkflowExecutionEntity } from '@/database/entities/workflow/workflow-execution';
-import { WorkflowRepository } from '@/database/repositories/workflow.repository';
+import { WorkflowMetadataEntity } from '@/database/entities/workflow/workflow-metadata';
+import { FindWorkflowCondition, WorkflowRepository } from '@/database/repositories/workflow.repository';
+import { Task } from '@inf-monkeys/conductor-javascript';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { isBoolean } from 'lodash';
+import _, { isBoolean } from 'lodash';
 import { FindManyOptions, Repository } from 'typeorm';
-import { SearchWorkflowExecutionsDto } from '../workflow/dto/req/search-workflow-execution.dto';
+import { ConductorService } from '../workflow/conductor/conductor.service';
+import { SearchWorkflowExecutionsDto, SearchWorkflowExecutionsOrderDto, WorkflowExecutionSearchableField } from '../workflow/dto/req/search-workflow-execution.dto';
 import { WorkflowExecutionService, WorkflowWithMetadata } from '../workflow/workflow.execution.service';
 
 @Injectable()
@@ -17,6 +22,7 @@ export class TenantService {
     @InjectRepository(WorkflowExecutionEntity)
     private readonly workflowExecutionRepository: Repository<WorkflowExecutionEntity>,
     private readonly workflowRepository: WorkflowRepository,
+    private readonly conductorService: ConductorService,
     private readonly workflowExecutionService: WorkflowExecutionService,
   ) {}
 
@@ -62,7 +68,6 @@ export class TenantService {
         .flat()
         .filter((value: string | number | null) => typeof value === 'string' && imageSuffix.some((suffix) => value.endsWith(suffix))).length,
     };
-
     return {
       workflow: {
         execution: {
@@ -79,7 +84,93 @@ export class TenantService {
     };
   }
 
-  public async searchWorkflowExecutionsForTeam(teamId: string, dto: SearchWorkflowExecutionsDto): Promise<{ page: number; limit: number; total: number; data: WorkflowWithMetadata[] }> {
-    return this.workflowExecutionService.searchWorkflowExecutions(teamId, dto);
+  public async searchWorkflowExecutionsForTeam(
+    teamId: string,
+    condition: SearchWorkflowExecutionsDto,
+  ): Promise<{ page: number; limit: number; total: number; data: WorkflowWithMetadata[]; definitions: WorkflowMetadataEntity[] }> {
+    const { pagination = {}, orderBy = {}, workflowId, status = [], startTimeFrom, startTimeTo, freeText = '*', startBy = [], triggerTypes = [], versions = [], workflowInstanceId } = condition;
+
+    const { page: p = 1, limit: l = 10 } = pagination as PaginationDto;
+    const [page, limitNum] = [+p, +l];
+    const start = (page - 1) * limitNum;
+
+    const workflowCondition: FindWorkflowCondition = { teamId };
+    if (workflowId) {
+      workflowCondition.workflowId = workflowId;
+    }
+    const workflowsToSearch = await this.workflowRepository.findWorkflowByCondition(workflowCondition);
+
+    if (workflowsToSearch.length === 0 && !workflowInstanceId) {
+      return { definitions: [], data: [], page, limit: limitNum, total: 0 };
+    }
+
+    const workflowTypes = workflowsToSearch.map((x) => x.workflowId);
+    let query: string[] = [];
+    if (workflowTypes.length > 0) {
+      query.push(`workflowType IN (${workflowTypes.join(',')})`);
+    }
+
+    if (status.length) {
+      query.push(`status IN (${status.join(',')})`);
+    }
+    if (startTimeFrom) {
+      query.push(`startTime > ${startTimeFrom}`);
+    }
+    if (startTimeTo) {
+      query.push(`startTime < ${startTimeTo}`);
+    }
+    if (versions?.length) {
+      query.push(`version IN (${versions.join(',')})`);
+    }
+
+    const workflowInstanceIdsToFilter: string[][] = [];
+
+    if (workflowInstanceId) {
+      query = [`workflowId IN (${workflowInstanceId})`];
+    } else {
+      if (startBy?.length) {
+        const instancesFromUser = await this.workflowRepository.findExecutionsByUserIds(startBy);
+        const ids = instancesFromUser.map((x) => x.workflowInstanceId);
+        if (ids.length === 0) return { definitions: [], data: [], page, limit: limitNum, total: 0 };
+        workflowInstanceIdsToFilter.push(ids);
+      }
+
+      if (triggerTypes?.length) {
+        const instancesFromTrigger = await this.workflowRepository.findExecutionsByTriggerTypes(triggerTypes);
+        const ids = instancesFromTrigger.map((x) => x.workflowInstanceId);
+        if (ids.length === 0) return { definitions: [], data: [], page, limit: limitNum, total: 0 };
+        workflowInstanceIdsToFilter.push(ids);
+      }
+
+      if (workflowInstanceIdsToFilter.length > 0) {
+        const intersection = _.intersection(...workflowInstanceIdsToFilter);
+        if (intersection.length === 0) {
+          return { definitions: [], data: [], page, limit: limitNum, total: 0 };
+        }
+        query.push(`workflowId IN (${intersection.slice(0, 500).join(',')})`);
+      }
+    }
+
+    const { field = WorkflowExecutionSearchableField.startTime, order = OrderBy.DESC } = orderBy as SearchWorkflowExecutionsOrderDto;
+    const sortText = `${field}:${order}`;
+
+    const conductorQueryString = query.join(' AND ') || undefined;
+
+    const searchData = await conductorClient.workflowResource.searchV21(start, limitNum, sortText, freeText, conductorQueryString);
+
+    const executionsResult = searchData?.results ?? [];
+    for (const executionItem of executionsResult) {
+      this.conductorService.convertConductorTasksToVinesTasks(teamId, (executionItem.tasks || []) as Task[], executionItem.workflowDefinition);
+    }
+
+    const executionsWithMetadata = await this.workflowExecutionService['populateMetadataByForExecutions'](executionsResult);
+
+    return {
+      definitions: workflowsToSearch,
+      page,
+      limit: limitNum,
+      data: executionsWithMetadata,
+      total: searchData?.totalHits ?? 0,
+    };
   }
 }
