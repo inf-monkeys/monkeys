e16a03b509969e27c26b8de9d6057c91f9e91b11
玖叁 <x@fur93.icu>
2025-06-23 10:50:35 +0800

    fix: hide team module

--- ui/src/consts/setttings/sidebar.tsx
+++ ui/src/consts/setttings/sidebar.tsx
@@ -1,5 +1,6 @@
 import { BotIcon } from 'lucide-react';
 export interface SettingsTabNavItem {
+  id: string;
   name: string;
   to?: string;
   icon?: React.ReactNode;
@@ -7,26 +8,31 @@ export interface SettingsTabNavItem {
 
 export const SETTINGS_SIDEBAR_MAP: SettingsTabNavItem[] = [
   {
+    id: 'account',
     name: 'settings.individual-configurations.title',
     icon: <BotIcon />,
     to: '/$teamId/settings?tab=account',
   },
   {
+    id: 'config',
     name: 'settings.team-configurations.title',
     icon: <BotIcon />,
     to: '/$teamId/settings?tab=team-configurations',
   },
   {
+    id: 'quota',
     name: 'settings.team-quota.title',
     icon: <BotIcon />,
     to: '/$teamId/settings?tab=team-quota',
   },
   {
+    id: 'stat',
     name: 'settings.stat.title',
     icon: <BotIcon />,
     to: '/$teamId/settings?tab=stat',
   },
   {
+    id: 'apikey',
     name: 'settings.api-key.title',
     icon: <BotIcon />,
     to: '/$teamId/settings?tab=apikey',
