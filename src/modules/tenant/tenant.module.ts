f03a415e8349953b5ce6ed0c9e1080bbbac2f2f6
玖叁 <x@fur93.icu>
2025-06-23 14:48:14 +0800

    fix: imports

--- src/modules/tenant/tenant.module.ts
+++ src/modules/tenant/tenant.module.ts
@@ -1,13 +1,14 @@
 import { WorkflowExecutionEntity } from '@/database/entities/workflow/workflow-execution';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConductorService } from '../workflow/conductor/conductor.service';
 import { WorkflowModule } from '../workflow/workflow.module';
 import { TenantController } from './tenant.controller';
 import { TenantService } from './tenant.service';
 
 @Module({
   controllers: [TenantController],
-  providers: [TenantService],
+  providers: [TenantService, ConductorService],
   imports: [TypeOrmModule.forFeature([WorkflowExecutionEntity]), WorkflowModule],
 })
 export class TenantModule {}
