37b78f9f02ea65e8ae4e39c49a9c880cfcfd7988
玖叁 <x@fur93.icu>
2025-07-23 16:21:30 +0800

    fix: card item style

--- ui/src/components/ui/card.tsx
+++ ui/src/components/ui/card.tsx
@@ -29,7 +29,7 @@ const CardDescription = React.forwardRef<HTMLParagraphElement, React.HTMLAttribu
 CardDescription.displayName = 'CardDescription';
 
 const CardContent = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
-  ({ className, ...props }, ref) => <div ref={ref} className={cn('p-6 pt-0', className)} {...props} />,
+  ({ className, ...props }, ref) => <div ref={ref} className={cn('p-6', className)} {...props} />,
 );
 CardContent.displayName = 'CardContent';
 
