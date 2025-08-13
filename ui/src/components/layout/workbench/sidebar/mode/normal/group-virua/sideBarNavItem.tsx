19aeeb4c9f093fa71c48269282d32426e5eb17b0
玖叁 <x@fur93.icu>
2025-08-13 15:46:00 +0800

    fix

--- ui/src/components/layout/workbench/sidebar/mode/normal/group-virua/sideBarNavItem.tsx
+++ ui/src/components/layout/workbench/sidebar/mode/normal/group-virua/sideBarNavItem.tsx
@@ -2,11 +2,13 @@ import React, { useContext } from 'react';
 
 import { useSortable } from '@dnd-kit/sortable';
 import { CSS } from '@dnd-kit/utilities';
-import { get } from 'lodash';
 import { LucideIcon } from 'lucide-react';
+import { get } from 'lodash';
+import { useTranslation } from 'react-i18next';
 
 import { useSystemConfig } from '@/apis/common';
 import { WorkbenchViewItemCurrentData } from '@/components/layout/workbench/sidebar/mode/normal/virtua/item.tsx';
+import { LANGUAGE_MAPPER } from '@/components/layout/workspace/vines-view/flow/headless-modal/endpoint/start-tool/workflow-input-config/input-config/input-editor/field/display-name';
 import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip';
 import { VinesLucideIcon } from '@/components/ui/vines-icon/lucide';
 import { cn, getI18nContent } from '@/utils';
@@ -27,13 +29,28 @@ export const SideBarNavItem: React.FC<ISpaceSidebarTabProps> = ({
   ...attr
 }) => {
   const { groupId: currentGroupId } = useContext(WorkbenchViewItemCurrentData);
+  const { t, i18n } = useTranslation();
   const { data: oem } = useSystemConfig();
   const themeMode = get(oem, 'theme.themeMode', 'shadow');
-
+  
   // 针对LF客户的主题定制
   const isLFTheme = themeMode === 'shadow';
   const backgroundClass = isLFTheme ? 'bg-[#f3f4f6]' : 'bg-neocard';
   // 获取当前语言的显示值
+
+  const displayText = (() => {
+    try {
+      // @ts-expect-error
+      const realDisplayName = JSON.parse(displayName);
+      const currentLanguageKey = LANGUAGE_MAPPER[i18n.language as keyof typeof LANGUAGE_MAPPER] || 'zh-CN';
+      const content = realDisplayName[currentLanguageKey];
+
+      // return t([`workspace.wrapper.space.tabs.${content || 'unknown'}`, content || 'Unknown Group']);
+      return content;
+    } catch {
+      return displayName;
+    }
+  })();
 
   const { attributes, listeners, setNodeRef, transform, transition, isDragging } = useSortable({ id: groupId });
 
@@ -54,8 +71,7 @@ export const SideBarNavItem: React.FC<ISpaceSidebarTabProps> = ({
         onlyShowWorkbenchIcon
           ? 'flex size-[var(--operation-bar-width)] items-center justify-center'
           : 'mb-global-1/2 flex w-full shrink-0 items-center justify-start gap-global-1/2 px-global-1/2',
-        groupId === currentGroupId &&
-          cn('group border border-input text-accent-foreground dark:bg-[#393939]', backgroundClass),
+        groupId === currentGroupId && cn('group border border-input text-accent-foreground dark:bg-[#393939]', backgroundClass),
       )}
       {...attr}
     >
@@ -65,7 +81,7 @@ export const SideBarNavItem: React.FC<ISpaceSidebarTabProps> = ({
         React.createElement(icon, { className: 'size-icon shrink-0', size: 20 })
       )}
       {!onlyShowWorkbenchIcon && (
-        <h1 className="line-clamp-1 max-w-20 text-ellipsis whitespace-nowrap text-sm">{getI18nContent(displayName)}</h1>
+        <h1 className="line-clamp-1 max-w-20 text-ellipsis whitespace-nowrap text-sm">{displayText}</h1>
       )}
       {!onlyShowWorkbenchIcon && children}
     </div>
