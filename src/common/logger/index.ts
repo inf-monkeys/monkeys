9e1aef66bca9ed3ce98e1a0a5bcb39869fff22b1
玖叁 <x@fur93.icu>
2025-08-19 19:14:38 +0800

    debug

--- src/common/logger/index.ts
+++ src/common/logger/index.ts
@@ -1,5 +1,8 @@
 import { configure, getLogger, Logger } from 'log4js';
+import { config } from '../config';
 export type LogLevel = 'error' | 'warn' | 'info' | 'debug';
+
+const appId = config.server.appId;
 
 const dateFileRollingOptions = {
   alwaysIncludePattern: true,
@@ -13,15 +16,15 @@ configure({
       type: 'stdout',
       layout: {
         type: 'pattern',
-        pattern: '[%d{ISO8601_WITH_TZ_OFFSET}] %p %c %f:%l  %m',
+        pattern: `[%d{ISO8601_WITH_TZ_OFFSET}] %p %c [${appId}] %f:%l  %m`,
       },
     },
     file: {
       type: 'dateFile',
-      filename: 'monkeys-server.log',
+      filename: `${appId}-server.log`,
       layout: {
         type: 'pattern',
-        pattern: '[%d{ISO8601_WITH_TZ_OFFSET}] %p %c %f:%l  %m',
+        pattern: `[%d{ISO8601_WITH_TZ_OFFSET}] %p %c [${appId}] %f:%l  %m`,
       },
       pattern: '.yyyy-MM-dd',
       ...dateFileRollingOptions,
