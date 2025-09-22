8c9f815fbf8a9bbeebc13347604062c46ee6e3d4
玖叁 <x@fur93.icu>
2025-09-22 17:22:19 +0800

    fix: migration

--- src/database/migrations/1758529338588-migration.ts
+++ src/database/migrations/1758529338588-migration.ts
@@ -9,7 +9,7 @@ export class Migration1758529338588 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`CREATE TYPE "public"."${appId}_model_training_status_enum" AS ENUM('idle', 'pending', 'running', 'completed', 'failed')`);
     await queryRunner.query(
-      `CREATE TABLE "${appId}_model_training" ("id" character varying(128) NOT NULL, "created_timestamp" TIMESTAMP NOT NULL DEFAULT now(), "updated_timestamp" TIMESTAMP NOT NULL DEFAULT now(), "is_deleted" boolean NOT NULL DEFAULT false, "team_id" character varying NOT NULL, "display_name" jsonb NOT NULL, "description" jsonb, "status" "public"."${appId}_model_training_status_enum" NOT NULL, CONSTRAINT "PK_80076e492c510c9714f182be766" PRIMARY KEY ("id"))`,
+      `CREATE TABLE "${appId}_model_training" ("id" character varying(128) NOT NULL, "created_timestamp" TIMESTAMP NOT NULL DEFAULT now(), "updated_timestamp" TIMESTAMP NOT NULL DEFAULT now(), "is_deleted" boolean NOT NULL DEFAULT false, "team_id" character varying NOT NULL, "display_name" jsonb NOT NULL, "description" jsonb, "status" "public"."${appId}_model_training_status_enum" NOT NULL, CONSTRAINT "PK_${appId}_80076e492c510c9714f182be766" PRIMARY KEY ("id"))`,
     );
   }
 
