ce4b6fa1f00757fe1df66ba965f099d059ee1659
玖叁 <x@fur93.icu>
2025-10-28 17:11:07 +0800

    fix: embed form provider

--- ui/src/components/layout/ugc-pages/design-project/design-project-card-wrapper.tsx
+++ ui/src/components/layout/ugc-pages/design-project/design-project-card-wrapper.tsx
@@ -1,13 +1,18 @@
+import React from 'react';
+
 import { useDesignProjectMetadataList } from '@/apis/designs';
 import { IDesignBoardMetadata, IDesignProject } from '@/apis/designs/typings';
 import { IAssetItem } from '@/apis/ugc/typings';
 import { DesignProjectCard } from '@/components/layout/ugc-pages/design-project/design-project-card';
-import React from 'react';
 
 interface DesignProjectCardWrapperProps {
   project: IAssetItem<IDesignProject>;
   onItemClick: (project: IAssetItem<IDesignProject>) => void;
-  operateArea?: (item: IAssetItem<IDesignProject>, trigger: React.ReactNode, tooltipTriggerContent?: string) => React.ReactNode;
+  operateArea?: (
+    item: IAssetItem<IDesignProject>,
+    trigger: React.ReactNode,
+    tooltipTriggerContent?: string,
+  ) => React.ReactNode;
 }
 
 export const DesignProjectCardWrapper: React.FC<DesignProjectCardWrapperProps> = ({
@@ -20,11 +25,6 @@ export const DesignProjectCardWrapper: React.FC<DesignProjectCardWrapperProps> =
   const firstBoard = boards?.[0] as IDesignBoardMetadata | undefined;
 
   return (
-    <DesignProjectCard
-      project={project}
-      firstBoard={firstBoard}
-      onItemClick={onItemClick}
-      operateArea={operateArea}
-    />
+    <DesignProjectCard project={project} firstBoard={firstBoard} onItemClick={onItemClick} operateArea={operateArea} />
   );
 };
