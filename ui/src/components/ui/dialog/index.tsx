af548c9679b85e999b2c88125974f0481331f076
Florian Liao <huahuadeliaoliao@icloud.com>
2025-11-12 23:55:27 +0800

    fix: resolve tldraw v4 popup interaction issue by enabling pointer-events

--- ui/src/components/ui/dialog/index.tsx
+++ ui/src/components/ui/dialog/index.tsx
@@ -40,6 +40,7 @@ const DialogOverlay = React.forwardRef<
       className,
     )}
     data-vines-overlay
+    style={{ pointerEvents: 'auto' }}
     {...props}
   />
 ));
@@ -59,6 +60,7 @@ const DialogContent = React.forwardRef<
         'bg-dialog data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-global p-6 shadow-lg duration-200 sm:rounded-lg',
         className,
       )}
+      style={{ pointerEvents: 'auto' }}
       {...props}
     >
       {children}
--- ui/src/components/ui/dropdown-menu.tsx
+++ ui/src/components/ui/dropdown-menu.tsx
@@ -49,6 +49,7 @@ const DropdownMenuSubContent = React.forwardRef<
       'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 min-w-[8rem] overflow-hidden rounded-md border bg-popover p-1 text-popover-foreground shadow-lg',
       className,
     )}
+    style={{ pointerEvents: 'auto' }}
     {...props}
   />
 ));
@@ -66,6 +67,7 @@ const DropdownMenuContent = React.forwardRef<
         'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 min-w-[8rem] overflow-hidden rounded-md border bg-popover p-1 text-popover-foreground shadow-md',
         className,
       )}
+      style={{ pointerEvents: 'auto' }}
       {...props}
     />
   </DropdownMenuPrimitive.Portal>
